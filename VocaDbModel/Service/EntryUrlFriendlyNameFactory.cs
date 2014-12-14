821ddf85fdc18d696fcb9efe0c487f8be144c248
riipah <riipah@gmail.com>
2014-12-14 20:54:46 +0200

    Add EntryUrlFriendlyNameFactory

---
+++ VocaDbModel/Service/EntryUrlFriendlyNameFactory.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Concurrent;
+using VocaDb.Model.Domain;
+
+namespace VocaDb.Model.Service {
+
+	public class EntryUrlFriendlyNameFactory : IEntryUrlFriendlyNameFactory {
+
+		private readonly ConcurrentDictionary<GlobalEntryId, string> cachedNames = new ConcurrentDictionary<GlobalEntryId, string>();
+
+		public string GetUrlFriendlyName(IEntryWithNames entry) {
+			
+			ParamIs.NotNull(() => entry);
+
+			return cachedNames.GetOrAdd(new GlobalEntryId(entry.EntryType, entry.Id), _ => entry.Names.GetUrlFriendlyName());
+
+		}
+
+	}
+
+	public interface IEntryUrlFriendlyNameFactory {
+		
+		string GetUrlFriendlyName(IEntryWithNames entry);
+
+	}
+
+}
