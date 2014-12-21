f437ef501a09df37c6228bd3c9cab0ac4388b705
riipah <riipah@gmail.com>
2014-12-21 23:05:28 +0200

    Search deleted albums. Added link from admin.

--- VocaDbWeb/Scripts/ViewModels/Album/DeletedAlbumsViewModel.ts
+++ VocaDbWeb/Scripts/ViewModels/Album/DeletedAlbumsViewModel.ts
@@ -9,6 +9,7 @@ module vdb.viewModels {
 			this.updateResults(true);
 			this.paging.page.subscribe(() => this.updateResults(false));
 			this.paging.pageSize.subscribe(() => this.updateResults(true));
+			this.searchTerm.subscribe(() => this.updateResults(true));
 		}
 
 		public discTypeName = (discType: string) => discType;
@@ -16,14 +17,19 @@ module vdb.viewModels {
 		public page = ko.observableArray<dc.AlbumContract>([]); // Current page of items
 		public paging = new ServerSidePagingViewModel(20); // Paging view model
 		public ratingStars = () => [];
+		public searchTerm = ko.observable("").extend({ rateLimit: { timeout: 300, method: "notifyWhenChangesStop" } });
 		public showTags = ko.observable(false);
 		public sort = ko.observable("Name");
 		public viewMode = ko.observable("Details");
 
 		private updateResults = (clearResults: boolean) => {
 
+			if (clearResults) {
+				this.paging.page(1);
+			}
+
 			var pagingProperties = this.paging.getPagingProperties(clearResults);
-			this.albumRepo.getList(pagingProperties, this.albumRepo.languagePreferenceStr, null, "Name", undefined, null,
+			this.albumRepo.getList(pagingProperties, this.albumRepo.languagePreferenceStr, this.searchTerm(), "Name", undefined, null,
 				null, undefined, undefined, "AdditionalNames,MainPicture", null, true, result => {
 
 					this.page(result.items);
