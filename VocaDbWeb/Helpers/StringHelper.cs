e77deced1014debd92dce6b997c14a32c21590fb
Jukka Hyvärinen <jukkahyv@gmail.com>
2014-12-25 17:26:33 +0200

    cleanup

--- VocaDbWeb/Helpers/StringHelper.cs
+++ VocaDbWeb/Helpers/StringHelper.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
-using System.Text;
 
 namespace VocaDb.Web.Helpers {
 
@@ -12,14 +9,6 @@ namespace VocaDb.Web.Helpers {
 		public static MvcHtmlString Join(string separator, IEnumerable<MvcHtmlString> strings) {
 
 			return new MvcHtmlString(string.Join(separator, strings.Select(s => s.ToHtmlString())));
-
-			/*if (!strings.Any())
-				return MvcHtmlString.Empty;
-
-			var s = strings.Aggregate(new StringBuilder(), (sb, str) => sb.Append(str.ToHtmlString() 
-				+ (str != strings.Last() ? ", " : string.Empty)));
-
-			return new MvcHtmlString(s.ToString());*/
 
 		}
 
