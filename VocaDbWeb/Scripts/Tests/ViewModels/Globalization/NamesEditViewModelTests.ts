d977a5abd688b90eb83b64e22472863b5dea80d3
Riipah Furude <riipah@vocadb.net>
2015-01-31 17:52:11 +0200

    #31: unit tests for NamesEditViewModel

---
+++ VocaDbWeb/Scripts/Tests/ViewModels/Globalization/NamesEditViewModelTests.ts
@@ -0,0 +1,59 @@
+﻿
+module vdb.tests.viewModels.globalization {
+
+	import vm = vdb.viewModels;
+	import LocalizedStringWithIdEditViewModel = vm.globalization.LocalizedStringWithIdEditViewModel;
+	import ContentLanguageSelection = vdb.models.globalization.ContentLanguageSelection;
+	import NamesEditViewModel = vm.globalization.NamesEditViewModel;
+
+	QUnit.module("NamesEditViewModelTests");
+
+	QUnit.test("primary name and aliases", assert => {
+
+		var names = [
+			new LocalizedStringWithIdEditViewModel(ContentLanguageSelection.English, "English name", 1),
+			new LocalizedStringWithIdEditViewModel(ContentLanguageSelection.Unspecified, "Alias", 2)
+		];
+
+		var viewModel = new NamesEditViewModel(names);
+
+		assert.equal(viewModel.englishName.value(), "English name", "englishName");
+		assert.equal(viewModel.originalName.value(), "", "originalName");
+		assert.equal(viewModel.aliases().length, 1, "aliases");
+		assert.equal(viewModel.aliases()[0].value(), "Alias", "aliases");
+		assert.equal(viewModel.hasPrimaryName(), true, "hasPrimaryName");
+
+	});
+
+	QUnit.test("only primary name", assert => {
+
+		var names = [
+			new LocalizedStringWithIdEditViewModel(ContentLanguageSelection.Japanese, "Japanese name", 1)
+		];
+
+		var viewModel = new NamesEditViewModel(names);
+
+		assert.equal(viewModel.englishName.value(), "", "englishName");
+		assert.equal(viewModel.originalName.value(), "Japanese name", "originalName");
+		assert.equal(viewModel.aliases().length, 0, "aliases");
+		assert.equal(viewModel.hasPrimaryName(), true, "hasPrimaryName");
+
+	});
+
+	QUnit.test("only aliases", assert => {
+
+		var names = [
+			new LocalizedStringWithIdEditViewModel(ContentLanguageSelection.Unspecified, "Alias", 1)
+		];
+
+		var viewModel = new NamesEditViewModel(names);
+
+		assert.equal(viewModel.englishName.value(), "", "englishName");
+		assert.equal(viewModel.originalName.value(), "", "originalName");
+		assert.equal(viewModel.aliases().length, 1, "aliases");
+		assert.equal(viewModel.aliases()[0].value(), "Alias", "aliases");
+		assert.equal(viewModel.hasPrimaryName(), false, "hasPrimaryName");
+
+	});
+
+} 
