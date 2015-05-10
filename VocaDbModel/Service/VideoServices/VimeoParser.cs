beea3e0ac8224e3bc81fcb6ff83212e73d293ad0
Riipah Furude <riipah@vocadb.net>
2015-05-10 23:45:30 +0300

    added upload date parsing for vimeo

--- VocaDbModel/Service/VideoServices/VimeoParser.cs
+++ VocaDbModel/Service/VideoServices/VimeoParser.cs
@@ -1,54 +1,67 @@
-﻿using System.Net;
-using System.Xml;
-using System.Xml.Linq;
-using System.Xml.XPath;
-using VocaDb.Model.Helpers;
+﻿using System;
+using System.Net;
+using System.Xml.Serialization;
 
 namespace VocaDb.Model.Service.VideoServices {
 
 	public class VimeoParser : IVideoServiceParser {
-
-		private int? GetLength(string lengthStr) {
-
-			int val;
-			if (int.TryParse(lengthStr, out val))
-				return val;
-			else
-				return null;
-
-		}
 
 		public VideoTitleParseResult GetTitle(string id) {
 
 			var url = string.Format("http://vimeo.com/api/v2/video/{0}.xml", id);
 
 			var request = WebRequest.Create(url);
-			XDocument doc;
+			var serializer = new XmlSerializer(typeof(VimeoResult));
+			VimeoResult result;
 
 			try {
 				using (var response = request.GetResponse())
 				using (var stream = response.GetResponseStream()) {
-					doc = XDocument.Load(stream);					
+					result = (VimeoResult)serializer.Deserialize(stream);					
 				}
 			} catch (WebException x) {
 				return VideoTitleParseResult.CreateError("Vimeo (error): " + x.Message);
-			} catch (XmlException x) {
-				return VideoTitleParseResult.CreateError("Vimeo (error): " + x.Message);
 			}
 
-			var titleElem = doc.XPathSelectElement("videos/video/title");
-
-			if (titleElem == null) {
+			if (result == null || result.Video == null || string.IsNullOrEmpty(result.Video.Title)) {
 				return VideoTitleParseResult.CreateError("Vimeo (error): title element not found");
 			}
 
-			var author = XmlHelper.GetNodeTextOrEmpty(doc, "videos/video/user_name");
-			var thumbUrl = XmlHelper.GetNodeTextOrEmpty(doc, "videos/video/thumbnail_small");
-			var length = GetLength(XmlHelper.GetNodeTextOrEmpty(doc, "videos/video/duration"));
+			var author = result.Video.User_Name;
+			var thumbUrl = result.Video.Thumbnail_Small;
+			var length = result.Video.Duration;
+			var date = Convert.ToDateTime(result.Video.Upload_Date); // xmlserializer can't parse the date
 
-			return VideoTitleParseResult.CreateSuccess(titleElem.Value, author, thumbUrl, length);
+			return VideoTitleParseResult.CreateSuccess(result.Video.Title, author, thumbUrl, length, uploadDate: date);
 
 		}
+
+	}
+
+	[XmlRoot("videos", Namespace = "")]
+	public class VimeoResult {
+		
+		[XmlElement("video")]
+		public VimeoVideo Video { get; set; }
+
+	}
+
+	public class VimeoVideo {
+		
+		[XmlElement("duration")]
+		public int Duration { get; set; }
+
+		[XmlElement("thumbnail_small")]
+		public string Thumbnail_Small { get; set; }
+
+		[XmlElement("title")]
+		public string Title { get; set; }
+
+		[XmlElement("upload_date")]
+		public string Upload_Date { get; set; }
+
+		[XmlElement("user_name")]
+		public string User_Name { get; set; }
 
 	}
 
