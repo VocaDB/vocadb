5fb9a6fcc5c41260dc37af89af818959ee694ff3
Riipah Furude <riipah@vocadb.net>
2015-05-26 20:26:49 +0300

    remove unused code

--- VocaDbModel/DataContracts/Activityfeed/ActivityEntryContract.cs
+++ VocaDbModel/DataContracts/Activityfeed/ActivityEntryContract.cs
@@ -39,24 +39,6 @@ namespace VocaDb.Model.DataContracts.Activityfeed {
 
 		}
 
-		// Note: not in use currently
-		public ActivityEntryContract(ActivityEntry entry, ContentLanguagePreference languagePreference, string changeMessage, string entryTypeName,
-			bool getArchivedVersion) {
-
-			ParamIs.NotNull(() => entry);
-
-			ArtistString = GetArtistString(entry.EntryBase, languagePreference);
-			Author = new UserContract(entry.Author);
-			CreateDate = entry.CreateDate;
-			EditEvent = entry.EditEvent;
-			EntryRef = new EntryWithImageContract(entry.EntryBase, GetMime(entry.EntryBase), GetSongThumbUrl(entry.EntryBase), languagePreference);
-			EntryTypeName = entryTypeName;
-
-			if (getArchivedVersion && entry.ArchivedVersionBase != null)
-				ArchivedVersion = new ArchivedVersionForReviewContract(entry.ArchivedVersionBase, changeMessage, languagePreference);
-
-		}
-
 		public ActivityEntryContract(ArchivedObjectVersion entry, ContentLanguagePreference languagePreference) {
 
 			ParamIs.NotNull(() => entry);
@@ -68,9 +50,6 @@ namespace VocaDb.Model.DataContracts.Activityfeed {
 			EntryRef = new EntryWithImageContract(entry.EntryBase, GetMime(entry.EntryBase), GetSongThumbUrl(entry.EntryBase), languagePreference);
 
 		}
-
-		// Note: not in use currently
-		public ArchivedVersionForReviewContract ArchivedVersion { get; set; }
 
 		public string ArtistString { get; set; }
 
