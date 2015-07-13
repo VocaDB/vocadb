9eed923d2d0a57433f6ab67d8d603e23254d7db2
Riipah Furude <riipah@vocadb.net>
2015-07-13 23:02:39 +0300

    sorting song lists

--- VocaDbWeb/Scripts/ViewModels/SongList/FeaturedSongListsViewModel.ts
+++ VocaDbWeb/Scripts/ViewModels/SongList/FeaturedSongListsViewModel.ts
@@ -27,7 +27,18 @@ module vdb.viewModels.songList {
 	export class FeaturedSongListCategoryViewModel {
 		
 		constructor(private listRepo: rep.SongListRepository, private category: string) {
-			
+
+			// Should figure out a better way for this.
+			this.showSort = category === "Concerts" || category === "VocaloidRanking";
+
+			this.sort.subscribe(this.clear);
+
+		}
+
+		public clear = () => {
+			this.lists([]);
+			this.start = 0;
+			this.loadMore();
 		}
 
 		public hasMore = ko.observable(false);
@@ -47,14 +58,18 @@ module vdb.viewModels.songList {
 		public lists = ko.observableArray<dc.SongListContract>([]);
 
 		public loadMore = () => {
-			this.listRepo.getFeatured(this.category, { start: this.start, maxEntries: 50, getTotalCount: true }, 'Date', (result) => {
+			this.listRepo.getFeatured(this.category, { start: this.start, maxEntries: 50, getTotalCount: true }, this.sort(), (result) => {
 				ko.utils.arrayPushAll(this.lists, result.items);
 				this.start = this.start + result.items.length;
 				this.hasMore(result.totalCount > this.start);
 			});
 		};
 
+		public showSort: boolean;
+
 		public start = 0;
+
+		public sort = ko.observable("Date");
 
 	}
 
