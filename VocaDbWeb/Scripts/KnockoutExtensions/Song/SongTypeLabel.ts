742264fce549513c09d517cf604a11e5b62fe3c0
Riipah Furude <riipah@vocadb.net>
2015-07-23 23:01:45 +0300

    re-added song type label to original songs

--- VocaDbWeb/Scripts/KnockoutExtensions/Song/SongTypeLabel.ts
+++ VocaDbWeb/Scripts/KnockoutExtensions/Song/SongTypeLabel.ts
@@ -3,62 +3,64 @@ interface KnockoutBindingHandlers {
 	songTypeLabel: KnockoutBindingHandler;
 }
 
-interface SongTypeLabelInfo {
-	addClass: string;
-	removeClasses: string;
-	content: string;
-}
+module vdb.knockoutExtensions.song {
 
-var songTypeLabelInfos = {
-	Cover: {
-		addClass: "",
-		removeClasses: "label-important label-inverse label-info label-success",
-		content: "C"
-	},
-	DramaPV: {
-		addClass: "label-success",
-		removeClasses: "label-info label-inverse label-important",
-		content: "D"
-	},
-	Instrumental: {
-		addClass: "label-inverse",
-		removeClasses: "label-info label-important label-success",
-		content: "I"
-	},
-	Mashup: {
-		addClass: "",
-		removeClasses: "label-important label-inverse label-info label-success",
-		content: "M"
-	},
-	Original: {
-		addClass: "label-info",
-		removeClasses: "label-important label-inverse",
-		content: "O"
-	},
-	Other: {
-		addClass: "",
-		removeClasses: "label-important label-inverse label-info",
-		content: "O"
-	},
-	Remaster: {
-		addClass: "label-info",
-		removeClasses: "label-important label-inverse label-success",
-		content: "R"
-	},
-	Remix: {
-		addClass: "",
-		removeClasses: "label-important label-inverse label-info label-success",
-		content: "R"
-	},
-	MusicPV: {
-		addClass: "label-success",
-		removeClasses: "label-important label-inverse label-info",
-		content: "PV"
+	interface SongTypeLabelInfo {
+		addClass: string;
+		removeClasses: string;
+		content: string;
 	}
-};
 
-ko.bindingHandlers.songTypeLabel = {
-	init: (element: HTMLElement, valueAccessor: () => string) => {
+	var songTypeLabelInfos = {
+		Cover: {
+			addClass: "",
+			removeClasses: "label-important label-inverse label-info label-success",
+			content: "C"
+		},
+		DramaPV: {
+			addClass: "label-success",
+			removeClasses: "label-info label-inverse label-important",
+			content: "D"
+		},
+		Instrumental: {
+			addClass: "label-inverse",
+			removeClasses: "label-info label-important label-success",
+			content: "I"
+		},
+		Mashup: {
+			addClass: "",
+			removeClasses: "label-important label-inverse label-info label-success",
+			content: "M"
+		},
+		Original: {
+			addClass: "label-info",
+			removeClasses: "label-important label-inverse",
+			content: "O"
+		},
+		Other: {
+			addClass: "",
+			removeClasses: "label-important label-inverse label-info",
+			content: "O"
+		},
+		Remaster: {
+			addClass: "label-info",
+			removeClasses: "label-important label-inverse label-success",
+			content: "R"
+		},
+		Remix: {
+			addClass: "",
+			removeClasses: "label-important label-inverse label-info label-success",
+			content: "R"
+		},
+		MusicPV: {
+			addClass: "label-success",
+			removeClasses: "label-important label-inverse label-info",
+			content: "PV"
+		}
+	};
+
+	export function songTypeLabel(element: HTMLElement, valueAccessor: () => string) {
+
 		var val = valueAccessor();
 
 		if (!val) {
@@ -74,4 +76,9 @@ ko.bindingHandlers.songTypeLabel = {
 		}
 
 	}
+
+}
+
+ko.bindingHandlers.songTypeLabel = {
+	init: vdb.knockoutExtensions.song.songTypeLabel
 };
