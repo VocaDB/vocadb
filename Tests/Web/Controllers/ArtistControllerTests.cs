6aab81a80fc5f88183cfada67f7588f433128063
Riipah Furude <riipah@vocadb.net>
2015-08-13 22:51:43 +0300

    added test, translation of export

--- Tests/Web/Controllers/ArtistControllerTests.cs
+++ Tests/Web/Controllers/ArtistControllerTests.cs
@@ -1,6 +1,9 @@
-﻿using System.Web.Mvc;
+﻿using System.Net;
+using System.Web;
+using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using VocaDb.Web.Controllers;
+using VocaDb.Web.Models.Artist;
 
 namespace VocaDb.Tests.Web.Controllers {
 
@@ -11,10 +14,34 @@ namespace VocaDb.Tests.Web.Controllers {
 	public class ArtistControllerTests {
 
 		private ArtistController controller;
+		private HttpResponseBase response;
 
 		[TestInitialize]
 		public void SetUp() {
+
 			controller = new ArtistController(null, null, null);
+
+			var responseMock = new Moq.Mock<HttpResponseBase>();
+			responseMock.SetupProperty(m => m.StatusCode);
+			responseMock.SetupProperty(m => m.StatusDescription);
+			response = responseMock.Object;
+
+			var context = new Moq.Mock<HttpContextBase>();
+			context.SetupGet(m => m.Response).Returns(response);
+			var controllerContext = new Moq.Mock<ControllerContext>();
+			controllerContext.SetupGet(m => m.HttpContext).Returns(context.Object);
+			controller.ControllerContext = controllerContext.Object;
+
+		}
+
+		[TestMethod]
+		public void Edit_ModelIsNull() {
+
+			var result = controller.Edit(new ArtistEditViewModel());
+
+			Assert.IsNotNull(result, "result");
+			Assert.AreEqual((int)HttpStatusCode.BadRequest, response.StatusCode, "Response status code");
+
 		}
 
 		[TestMethod]
