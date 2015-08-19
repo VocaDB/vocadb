54aabba43f744ffc52aef8f734564b26a3262131
Riipah Furude <riipah@vocadb.net>
2015-08-19 20:41:58 +0300

    extracted youtubevideoresponse

---
+++ VocaDbModel/Service/VideoServices/Youtube/YoutubeService.cs
@@ -0,0 +1,25 @@
+﻿using VocaDb.Model.Helpers;
+
+namespace VocaDb.Model.Service.VideoServices.Youtube {
+
+	public class YoutubeService {
+
+		private const string videoQueryFormat =
+			"https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails&key={0}&id={1}";
+
+		private readonly string apiKey;
+
+		public YoutubeService(string apiKey) {
+			this.apiKey = apiKey;
+		}
+
+		public YoutubeVideoResponse Video(string id) {
+
+			var url = string.Format(videoQueryFormat, apiKey, id);
+
+			return JsonRequest.ReadObject<YoutubeVideoResponse>(url);
+
+		}
+
+	}
+}
--- VocaDbModel/Service/VideoServices/YoutubeParser.cs
+++ VocaDbModel/Service/VideoServices/YoutubeParser.cs
@@ -1,16 +1,13 @@
 ﻿using System;
 using System.Linq;
+using System.Net;
 using System.Xml;
-using VocaDb.Model.Helpers;
 using VocaDb.Model.Service.VideoServices.Youtube;
 using VocaDb.Model.Utils;
 
 namespace VocaDb.Model.Service.VideoServices {
 
 	public class YoutubeParser : IVideoServiceParser {
-
-		private const string videoQueryFormat =
-			"https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails&key={0}&id={1}";
 
 		private int? GetLength(YoutubeVideoItem video) {
 
@@ -35,14 +32,12 @@ namespace VocaDb.Model.Service.VideoServices {
 
 		public VideoTitleParseResult GetTitle(string id) {
 
-			var apiKey = AppConfig.YoutubeApiKey;
-
-			var url = string.Format(videoQueryFormat, apiKey, id);
+			var service = new YoutubeService(AppConfig.YoutubeApiKey);
 
 			YoutubeVideoResponse result;
 			try {
-				result = JsonRequest.ReadObject<YoutubeVideoResponse>(url);
-			} catch (Exception x) {
+				result = service.Video(id);
+            } catch (WebException x) {
 				return VideoTitleParseResult.CreateError(x.Message);
 			}
 
@@ -57,40 +52,6 @@ namespace VocaDb.Model.Service.VideoServices {
 			var publishDate = GetPublishDate(video);
 
 			return VideoTitleParseResult.CreateSuccess(video.Snippet.Title, author, thumbUrl, length, uploadDate: publishDate);
-
-		}
-
-		public class YoutubeVideoResponse : YoutubeResponse<YoutubeVideoItem> {}
-
-		public class YoutubeVideoItem : YoutubeItem<YoutubeVideoSnippet> {
-			
-			public YoutubeVideoContentDetails ContentDetails { get; set; }
-
-		}
-
-		public class YoutubeVideoSnippet : Snippet {
-			
-			public string ChannelTitle { get; set; }
-
-			public YoutubeVideoThumbnails Thumbnails { get; set; }
-
-		}
-
-		public class YoutubeVideoThumbnails {
-			
-			public YoutubeVideoThumbnail Default { get; set; }
-
-		}
-
-		public class YoutubeVideoThumbnail {
-			
-			public string Url { get; set;  }
-
-		}
-
-		public class YoutubeVideoContentDetails {
-			
-			public string Duration { get; set; }
 
 		}
 
