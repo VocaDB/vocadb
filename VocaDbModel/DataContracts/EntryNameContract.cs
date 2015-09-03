a87399f4fcdd2d740a900d837e7fb4d529739125
Riipah Furude <riipah@vocadb.net>
2015-09-03 19:38:57 +0300

    add DataContract attribute

--- VocaDbModel/DataContracts/EntryNameContract.cs
+++ VocaDbModel/DataContracts/EntryNameContract.cs
@@ -1,8 +1,11 @@
-﻿namespace VocaDb.Model.DataContracts {
+﻿using System.Runtime.Serialization;
+
+namespace VocaDb.Model.DataContracts {
 
 	/// <summary>
 	/// Entry name (title) with primary display name and additional names.
 	/// </summary>
+	[DataContract(Namespace = Schemas.VocaDb)]
 	public class EntryNameContract {
 
 		public static EntryNameContract Empty {
@@ -22,14 +25,14 @@
 		/// <summary>
 		/// Comma-separated list of additional names (excluding the primary display name).
 		/// </summary>
+		[DataMember]
 		public string AdditionalNames { get; set; }
 
 		/// <summary>
 		/// Display name is the primary name localized to the requested language.
 		/// </summary>
+		[DataMember]
 		public string DisplayName { get; set; }
-
-
 
 	}
 
