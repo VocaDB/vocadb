d6c13764760b6d39b9ba03e49658dd0d70389e14
Riipah Furude <riipah@vocadb.net>
2015-10-31 18:44:05 +0200

    missing ID handling

--- VocaDbWeb/Controllers/TagController.cs
+++ VocaDbWeb/Controllers/TagController.cs
@@ -132,7 +132,10 @@ namespace VocaDb.Web.Controllers
 
 		}
 
-		public ActionResult Versions(int id) {
+		public ActionResult Versions(int id = invalidId) {
+
+			if (id == invalidId)
+				return NoId();
 
 			var contract = queries.GetTagWithArchivedVersions(id);
 
