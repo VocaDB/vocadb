12a02897f2e764662053b03af33f4232c0fd37c0
Riipah Furude <riipah@vocadb.net>
2015-11-15 16:24:31 +0200

    fix timezone for recent activity on front page

--- VocaDbWeb/Code/TimeAgoStringBuilder.cs
+++ VocaDbWeb/Code/TimeAgoStringBuilder.cs
@@ -5,6 +5,16 @@ namespace VocaDb.Web.Code {
 
 	public static class TimeAgoStringBuilder {
 
+		private static TimeSpan Diff(DateTime now, DateTime time) {
+
+			// Note: DateTime substraction does not automatically consider the DateTimeKind - we need to convert them to the same kind. 
+			if (now.Kind == DateTimeKind.Local && time.Kind == DateTimeKind.Utc)
+				now = now.ToUniversalTime();
+
+			return now - time;
+
+		}
+
 		public static string FormatTimeAgo(TimeSpan timeSpan) {
 
 			if (timeSpan.TotalDays >= 2)
@@ -19,7 +29,7 @@ namespace VocaDb.Web.Code {
 
 		public static string FormatTimeAgo(DateTime now, DateTime time) {
 
-			var timeSpan = now - time;
+			var timeSpan = Diff(now, time);
 
 			if (timeSpan.TotalDays > 60) {
 				var months = ((now.Year - time.Year) * 12) + now.Month - time.Month;
