a806c243e2d31b4e2d9a4e73c0f1f393f697e492
Riipah Furude <riipah@vocadb.net>
2015-12-13 16:15:52 +0200

    show song publish date in search

--- VocaDbWeb/Scripts/KnockoutExtensions/Bootstrap/Tooltip.ts
+++ VocaDbWeb/Scripts/KnockoutExtensions/Bootstrap/Tooltip.ts
@@ -1,10 +1,12 @@
 ﻿
 interface KnockoutBindingHandlers {
+	// Shows bootstrap tooltip
 	tooltip: KnockoutBindingHandler;
 }
 
 ko.bindingHandlers.tooltip = {
-    init: (element: HTMLElement, params: any) => {
-		$(element).tooltip(ko.unwrap(params));
+    init: (element: HTMLElement, params: Function) => {
+		var unwrapped = ko.unwrap(params());
+		$(element).tooltip(unwrapped);
     }
 };
