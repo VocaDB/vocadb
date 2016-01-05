78a53297f842460f045888ec485b79ed5667db3e
Riipah Furude <riipah@vocadb.net>
2016-01-06 00:49:48 +0200

    made database tests run again

---
+++ Tests/TestSupport/TestDatabaseFactory.cs
@@ -0,0 +1,85 @@
+﻿using System.Configuration;
+using System.Data.SqlClient;
+using NHibernate;
+using NHibernate.Tool.hbm2ddl;
+using VocaDb.Model.Service;
+
+namespace VocaDb.Tests.TestSupport {
+
+	public class TestDatabaseFactory {
+
+		private void RunSql(string connectionStringName, string sql) {
+
+			var connectionString = ConfigurationManager.ConnectionStrings[connectionStringName].ConnectionString;
+
+			using (var connection = new SqlConnection(connectionString)) {
+
+				connection.Open();
+
+				var command = new SqlCommand(sql, connection);
+				command.ExecuteNonQuery();
+
+			}
+
+
+		}
+
+		/// <summary>
+		/// Creates additional required database schemas.
+		/// NHibernate schema export doesn't create any schemas, so only the dbo schema is created by default.
+		/// </summary>
+		private void CreateSchemas(string connectionString) {
+
+			// SQL from http://stackoverflow.com/a/521271 (might be T-SQL specific)
+			RunSql(connectionString, @"
+				IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'mikudb')
+				BEGIN
+				EXEC('CREATE SCHEMA [mikudb]');
+				END
+				IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'discussions')
+				BEGIN
+				EXEC('CREATE SCHEMA [discussions]');
+				END
+			");
+
+		}
+
+		public ISessionFactory BuildTestSessionFactory() {
+
+			var testDatabaseConnectionString = "LocalDB";
+			var config = DatabaseConfiguration.Configure(testDatabaseConnectionString);
+
+			/* 
+			 * Need to comment these out when not needed because session factory can only be created once.
+			 * Database schemas need to be created BEFORE NHibernate schema export.
+			 * This needs to be run only once.
+			*/
+			/*
+			var fac = DatabaseConfiguration.BuildSessionFactory(config);
+
+			CreateSchemas(fac);*/
+
+			// Drop old database if any, create new schema
+			config.ExposeConfiguration(cfg => {
+
+				// NH schema export does not correctly drop all constraints
+				// SQL from http://stackoverflow.com/a/26348027
+				RunSql(testDatabaseConnectionString, @"
+					exec sp_MSforeachtable ""declare @name nvarchar(max); set @name = parsename('?', 1); exec sp_MSdropconstraints @name"";
+				");
+
+				var export = new SchemaExport(cfg);
+				//export.SetOutputFile(@"C:\Temp\vdb.sql");
+				export.Drop(false, true);
+				export.Create(false, true);
+
+			});
+
+			var fac = DatabaseConfiguration.BuildSessionFactory(config);
+			return fac;
+
+		}
+
+	}
+
+}
