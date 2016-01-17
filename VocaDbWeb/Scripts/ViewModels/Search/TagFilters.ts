1c196c6885eb0a5a04e576e653e008c9bf1198ff
Riipah Furude <riipah@vocadb.net>
2016-01-17 23:51:16 +0200

    #112: allow filtering by multiple artists and tags on rated songs page

---
+++ VocaDbWeb/Scripts/ViewModels/Search/TagFilters.ts
@@ -0,0 +1,46 @@
+﻿
+module vdb.viewModels.search {
+	
+	// Manages tag filters for search
+	export class TagFilters {
+
+		constructor(private tagRepo: repositories.TagRepository) {
+			
+			this.tagIds = ko.computed(() => _.map(this.tags(), t => t.id));
+
+		}
+
+		public addTag = (tag: dc.TagBaseContract) => this.tags.push(new TagFilter(tag.id, tag.name, tag.urlSlug));
+
+		public addTags = (
+			selectedTagIds: number[],
+			tagRepo: rep.TagRepository) => {
+
+			if (!selectedTagIds)
+				return;
+
+			var filters = _.map(selectedTagIds, a => new TagFilter(a));
+			ko.utils.arrayPushAll(this.tags, filters);
+
+			if (!tagRepo)
+				return;
+
+			_.forEach(filters, newTag => {
+
+				var selectedTagId = newTag.id;
+
+				tagRepo.getById(selectedTagId, null, tag => {
+					newTag.name(tag.name);
+					newTag.urlSlug(tag.urlSlug);
+				});
+
+			});
+
+		};
+
+		public tags = ko.observableArray<TagFilter>([]);
+		public tagIds: KnockoutComputed<number[]>;
+
+	}
+
+}
