0cb192f36f5e58f9b489b055ca9327a79fc82eeb
Riipah Furude <riipah@vocadb.net>
2016-02-04 21:58:21 +0200

    changed full image size for tags and song lists to 600px (up from 500px)

--- VocaDbModel/Database/Queries/TagQueries.cs
+++ VocaDbModel/Database/Queries/TagQueries.cs
@@ -466,7 +466,7 @@ namespace VocaDb.Model.Database.Queries {
 					var thumb = new EntryThumb(tag, uploadedImage.Mime);
 					tag.Thumb = thumb;
 					var thumbGenerator = new ImageThumbGenerator(imagePersister);
-					thumbGenerator.GenerateThumbsAndMoveImage(uploadedImage.Stream, thumb, Tag.ImageSizes, originalSize: 500);
+					thumbGenerator.GenerateThumbsAndMoveImage(uploadedImage.Stream, thumb, Tag.ImageSizes, originalSize: Constants.RestrictedImageOriginalSize);
 
 				}
 
--- VocaDbModel/Domain/Constants.cs
+++ VocaDbModel/Domain/Constants.cs
@@ -3,6 +3,12 @@
 	public static class Constants {
 
 		/// <summary>
+		/// For some entry types we want to prevent users from uploading too large images.
+		/// 600px is the minimum recommended size for Facebook.
+		/// </summary>
+		public const int RestrictedImageOriginalSize = 600;
+
+		/// <summary>
 		/// 24h
 		/// </summary>
 		public const int SecondsInADay = 86400;
