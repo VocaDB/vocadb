9b890d43a8bffb35c5a48fdad72cfedd2d7cdad4
Riipah Furude <riipah@vocadb.net>
2016-03-04 21:07:47 +0200

    fix search page broken because of wrong enum

--- VocaDbWeb/Scripts/ViewModels/Search/SearchViewModel.ts
+++ VocaDbWeb/Scripts/ViewModels/Search/SearchViewModel.ts
@@ -69,7 +69,7 @@ module vdb.viewModels.search {
 				shuffle,
 				pvPlayersFactory);
 
-			this.tagSearchViewModel = new TagSearchViewModel(this, models.globalization.ContentLanguageSelection[languageSelection], tagRepo);
+			this.tagSearchViewModel = new TagSearchViewModel(this, models.globalization.ContentLanguagePreference[languageSelection], tagRepo);
 
 			if (tagIds != null || artistId != null || artistType || albumType || songType || onlyWithPVs != null || since || minScore)
 				this.showAdvancedFilters(true);
