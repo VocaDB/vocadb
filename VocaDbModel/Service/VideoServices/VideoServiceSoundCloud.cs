b5ac5457258dc805ed590746bf5bef47e65c3269
Riipah Furude <riipah@vocadb.net>
2016-06-13 22:04:57 +0300

    handle forbidden response for SC

--- VocaDbModel/Service/VideoServices/VideoServiceSoundCloud.cs
+++ VocaDbModel/Service/VideoServices/VideoServiceSoundCloud.cs
@@ -59,11 +59,19 @@ namespace VocaDb.Model.Service.VideoServices {
 			try {				
 				result = JsonRequest.ReadObject<SoundCloudResult>(apiUrl, timeoutMs: 10000);
 			} catch (WebException x) {
-				var msg = string.Format("Unable to load SoundCloud URL '{0}'", url);
+
+				var msg = string.Format("Unable to load SoundCloud URL '{0}'.", url);
+
+				// Forbidden most likely means the artist has prevented API access to their tracks, http://stackoverflow.com/a/36529330
+				if (((HttpWebResponse)x.Response).StatusCode == HttpStatusCode.Forbidden) {
+					msg += "This track cannot be embedded.";
+				}
+
 				log.Warn(x, msg);
 				return VideoUrlParseResult.CreateError(url, VideoUrlParseResultType.LoadError, new VideoParseException(msg, x));
+
 			} catch (JsonSerializationException x) {
-				var msg = string.Format("Unable to load SoundCloud URL '{0}'", url);
+				var msg = string.Format("Unable to load SoundCloud URL '{0}'.", url);
 				log.Warn(x, msg);
 				return VideoUrlParseResult.CreateError(url, VideoUrlParseResultType.LoadError, new VideoParseException(msg, x));
 			}
