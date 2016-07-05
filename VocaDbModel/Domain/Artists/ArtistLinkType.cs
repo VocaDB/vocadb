4eaf0404f0d7b10ff0c357b87b51e0d744cd3d1c
Riipah Furude <riipah@vocadb.net>
2016-07-05 17:02:26 +0300

    comments

--- VocaDbModel/Domain/Artists/ArtistLinkType.cs
+++ VocaDbModel/Domain/Artists/ArtistLinkType.cs
@@ -1,9 +1,25 @@
 ﻿namespace VocaDb.Model.Domain.Artists {
 
+	/// <summary>
+	/// Type of (bidirectional) link between artists.
+	/// </summary>
 	public enum ArtistLinkType {
+
+		/// <summary>
+		/// Group/member
+		/// </summary>
 		Group,
+
+		/// <summary>
+		/// Illustrator/illustration
+		/// </summary>
 		Illustrator,
+
+		/// <summary>
+		/// Voice provider / voice provider of
+		/// </summary>
 		VoiceProvider
+
 	}
 
 }
