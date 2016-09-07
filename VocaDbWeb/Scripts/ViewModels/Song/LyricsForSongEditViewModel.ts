71c4eb97f11d4d18a1d131a1625550e5df360d48
Riipah Furude <riipah@vocadb.net>
2016-09-07 22:21:30 +0300

    buttons for changing lyrics type

--- VocaDbWeb/Scripts/ViewModels/Song/LyricsForSongEditViewModel.ts
+++ VocaDbWeb/Scripts/ViewModels/Song/LyricsForSongEditViewModel.ts
@@ -9,17 +9,18 @@ namespace vdb.viewModels.songs {
 		constructor(contract?: dc.songs.LyricsForSongContract) {
 			
 			if (contract) {
-				this.id = contract.id;
+				this.id = ko.observable(contract.id);
 				this.cultureCode = ko.observable(contract.cultureCode);
 				this.language = ko.observable(contract.language);
 				this.source = ko.observable(contract.source);
-				this.translationType = contract.translationType;
+				this.translationType = ko.observable(contract.translationType);
 				this.value = ko.observable(contract.value);
 			} else {
+				this.id = ko.observable(0);
 				this.cultureCode = ko.observable("");
 				this.language = ko.observable(vdb.models.globalization.ContentLanguageSelection[vdb.models.globalization.ContentLanguageSelection.Unspecified]);
 				this.source = ko.observable("");
-				this.translationType = cls.globalization.TranslationType[cls.globalization.TranslationType.Translation];
+				this.translationType = ko.observable(cls.globalization.TranslationType[cls.globalization.TranslationType.Translation]);
 				this.value = ko.observable("");
 			}
 
@@ -34,17 +35,17 @@ namespace vdb.viewModels.songs {
 
 		public cultureCode: KnockoutObservable<string>;
 
-		public id: number;
+		public id: KnockoutObservable<number>;
 
 		public isNew: boolean;
 
 		public language: KnockoutObservable<string>;
 
-		public showLanguageSelection = () => this.translationType !== cls.globalization.TranslationType[cls.globalization.TranslationType.Romanized];
+		public showLanguageSelection = () => this.translationType() !== cls.globalization.TranslationType[cls.globalization.TranslationType.Romanized];
 
 		public source: KnockoutObservable<string>;
 
-		public translationType: string;
+		public translationType: KnockoutObservable<string>;
 
 		public value: KnockoutObservable<string>;
 
@@ -53,7 +54,7 @@ namespace vdb.viewModels.songs {
 	export class LyricsForSongListEditViewModel extends BasicListEditViewModel<LyricsForSongEditViewModel, dc.songs.LyricsForSongContract> {
 
 		private find = (translationType: string) => {
-			var vm = _.find(this.items(), i => i.translationType === translationType);
+			var vm = _.find(this.items(), i => i.translationType() === translationType);
 			if (vm)
 				_.remove(this.items(), vm);
 			else {
@@ -66,6 +67,37 @@ namespace vdb.viewModels.songs {
 			super(LyricsForSongEditViewModel, contracts);
 			this.original = this.find("Original");
 			this.romanized = this.find("Romanized");
+		}
+
+		public changeToOriginal = (lyrics: LyricsForSongEditViewModel) => {
+			this.original.id(lyrics.id());
+			this.original.value(lyrics.value());
+			this.original.cultureCode(lyrics.cultureCode());
+			this.original.source(lyrics.source());
+			this.items.remove(lyrics);
+		}
+
+		public changeToTranslation = (lyrics: LyricsForSongEditViewModel) => {
+
+			if (lyrics === this.original) {
+
+				var newLyrics = new LyricsForSongEditViewModel({
+					id: this.original.id(), cultureCode: this.original.cultureCode(), source: this.original.source(),
+					value: this.original.value(),
+					translationType: cls.globalization.TranslationType[cls.globalization.TranslationType.Translation]
+				});
+
+				this.items.push(newLyrics);
+
+				this.original.id(0);
+				this.original.value("");
+				this.original.cultureCode("");
+				this.original.source("");
+
+			} else {
+				lyrics.translationType(cls.globalization.TranslationType[cls.globalization.TranslationType.Translation]);
+			}
+
 		}
 
 		public original: LyricsForSongEditViewModel;
