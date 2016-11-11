723e0a8f25ea6a0eda9bf7ecfe2bec8f080e02e5
Riipah Furude <riipah@vocadb.net>
2016-11-11 20:42:43 +0200

    base voicebanks should be allowed by default (fixes redirect to child VB when searching)

--- VocaDbModel/Service/Search/Artists/ArtistQueryParams.cs
+++ VocaDbModel/Service/Search/Artists/ArtistQueryParams.cs
@@ -37,7 +37,7 @@ namespace VocaDb.Model.Service.Search.Artists {
 
 		public AdvancedSearchFilter[] AdvancedFilters { get; set; }
 
-		public bool AllowBaseVoicebanks { get; set; }
+		public bool AllowBaseVoicebanks { get; set; } = true;
 
 		public ArtistType[] ArtistTypes { get; set; }
 
