8fa652d811262b533d8fff263fedde9a0b3eed01
Riipah Furude <riipah@vocadb.net>
2016-12-18 20:08:55 +0200

    preparing for jqui update

--- VocaDbWeb/Scripts/KnockoutExtensions/jqButton.ts
+++ VocaDbWeb/Scripts/KnockoutExtensions/jqButton.ts
@@ -21,7 +21,7 @@ ko.bindingHandlers.jqButton = {
 
 		var params: jqButtonParams = ko.unwrap(valueAccessor()) || {};
 		var allBindings = allBindingsAccessor();
-		var disable = ko.unwrap(allBindings.disable) || params.disabled;
+		var disable = ko.unwrap(allBindings.disable) || params.disabled || false;
 
 		$(element).button({ disabled: disable, icons: { primary: params.icon } });
 
@@ -30,7 +30,7 @@ ko.bindingHandlers.jqButton = {
 
 		var params: jqButtonParams = ko.unwrap(valueAccessor()) || {};
 		var allBindings = allBindingsAccessor();
-		var disable = ko.unwrap(allBindings.disable) || params.disabled;
+		var disable = ko.unwrap(allBindings.disable) || params.disabled || false;
 
 		$(element).button("option", "disabled", disable);
 			
