b89f40cc50adc250bbc3bfdf1fbca9ec174b9059
Riipah Furude <riipah@vocadb.net>
2017-04-09 15:04:12 +0300

    add links to images

---
+++ VocaDbModel/Domain/ReleaseEvents/EventType.cs
@@ -0,0 +1,25 @@
+﻿namespace VocaDb.Model.Domain.ReleaseEvents {
+
+	public enum EventType {
+		Unspecified = 0,
+		/// <summary>
+		/// Comiket, VoMas
+		/// </summary>
+		AlbumRelease = 1,
+		CharacterAnniversary = 2,
+		/// <summary>
+		/// Vocaloid Freak
+		/// </summary>
+		Club = 4,
+		/// <summary>
+		/// Magical Mirai
+		/// </summary>
+		Concert = 8,
+		/// <summary>
+		/// Nico Chokaigi
+		/// </summary>
+		Convention = 16,
+		Other = 32
+	}
+
+}
