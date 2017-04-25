330e9a2f988a0f9afba45ddb037b0d5e01712691
Riipah Furude <riipah@vocadb.net>
2017-04-25 22:10:49 +0300

    AllDeleteOrphan

--- VocaDbModel/Mapping/ReleaseEvents/ReleaseEventMap.cs
+++ VocaDbModel/Mapping/ReleaseEvents/ReleaseEventMap.cs
@@ -37,7 +37,7 @@ namespace VocaDb.Model.Mapping.ReleaseEvents {
 
 			Component(m => m.Names, c => {
 				c.Map(m => m.AdditionalNamesString).Not.Nullable().Length(1024);
-				c.HasMany(m => m.Names).Table("EventNames").KeyColumn("[Event]").Inverse().Cascade.All().Cache.ReadWrite();
+				c.HasMany(m => m.Names).Table("EventNames").KeyColumn("[Event]").Inverse().Cascade.AllDeleteOrphan().Cache.ReadWrite();
 				c.Component(m => m.SortNames, c2 => {
 					c2.Map(m => m.DefaultLanguage, "DefaultNameLanguage");
 					c2.Map(m => m.Japanese, "JapaneseName");
