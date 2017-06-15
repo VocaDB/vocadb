88c7e3f144e2e4725be51a6ca9caca85c7a82636
Riipah Furude <riipah@vocadb.net>
2017-06-15 21:56:52 +0300

    report tag version

---
+++ VocaDbWeb/Scripts/ViewModels/ArchivedEntryViewModel.ts
@@ -0,0 +1,28 @@
+﻿
+module vdb.viewModels {
+
+	import rep = repositories;
+
+	export class ArchivedEntryViewModel {
+		
+		constructor(entryId: number, versionNumber: number, private readonly repository: IEntryReportsRepository) {
+			
+			this.reportViewModel = new ReportEntryViewModel(null, (reportType, notes) => {
+
+				repository.createReport(entryId, reportType, notes, versionNumber);
+
+				vdb.ui.showSuccessMessage(vdb.resources.shared.reportSent);
+
+			}, { notesRequired: true, id: 'Other', name: null });
+
+		}
+
+		public reportViewModel: ReportEntryViewModel;
+
+	}
+
+	export interface IEntryReportsRepository {
+		createReport(entryId: number, reportType: string, notes: string, version?: number);
+	}
+
+} 
