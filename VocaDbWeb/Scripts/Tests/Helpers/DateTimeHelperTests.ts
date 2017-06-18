68f38d619f7149c786a6710ac8ad907bd9599f6f
Riipah Furude <riipah@vocadb.net>
2017-06-18 17:32:07 +0300

    another attempt to fix timezones for event edit...

---
+++ VocaDbWeb/Scripts/Tests/Helpers/DateTimeHelperTests.ts
@@ -0,0 +1,27 @@
+﻿
+module vdb.tests.helpers {
+
+	QUnit.module("DateTimeHelper");
+
+	QUnit.test("converToLocal", () => {
+
+		var date = new Date(2016, 9, 3);
+		date.setMinutes(date.getMinutes() - date.getTimezoneOffset());
+		var result = vdb.helpers.DateTimeHelper.converToLocal(date);
+		var expected = new Date(2016, 9, 3);
+
+		QUnit.equal(result.toString(), expected.toString());
+
+	});
+
+	QUnit.test("convertToUtc", () => {
+
+		var date = new Date(2016, 9, 3);
+		var result = vdb.helpers.DateTimeHelper.convertToUtc(date);
+		date.setMinutes(date.getMinutes() - date.getTimezoneOffset());
+
+		QUnit.equal(result.toString(), date.toString());
+
+	});
+
+}
