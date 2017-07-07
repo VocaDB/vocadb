2f845f52db150d53352c1e30e31e28c5fcd392dd
Riipah Furude <riipah@vocadb.net>
2017-07-07 22:22:36 +0300

    275: add class for venues

---
+++ VocaDbModel/Domain/ReleaseEvents/Venue.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VocaDb.Model.Domain.Globalization;
+
+namespace VocaDb.Model.Domain.ReleaseEvents {
+
+	public class Venue {
+
+		public virtual string Address { get; set; }
+
+		public virtual string Country { get; set; }
+
+		public virtual int Id { get; set; }
+
+		public virtual decimal Latitude { get; set; }
+
+		public virtual decimal Longitude { get; set; }
+
+		public virtual TranslatedString Name { get; set; }
+
+	}
+
+}
