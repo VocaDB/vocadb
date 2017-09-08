291ca9252e254f9a510007b6a0f4cfc56f130b38
riipah <riipah@vocadb.net>
2017-09-08 14:07:54 +0300

    added translations

--- VocaDbWeb/Resources/Views/Song/EditStrings.Designer.cs
+++ VocaDbWeb/Resources/Views/Song/EditStrings.Designer.cs
@@ -19,7 +19,7 @@ namespace ViewRes.Song {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class EditStrings {
@@ -192,6 +192,24 @@ namespace ViewRes.Song {
         public static string BaSongType {
             get {
                 return ResourceManager.GetString("BaSongType", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Use album release date.
+        /// </summary>
+        public static string BaUseAlbumDate {
+            get {
+                return ResourceManager.GetString("BaUseAlbumDate", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Use event date.
+        /// </summary>
+        public static string BaUseEventDate {
+            get {
+                return ResourceManager.GetString("BaUseEventDate", resourceCulture);
             }
         }
         
