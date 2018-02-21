34845fd432718c1fe9315067cf9646f757d937d9
riipah <riipah@vocadb.net>
2018-02-21 22:36:23 +0200

    allow overriding favicon

---
+++ VocaDbModel/Utils/Config/AssetsSection.cs
@@ -0,0 +1,15 @@
+﻿using System.Configuration;
+
+namespace VocaDb.Model.Utils.Config {
+
+	public class AssetsSection : ConfigurationSection {
+
+		[ConfigurationProperty("favIconUrl", DefaultValue = null)]
+		public string FavIconUrl {
+			get => (string)this["favIconUrl"];
+			set => this["favIconUrl"] = value;
+		}
+
+	}
+
+}
