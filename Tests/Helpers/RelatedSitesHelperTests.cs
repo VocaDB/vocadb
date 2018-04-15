1138eb8c982f42734933d7c88c25432bda7dec50
riipah <riipah@vocadb.net>
2018-04-15 21:49:44 +0300

    test related sites using regex

---
+++ Tests/Helpers/RelatedSitesHelperTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VocaDb.Model.Helpers;
+
+namespace VocaDb.Tests.Helpers {
+
+	[TestClass]
+	public class RelatedSitesHelperTests {
+
+		private void TestRelatedSite(bool expected, string url) {
+			var result = RelatedSitesHelper.IsRelatedSite(url);
+			Assert.AreEqual(expected, result, url);
+		}
+
+		[TestMethod]
+		public void IsRelatedSite_NoMatch() {
+			TestRelatedSite(false, "http://google.com");
+		}
+
+		[TestMethod]
+		public void IsRelatedSite_MatchHttp() {
+			TestRelatedSite(true, "http://vocadb.net/S/3939");
+		}
+
+		[TestMethod]
+		public void IsRelatedSite_MatchHttps() {
+			TestRelatedSite(true, "https://vocadb.net/S/3939");
+		}
+
+	}
+
+}
---
+++ VocaDbModel/Helpers/RelatedSitesHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace VocaDb.Model.Helpers {
+
+	public static class RelatedSitesHelper {
+		 
+		private static readonly Regex relatedSiteUrlRegex = new Regex(@"^https?://(?:(?:utaitedb\.net)|(?:vocadb\.net)|(?:touhoudb\.com))\/(?:(?:Song)\/Details|(?:S))\/\d+", RegexOptions.IgnoreCase);
+
+		public static bool IsRelatedSite(string url) {
+
+			if (string.IsNullOrEmpty(url))
+				return false;
+
+			return relatedSiteUrlRegex.IsMatch(url);
+
+		}
+
+	}
+
+}
