60f0f1f4726fd009e4789273cc2cfac839ccb2d5
Move-Forward <32796979+Move-Forward39@users.noreply.github.com>
2018-10-24 07:38:54 -0400

    Added vocaloidlyrics.fandom.com/wiki/

--- VocaDbWeb/Scripts/Shared/WebLinkMatcher.ts
+++ VocaDbWeb/Scripts/Shared/WebLinkMatcher.ts
@@ -89,6 +89,7 @@ module vdb.utils {
 			{ url: "vocadb.net/", desc: "VocaDB", cat: c.WebLinkCategory.Reference },
             { url: "vocaloiders.com/", desc: "Vocaloiders", cat: c.WebLinkCategory.Reference },
 			{ url: "vocaloidlyrics.wikia.com/wiki/", desc: "Vocaloid Lyrics Wiki", cat: c.WebLinkCategory.Reference },
+			{ url: "vocaloidlyrics.fandom.com/wiki/", desc: "Vocaloid Lyrics Wiki", cat: c.WebLinkCategory.Reference },
             { url: "vocaloid.wikia.com/wiki/", desc: "Vocaloid Wiki", cat: c.WebLinkCategory.Reference },
 			{ url: "voiceroid.wikia.com/wiki/", desc: "Voiceroid Wiki", cat: c.WebLinkCategory.Reference },
 			{ url: "cevio.wikia.com/wiki/", desc: "CeVIO Wiki", cat: c.WebLinkCategory.Reference },
