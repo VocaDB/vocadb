23f21ad0d708559648634cf0f90178b6889c4472
riipah <riipah@vocadb.net>
2019-02-09 10:56:49 +0200

    #70: fix null handling for title

--- VocaDbModel/DataContracts/Albums/AlbumReviewContract.cs
+++ VocaDbModel/DataContracts/Albums/AlbumReviewContract.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 using VocaDb.Model.DataContracts.Users;
 using VocaDb.Model.Domain.Albums;
 
@@ -6,9 +7,11 @@ namespace VocaDb.Model.DataContracts.Albums {
 
 	public class AlbumReviewContract {
 
-		public AlbumReviewContract() { }
+		public AlbumReviewContract() {
+			Title = string.Empty;
+		}
 
-		public AlbumReviewContract(AlbumReview review, IUserIconFactory userIconFactory) {
+		public AlbumReviewContract(AlbumReview review, IUserIconFactory userIconFactory) : this() {
 
 			if (review == null)
 				throw new ArgumentNullException(nameof(review));
@@ -33,6 +36,10 @@ namespace VocaDb.Model.DataContracts.Albums {
 
 		public virtual string Text { get; set; }
 
+		/// <summary>
+		/// See https://stackoverflow.com/a/9323209
+		/// </summary>
+		[DisplayFormat(ConvertEmptyStringToNull = false)]
 		public virtual string Title { get; set; }
 
 		public virtual UserForApiContract User { get; set; }
