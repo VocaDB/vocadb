1fb80b517134d9a41bd1f1d67ac1c1785f431361
riipah <riipah@vocadb.net>
2019-04-20 18:45:48 +0300

    update to DNF 4.8

--- VocaDbWeb/App_GlobalResources/AlbumArchiveReasonNames.Designer.cs
+++ VocaDbWeb/App_GlobalResources/AlbumArchiveReasonNames.Designer.cs
@@ -19,7 +19,7 @@ namespace Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option or rebuild the Visual Studio project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Web.Application.StronglyTypedResourceProxyBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Web.Application.StronglyTypedResourceProxyBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class AlbumArchiveReasonNames {
