22a1bdefe4aa5fbe231e80df4350b7ac6a1722e0
riipah <riipah@vocadb.net>
2019-11-20 18:23:12 +0200

    #527: add ReportUser button

--- VocaDbModel/Domain/Tags/Tag.cs
+++ VocaDbModel/Domain/Tags/Tag.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Xml.Linq;
@@ -431,6 +431,7 @@ namespace VocaDb.Model.Domain.Tags {
 		/// <summary>
 		/// List of all song tag usages (not including deleted songs) for this tag.
 		/// Warning: this list can be huge! Avoid traversing the list if possible.
+		/// The list exists mainly so that it can be queried with NHibernate.
 		/// </summary>
 		public virtual IEnumerable<SongTagUsage> SongTagUsages {
 			get {
