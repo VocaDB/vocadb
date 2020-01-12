6c9414f23e3bdd5d137fb72a0f0db49762a02936
ycanardeau <51428094+ycanardeau@users.noreply.github.com>
2020-01-12 21:45:54 +1000

    Update LaravelMixHelper

--- VocaDbWeb/Helpers/LaravelMixHelper.cs
+++ VocaDbWeb/Helpers/LaravelMixHelper.cs
@@ -1,13 +1,29 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Web;
 using System.Web.Optimization;
+using Newtonsoft.Json;
 
 namespace VocaDb.Web.Helpers {
 
 	public static class LaravelMixHelper {
 
-		public static IHtmlString RenderScripts(params string[] paths) => Scripts.Render(paths);
+		private static string GetPathToVersionedMixFile(string path) {
 
-		public static IHtmlString RenderStyles(params string[] paths) => Styles.Render(paths);
+			var manifestPath = HttpContext.Current.Server.MapPath("~/mix-manifest.json");
+			var json = File.ReadAllText(manifestPath);
+			var dict = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+
+			return dict[path];
+
+		}
+
+		private static string GetPath(string virtualPath) => VirtualPathUtility.ToAbsolute(virtualPath);
+
+		public static IHtmlString RenderScripts(params string[] paths) => Scripts.Render(paths.Select(p => GetPathToVersionedMixFile(GetPath(p))).ToArray());
+
+		public static IHtmlString RenderStyles(params string[] paths) => Styles.Render(paths.Select(p => GetPathToVersionedMixFile(GetPath(p))).ToArray());
 
 	}
 
