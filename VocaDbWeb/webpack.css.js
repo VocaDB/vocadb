ec098420743db581551c007ca5f6a5274c612901
ycanardeau <51428094+ycanardeau@users.noreply.github.com>
2020-01-12 18:27:03 +1000

    Update

--- VocaDbWeb/webpack.css.js
+++ VocaDbWeb/webpack.css.js
@@ -13,44 +13,19 @@ const mix = require('laravel-mix');
 
 // https://github.com/JeffreyWay/laravel-mix/issues/1914#issuecomment-503392761
 
+require('laravel-mix-merge-manifest');
+
 mix
+	.mergeManifest()
 	.options({
 		processCssUrls: false
 	})
 
 
 	// Base CSS
-	.less("Content/Styles/base.less", "Content/Styles", {
-		strictMath: true
-	})
-	.less("Content/Styles/PVViewer_Black.less", "Content/Styles")
-	.less("Content/Styles/ExtLinks.less", "Content/Styles")
-	.less("Content/Styles/Overrides.less", "Content/Styles")
-	.less("Content/Styles/Search.less", "Content/Styles")
-	.less("Content/Styles/song.less", "Content/Styles", {
-		strictMath: true
-	})
-	.less("Content/Styles/userpage.less", "Content/Styles")
-	.styles([
-		"Content/bootstrap.css",
-		"Content/bootstrap-responsive.css",
-		"Content/Site.css",
-		"Content/Styles/base.css",
-		//"Content/Styles/Snow2013.css",
-		"Content/Styles/PVViewer_Black.css",
-		"Content/Styles/ExtLinks.css",
-		"Content/Styles/Overrides.css",
-		"Content/Styles/StyleOverrides.css",
-		"Content/Styles/Search.css",
-		"Content/Styles/song.css",
-		"Content/Styles/userpage.css"
-	], "Content/css.css")
+	.less("Content/css.less", "Content")
 
-	.less("Content/Styles/embedSong.less", "Content/Styles")
-	.styles([
-		"Content/bootstrap.css",
-		"Content/Styles/embedSong.css"
-	], "Content/embedSong.css")
+	.less("Content/embedSong.less", "Content")
 
 	// CSS for jqxRating
 	.styles([
--- VocaDbWeb/webpack.mix.js
+++ VocaDbWeb/webpack.mix.js
@@ -11,7 +11,10 @@ const mix = require('laravel-mix');
  |
  */
 
+require('laravel-mix-merge-manifest');
+
 mix
+	.mergeManifest()
 	.webpackConfig({
 		output: {
 			library: 'app'
