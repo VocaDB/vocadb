e866fe305b92d09f9831774710b6f32ce6f2c9d6
ycanardeau <51428094+ycanardeau@users.noreply.github.com>
2020-02-16 17:46:19 +1000

    Remove hostAddress variable from GlobalValues

--- VocaDbWeb/Scripts/Shared/GlobalValues.ts
+++ VocaDbWeb/Scripts/Shared/GlobalValues.ts
@@ -4,9 +4,6 @@ module vdb.values {
     // URL of the site path, for example "/"
     export var baseAddress: string;
 
-    // URL including the scheme and site path, for example "http://vocadb.net/"
-	export var hostAddress: string;
-
 	// Whether the user is logged in.
 	export var isLoggedIn: boolean;
 
