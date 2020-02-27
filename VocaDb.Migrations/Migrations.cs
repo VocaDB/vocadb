0fcb21a4aa122fc0034233a501207bb66b40d555
ycanardeau <51428094+ycanardeau@users.noreply.github.com>
2020-02-27 21:41:17 +1000

    Update migration

--- VocaDb.Migrations/Migrations.cs
+++ VocaDb.Migrations/Migrations.cs
@@ -4,6 +4,66 @@ using FluentMigrator;
 namespace VocaDb.Migrations {
 
 	// Migration version format: YYYY_MM_DD_HHmm
+	
+	[Migration(2020_02_27_2100)]
+	public class EventVenues : AutoReversingMigration {
+	
+		public override void Up() {
+		
+			Create.Table("Venues")
+				.WithColumn("Id").AsInt32().NotNullable().PrimaryKey().Identity()
+				.WithColumn("Description").AsString(int.MaxValue).NotNullable().WithDefaultValue(string.Empty)
+				.WithColumn("Version").AsInt32().NotNullable().WithDefaultValue(0)
+				.WithColumn("Deleted").AsBoolean().NotNullable().WithDefaultValue(false)
+				.WithColumn("Status").AsString(10).NotNullable().WithDefaultValue("Draft")
+				.WithColumn("Address").AsString().NotNullable().WithDefaultValue(string.Empty)
+				.WithColumn("DefaultNameLanguage").AsString(20).NotNullable().WithDefaultValue("English")
+				.WithColumn("JapaneseName").AsString(255).NotNullable().WithDefaultValue(string.Empty)
+				.WithColumn("RomajiName").AsString(255).NotNullable().WithDefaultValue(string.Empty)
+				.WithColumn("EnglishName").AsString(255).NotNullable().WithDefaultValue(string.Empty)
+				.WithColumn("AdditionalNamesString").AsString(1024).NotNullable().WithDefaultValue(string.Empty)
+				.WithColumn("Latitude").AsDouble().Nullable()
+				.WithColumn("Longitude").AsDouble().Nullable();
+
+			Create.Table("ArchivedVenueVersions")
+				.WithColumn("Id").AsInt32().NotNullable().PrimaryKey().Identity()
+				.WithColumn("Author").AsInt32().NotNullable().ForeignKey(TableNames.Users, "Id")
+				.WithColumn("ChangedFields").AsAnsiString(100).NotNullable()
+				.WithColumn("CommonEditEvent").AsAnsiString(30).NotNullable()
+				.WithColumn("Created").AsDateTime().NotNullable()
+				.WithColumn("Data").AsXml().NotNullable()
+				.WithColumn("Venue").AsInt32().NotNullable().ForeignKey(TableNames.Venues, "Id").OnDelete(Rule.Cascade)
+				.WithColumn("IsSnapshot").AsBoolean().NotNullable().WithDefaultValue(true)
+				.WithColumn("Notes").AsString(200).NotNullable()
+				.WithColumn("Status").AsString(10).NotNullable().WithDefaultValue("Draft")
+				.WithColumn("Version").AsInt32().NotNullable();
+
+			Create.Table("VenueNames")
+				.WithColumn("Id").AsInt32().NotNullable().PrimaryKey().Identity()
+				.WithColumn("Venue").AsInt32().NotNullable().ForeignKey(TableNames.Venues, "Id").OnDelete(Rule.Cascade)
+				.WithColumn("Language").AsString(16).NotNullable()
+				.WithColumn("Value").AsString().NotNullable();
+
+			Create.Table("VenueWebLinks")
+				.WithColumn("Id").AsInt32().NotNullable().PrimaryKey().Identity()
+				.WithColumn("Category").AsAnsiString(20).NotNullable()
+				.WithColumn("Description").AsString(512).NotNullable()
+				.WithColumn("Venue").AsInt32().NotNullable().ForeignKey(TableNames.Venues, "Id").OnDelete(Rule.Cascade)
+				.WithColumn("Url").AsString(512).NotNullable();
+				
+			Alter.Table(TableNames.EntryReports).AddColumn("Venue").AsInt32().Nullable().ForeignKey(TableNames.Venues, "Id").OnDelete(Rule.Cascade);
+			
+			Create.Column("Venue").OnTable(TableNames.ActivityEntries).AsInt32().Nullable()
+				.ForeignKey(TableNames.Venues, "Id").OnDelete(Rule.Cascade);
+
+			Create.Column("ArchivedVenueVersion").OnTable(TableNames.ActivityEntries).AsInt32().Nullable()
+				.ForeignKey(TableNames.ArchivedVenueVersions, "Id").OnDelete(Rule.None);
+
+			Create.Column("VenueEntry").OnTable(TableNames.AlbumReleaseEvents).AsInt32().Nullable().ForeignKey(TableNames.Venues, "Id").OnDelete(Rule.SetNull);
+
+		}
+		
+	}
 
 	[Migration(2020_02_08_1800)]
 	public class IPRuleAddressUniqueIndex : AutoReversingMigration {
