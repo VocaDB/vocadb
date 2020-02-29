8631647abdd5c09950112af3de548e2f1da9b031
riipah <riipah@vocadb.net>
2020-02-29 14:35:11 +0200

    change to JSDoc comments

--- VocaDbWeb/Scripts/Shared/GlobalValues.ts
+++ VocaDbWeb/Scripts/Shared/GlobalValues.ts
@@ -1,15 +1,15 @@
 
 module vdb.values {
 
-    // URL of the site path, for example "/"
+    /** URL of the site path, for example "/" */
     export var baseAddress: string;
 
-	// Whether the user is logged in.
+	/** Whether the user is logged in. */
 	export var isLoggedIn: boolean;
 
 	export var languagePreference: number;
 
-	// UI language code, for example "en"
+	/** UI language code, for example "en" */
 	export var uiLanguage: string;
 
 }
