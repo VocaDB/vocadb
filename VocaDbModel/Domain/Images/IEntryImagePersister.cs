07c7f46e81265c2ed729052fc74a61ff26976bda
riipah <riipah@vocadb.net>
2020-04-11 13:22:21 +0300

    add some comments

--- VocaDbModel/Domain/Images/IEntryImagePersister.cs
+++ VocaDbModel/Domain/Images/IEntryImagePersister.cs
@@ -9,11 +9,14 @@ namespace VocaDb.Model.Domain.Images {
 	public interface IEntryImagePersister {
 
 		/// <summary>
-		/// Gets an absolute URL to an image.
+		/// Gets an absolute URL for internal and external access to an image.
 		/// </summary>
 		/// <param name="picture">Image information. Cannot be null.</param>
 		/// <param name="size">Image size.</param>
-		/// <returns>Absolute URL to the image, for example "http://static.vocadb.net/img/Album/Orig/Full/123.jpg"</returns>
+		/// <returns>
+		/// Absolute URL to the image, for example "https://static.vocadb.net/img/Album/Orig/Full/123.jpg".
+		/// Cannot be null, but not guaranteed to exist (use <see cref="HasImage"/> to make sure file exists).
+		/// </returns>
 		string GetUrlAbsolute(IEntryImageInformation picture, ImageSize size);
 
 		/// <summary>
@@ -51,8 +54,22 @@ namespace VocaDb.Model.Domain.Images {
 
 	}
 
+	/// <summary>
+	/// Extensions to <see cref="IEntryImagePersister"/>.
+	/// </summary>
 	public static class IEntryImagePersisterExtender {
 
+		/// <summary>
+		/// Gets absolute URL to image, optionally verifying that it exists.
+		/// </summary>
+		/// <param name="persister">Persister. Cannot be null.</param>
+		/// <param name="picture">Image information. Cannot be null.</param>
+		/// <param name="size">Image size.</param>
+		/// <param name="checkExists">If true, verify that the image exists. If false, no verification is done.</param>
+		/// <returns>
+		/// Absolute URL to image, if image exists or <paramref name="checkExists"/> is false. 
+		/// If <paramref name="checkExists"/> is true and image does not exist, this will be null.
+		/// </returns>
 		public static string GetUrlAbsolute(this IEntryImagePersister persister, IEntryImageInformation picture, ImageSize size, bool checkExists) {
 
 			if (checkExists && !persister.HasImage(picture, size))
