98f35dd292ff56aa99aa239067552e098af147a8
ycanardeau <51428094+ycanardeau@users.noreply.github.com>
2020-06-21 16:31:44 +1000

    Replace GlobalResourceProxyGenerator with PublicResXFileCodeGenerator

--- VocaDbWeb.Resources/Resources/Controllers/TagControllerStrings.Designer.cs
+++ VocaDbWeb.Resources/Resources/Controllers/TagControllerStrings.Designer.cs
@@ -22,7 +22,7 @@ namespace VocaDb.Web.Resources.Controllers {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class TagControllerStrings {
+    public class TagControllerStrings {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace VocaDb.Web.Resources.Controllers {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("VocaDb.Web.Resources.Controllers.TagControllerStrings", typeof(TagControllerStrings).Assembly);
@@ -51,7 +51,7 @@ namespace VocaDb.Web.Resources.Controllers {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
@@ -63,7 +63,7 @@ namespace VocaDb.Web.Resources.Controllers {
         /// <summary>
         ///   Looks up a localized string similar to Tag name may contain only word characters and cannot be empty..
         /// </summary>
-        internal static string TagNameError {
+        public static string TagNameError {
             get {
                 return ResourceManager.GetString("TagNameError", resourceCulture);
             }
