9c03a7e4d81699436c8a9df9063809646673ecfa
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2020-10-25 21:06:05 +1000

    Create ArtistIconLinkViewModel

---
+++ VocaDbWeb/Models/Shared/Partials/Artist/ArtistIconLinkViewModel.cs
@@ -0,0 +1,15 @@
+using VocaDb.Model.DataContracts.Artists;
+
+namespace VocaDb.Web.Models.Shared.Partials.Artist {
+
+	public class ArtistIconLinkViewModel {
+
+		public ArtistIconLinkViewModel(ArtistContract artist) {
+			Artist = artist;
+		}
+
+		public ArtistContract Artist { get; set; }
+
+	}
+
+}
