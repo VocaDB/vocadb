33a39728739ac60986d348a03c854299e9ba94d1
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2020-10-25 20:29:51 +1000

    Create PrintArchivedEventSeriesDataViewModel

---
+++ VocaDbWeb/Models/Shared/Partials/Event/PrintArchivedEventSeriesDataViewModel.cs
@@ -0,0 +1,15 @@
+using VocaDb.Model.DataContracts.ReleaseEvents;
+
+namespace VocaDb.Web.Models.Shared.Partials.Event {
+
+	public class PrintArchivedEventSeriesDataViewModel {
+
+		public PrintArchivedEventSeriesDataViewModel(ComparedEventSeriesContract comparedSeries) {
+			ComparedSeries = comparedSeries;
+		}
+
+		public ComparedEventSeriesContract ComparedSeries { get; set; }
+
+	}
+
+}
