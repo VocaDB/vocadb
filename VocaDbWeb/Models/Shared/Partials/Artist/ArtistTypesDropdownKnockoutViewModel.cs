4f8f141c9fce4ba4af87d1455568053487506932
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2020-10-26 22:06:13 +1000

    Create _ArtistTypesDropdownKnockout

---
+++ VocaDbWeb/Models/Shared/Partials/Artist/ArtistTypesDropdownKnockoutViewModel.cs
@@ -0,0 +1,13 @@
+namespace VocaDb.Web.Models.Shared.Partials.Artist {
+
+	public class ArtistTypesDropdownKnockoutViewModel {
+
+		public ArtistTypesDropdownKnockoutViewModel(string binding) {
+			Binding = binding;
+		}
+
+		public string Binding { get; set; }
+
+	}
+
+}
