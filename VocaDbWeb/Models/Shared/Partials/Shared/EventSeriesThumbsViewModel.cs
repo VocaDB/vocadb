9b72f99c4ab453584e74b9a6d123b0695785f55d
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2020-10-27 23:19:51 +1000

    Create _EventSeriesThumbs

---
+++ VocaDbWeb/Models/Shared/Partials/Shared/EventSeriesThumbsViewModel.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using VocaDb.Model.DataContracts.ReleaseEvents;
+
+namespace VocaDb.Web.Models.Shared.Partials.Shared {
+
+	public class EventSeriesThumbsViewModel {
+
+		public EventSeriesThumbsViewModel(IEnumerable<ReleaseEventSeriesContract> events) {
+			Events = events;
+		}
+
+		public IEnumerable<ReleaseEventSeriesContract> Events { get; set; }
+
+	}
+
+}
