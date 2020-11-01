5b8673ef6272a290f55f12d449408b6f77a370e4
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2020-11-01 20:34:30 +1000

    Create _LanguageFlag

---
+++ VocaDbWeb/Models/Shared/Partials/Html/LanguageFlagViewModel.cs
@@ -0,0 +1,13 @@
+namespace VocaDb.Web.Models.Shared.Partials.Html {
+
+	public class LanguageFlagViewModel {
+
+		public LanguageFlagViewModel(string languageCode) {
+			LanguageCode = languageCode;
+		}
+
+		public string LanguageCode { get; set; }
+
+	}
+
+}
