d2b2c1273550d695193b8939772b7e12bacea334
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2020-11-01 20:56:40 +1000

    Create _OpenGraphMetaTags

---
+++ VocaDbWeb/Models/Shared/Partials/Html/OpenGraphMetaTagsViewModel.cs
@@ -0,0 +1,15 @@
+using VocaDb.Web.Code;
+
+namespace VocaDb.Web.Models.Shared.Partials.Html {
+
+	public class OpenGraphMetaTagsViewModel {
+
+		public OpenGraphMetaTagsViewModel(PagePropertiesData pageProperties) {
+			PageProperties = pageProperties;
+		}
+
+		public PagePropertiesData PageProperties { get; set; }
+
+	}
+
+}
