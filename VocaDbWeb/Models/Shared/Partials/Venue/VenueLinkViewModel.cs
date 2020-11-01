37546b477de8c6c99bc5d5c6256f636dde056b4c
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2020-11-01 16:36:01 +1000

    Create _VenueLink

---
+++ VocaDbWeb/Models/Shared/Partials/Venue/VenueLinkViewModel.cs
@@ -0,0 +1,15 @@
+using VocaDb.Model.DataContracts.Venues;
+
+namespace VocaDb.Web.Models.Shared.Partials.Venue {
+
+	public class VenueLinkViewModel {
+
+		public VenueLinkViewModel(VenueForApiContract venue) {
+			Venue = venue;
+		}
+
+		public VenueForApiContract Venue { get; set; }
+
+	}
+
+}
