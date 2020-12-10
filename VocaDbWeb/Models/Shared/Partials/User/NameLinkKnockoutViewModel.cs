795798eed3adf3f048d095dfc1bfe88e9527cd72
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2020-12-10 23:33:12 +1000

    Create _NameLinkKnockout

---
+++ VocaDbWeb/Models/Shared/Partials/User/NameLinkKnockoutViewModel.cs
@@ -0,0 +1,12 @@
+namespace VocaDb.Web.Models.Shared.Partials.User
+{
+	public class NameLinkKnockoutViewModel
+	{
+		public NameLinkKnockoutViewModel(string userBinding)
+		{
+			UserBinding = userBinding;
+		}
+
+		public string UserBinding { get; set; }
+	}
+}
