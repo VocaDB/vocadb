5a20d921b5507bd5da667058fd35741005dbf61a
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2020-12-10 23:38:26 +1000

    Create _SongLinkKnockout

---
+++ VocaDbWeb/Models/Shared/Partials/Song/SongLinkKnockoutViewModel.cs
@@ -0,0 +1,21 @@
+namespace VocaDb.Web.Models.Shared.Partials.Song
+{
+	public class SongLinkKnockoutViewModel
+	{
+		public SongLinkKnockoutViewModel(string binding, string extUrlBinding = null, bool tooltip = false, string toolTipDomainBinding = null)
+		{
+			Binding = binding;
+			ExtUrlBinding = extUrlBinding;
+			Tooltip = tooltip;
+			ToolTipDomainBinding = toolTipDomainBinding;
+		}
+
+		public string Binding { get; set; }
+
+		public string ExtUrlBinding { get; set; }
+
+		public bool Tooltip { get; set; }
+
+		public string ToolTipDomainBinding { get; set; }
+	}
+}
