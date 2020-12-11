5a42aaa390b31e5b2b617b0bd1dc76a8fd55cb62
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2020-12-11 20:19:34 +1000

    Create _StarsMetaSpan

---
+++ VocaDbWeb/Models/Shared/Partials/Shared/StarsMetaSpanViewModel.cs
@@ -0,0 +1,15 @@
+namespace VocaDb.Web.Models.Shared.Partials.Shared
+{
+	public class StarsMetaSpanViewModel
+	{
+		public StarsMetaSpanViewModel(double current, int max)
+		{
+			Current = current;
+			Max = max;
+		}
+
+		public double Current { get; set; }
+
+		public int Max { get; set; }
+	}
+}
