5f31d9feaf10fa81b65e1ae499c3c6f72cb80bab
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2021-01-05 00:14:57 +1000

    Copy `ErrorController` from VocaDbWeb to VocaDbWeb.Core

--- VocaDbWeb.Core/Startup.cs
+++ VocaDbWeb.Core/Startup.cs
@@ -208,7 +208,7 @@ namespace VocaDb.Web
 			}
 			else
 			{
-				app.UseExceptionHandler("/Home/Error");
+				app.UseExceptionHandler("/Error");
 				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 				//app.UseHsts();
 			}
