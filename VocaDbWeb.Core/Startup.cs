f26213ed9fb9f98f8b0369f8bb935bdfbcf6ced2
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2021-01-12 00:55:12 +1000

    #598: UseStatusCodePagesWithRedirects

--- VocaDbWeb.Core/Startup.cs
+++ VocaDbWeb.Core/Startup.cs
@@ -209,6 +209,9 @@ namespace VocaDb.Web
 				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 				//app.UseHsts();
 			}
+
+			app.UseStatusCodePagesWithRedirects("/Error?code={0}");
+
 			app.UseHttpsRedirection();
 			app.UseStaticFiles();
 
