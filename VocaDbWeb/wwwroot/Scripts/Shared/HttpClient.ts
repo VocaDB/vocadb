7d00e110f913215c87d9efdaa669ec99743c7cda
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2021-05-04 17:09:34 +1000

    #860: Specify paramsSerializer

--- VocaDbWeb/wwwroot/Scripts/Shared/HttpClient.ts
+++ VocaDbWeb/wwwroot/Scripts/Shared/HttpClient.ts
@@ -7,7 +7,15 @@ export default class HttpClient {
   };
 
   public get = async <T>(url: string, data?: any): Promise<T> => {
-    const response = await axios.get<T>(url, { params: data });
+    const response = await axios.get<T>(url, {
+      params: data,
+      // HACK: This is required for advanced search filters.
+      paramsSerializer: (params) => {
+        // HACK: Removes undefined.
+        // Code from: https://stackoverflow.com/questions/286141/remove-blank-attributes-from-an-object-in-javascript/30386744#30386744
+        return $.param(JSON.parse(JSON.stringify(params)));
+      },
+    });
     return response.data;
   };
 
