690a1eddacf507083d1f3f343d92652fc82e6dce
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2021-05-29 11:31:04 +1000

    Convert QUnit tests to Jest

--- VocaDbWeb/Scripts/Tests/Helpers/DateTimeHelper.test.ts
+++ VocaDbWeb/Scripts/Tests/Helpers/DateTimeHelper.test.ts
@@ -1,32 +1,30 @@
 import DateTimeHelper from '@Helpers/DateTimeHelper';
 
-QUnit.module('DateTimeHelper');
-
-QUnit.test('convertToLocal', () => {
+test('convertToLocal', () => {
   var date = new Date(2016, 9, 3);
   date.setMinutes(date.getMinutes() - date.getTimezoneOffset());
   var result = DateTimeHelper.convertToLocal(date);
   var expected = new Date(2016, 9, 3);
 
-  QUnit.equal(result!.toString(), expected.toString());
+  expect(result!.toString()).toBe(expected.toString());
 });
 
-QUnit.test('convertToLocal null', () => {
+test('convertToLocal null', () => {
   var result = DateTimeHelper.convertToLocal(null!);
 
-  QUnit.equal(result, null, 'result');
+  expect(result, 'result').toBeNull();
 });
 
-QUnit.test('convertToUtc', () => {
+test('convertToUtc', () => {
   var date = new Date(2016, 9, 3);
   var result = DateTimeHelper.convertToUtc(date);
   date.setMinutes(date.getMinutes() - date.getTimezoneOffset());
 
-  QUnit.equal(result!.toString(), date.toString());
+  expect(result!.toString()).toBe(date.toString());
 });
 
-QUnit.test('convertToUtc null', () => {
+test('convertToUtc null', () => {
   var result = DateTimeHelper.convertToUtc(null!);
 
-  QUnit.equal(result, null, 'result');
+  expect(result, 'result').toBeNull();
 });
