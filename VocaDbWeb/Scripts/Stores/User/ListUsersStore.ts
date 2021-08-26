1bfe4535a9332dc6f6f2c04839581bc66ccb906d
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2021-08-26 23:45:40 +1000

    Create ListUsersRouteParams

--- VocaDbWeb/Scripts/Stores/User/ListUsersStore.ts
+++ VocaDbWeb/Scripts/Stores/User/ListUsersStore.ts
@@ -2,13 +2,30 @@ import UserApiContract from '@DataContracts/User/UserApiContract';
 import UserGroup from '@Models/Users/UserGroup';
 import UserRepository from '@Repositories/UserRepository';
 import ServerSidePagingStore from '@Stores/ServerSidePagingStore';
-import { makeObservable, observable, reaction, runInAction } from 'mobx';
+import {
+	computed,
+	makeObservable,
+	observable,
+	reaction,
+	runInAction,
+} from 'mobx';
 
 // Corresponds to the UserSortRule enum in C#.
 export enum UserSortRule {
 	RegisterDate = 'RegisterDate',
 	Name = 'Name',
 	Group = 'Group',
+}
+
+export interface ListUsersRouteParams {
+	disabledUsers?: boolean;
+	filter?: string;
+	groupId?: UserGroup;
+	knowsLanguage?: string;
+	onlyVerifiedArtists?: boolean;
+	page?: number;
+	pageSize?: number;
+	sort?: UserSortRule;
 }
 
 export default class ListUsersStore {
@@ -36,6 +53,29 @@ export default class ListUsersStore {
 		reaction(() => this.sort, this.updateResultsWithoutTotalCount);
 
 		this.updateResults(true);
+	}
+
+	@computed.struct public get routeParams(): ListUsersRouteParams {
+		return {
+			disabledUsers: this.disabledUsers,
+			filter: this.searchTerm,
+			groupId: this.group,
+			knowsLanguage: this.knowsLanguage,
+			onlyVerifiedArtists: this.onlyVerifiedArtists,
+			page: this.paging.page,
+			pageSize: this.paging.pageSize,
+			sort: this.sort,
+		};
+	}
+	public set routeParams(value: ListUsersRouteParams) {
+		this.disabledUsers = value.disabledUsers ?? false;
+		this.searchTerm = value.filter ?? '';
+		this.group = value.groupId ?? UserGroup.Nothing;
+		this.knowsLanguage = value.knowsLanguage ?? '';
+		this.onlyVerifiedArtists = value.onlyVerifiedArtists ?? false;
+		this.paging.page = value.page ?? 1;
+		this.paging.pageSize = value.pageSize ?? 20;
+		this.sort = value.sort ?? UserSortRule.RegisterDate;
 	}
 
 	private updateResults = (clearResults: boolean): void => {
