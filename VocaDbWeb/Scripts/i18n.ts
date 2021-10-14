a5c17943907ac5b22e56089a83232b443c4c1051
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2021-10-14 22:40:33 +1000

    Uninstall i18next-browser-languagedetector

--- VocaDbWeb/Scripts/i18n.ts
+++ VocaDbWeb/Scripts/i18n.ts
@@ -1,15 +1,13 @@
 import i18n from 'i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
 
 i18n
 	.use(Backend)
-	.use(LanguageDetector)
 	.use(initReactI18next)
 	.init({
 		lng: vdb.values.uiCulture,
-		load: 'languageOnly',
+		load: 'currentOnly',
 		fallbackLng: 'en',
 
 		interpolation: {
