799ef68390021e7e7e13935d3fa8f25999a059a4
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2022-01-22 10:48:30 +1000

    Update EntryPermissionManager

--- VocaDbModel/Domain/Security/EntryPermissionManager.cs
+++ VocaDbModel/Domain/Security/EntryPermissionManager.cs
@@ -225,9 +225,15 @@ namespace VocaDb.Model.Domain.Security
 			return permissionContext.HasPermission(PermissionToken.EditFeaturedLists);
 		}
 
-		public static bool CanRemoveTagUsages(IUserPermissionContext permissionContext, IEntryBase? entry)
+		private static bool CanEditTagsForEntry(IUserPermissionContext permissionContext, IEntryWithStatus entry) =>
+			permissionContext.HasPermission(PermissionToken.EditTags) && CanEdit(permissionContext, entry);
+
+		public static bool CanRemoveTagUsages(IUserPermissionContext permissionContext, IEntryWithStatus entry)
 		{
 			if (!permissionContext.IsLoggedIn)
+				return false;
+
+			if (!CanEditTagsForEntry(permissionContext, entry))
 				return false;
 
 			if (permissionContext.HasPermission(PermissionToken.RemoveTagUsages))
@@ -276,9 +282,6 @@ namespace VocaDb.Model.Domain.Security
 		{
 			VerifyAccess(permissionContext, entry, CanEdit);
 		}
-
-		private static bool CanEditTagsForEntry(IUserPermissionContext permissionContext, IEntryWithStatus entry) =>
-			permissionContext.HasPermission(PermissionToken.EditTags) && CanEdit(permissionContext, entry);
 
 		public static void VerifyEditTagsForEntry(IUserPermissionContext permissionContext, IEntryWithStatus entry) =>
 			VerifyAccess(permissionContext, entry, CanEditTagsForEntry);
