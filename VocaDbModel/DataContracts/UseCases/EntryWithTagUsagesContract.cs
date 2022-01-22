ce3e309976084f07e116a7f393ffc7bf9cd456f5
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2022-01-22 10:40:30 +1000

    Format

--- VocaDbModel/DataContracts/UseCases/EntryWithTagUsagesContract.cs
+++ VocaDbModel/DataContracts/UseCases/EntryWithTagUsagesContract.cs
@@ -2,7 +2,6 @@
 
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.Serialization;
 using VocaDb.Model.DataContracts.Tags;
 using VocaDb.Model.DataContracts.Users;
 using VocaDb.Model.Domain;
@@ -16,7 +15,13 @@ namespace VocaDb.Model.DataContracts.UseCases
 	{
 		public EntryWithTagUsagesContract() { }
 
-		public EntryWithTagUsagesContract(IEntryBase entry, IEnumerable<TagUsage> tagUsages, ContentLanguagePreference languagePreference, IUserPermissionContext userContext, IUserIconFactory userIconFactory)
+		public EntryWithTagUsagesContract(
+			IEntryBase entry,
+			IEnumerable<TagUsage> tagUsages,
+			ContentLanguagePreference languagePreference,
+			IUserPermissionContext userContext,
+			IUserIconFactory userIconFactory
+		)
 			: base(entry)
 		{
 			CanRemoveTagUsages = EntryPermissionManager.CanRemoveTagUsages(userContext, entry);
