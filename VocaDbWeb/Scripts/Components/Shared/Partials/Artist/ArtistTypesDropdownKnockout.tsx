51855e568f60aed7d438b8616ca589f801730539
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2022-01-23 11:09:55 +1000

    Improve ArtistTypesDropdownKnockout

--- VocaDbWeb/Scripts/Components/Shared/Partials/Artist/ArtistTypesDropdownKnockout.tsx
+++ VocaDbWeb/Scripts/Components/Shared/Partials/Artist/ArtistTypesDropdownKnockout.tsx
@@ -1,36 +1,43 @@
-import ArtistCategories from '@Models/Artists/ArtistCategories';
 import ArtistType from '@Models/Artists/ArtistType';
 import _ from 'lodash';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
+enum ArtistTypeGroup {
+	Nothing = 'Nothing',
+	Vocalist = 'Vocalist',
+	Producer = 'Producer',
+	Group = 'Group',
+	Other = 'Other',
+}
+
 // Corresponds to ArtistHelper.CategoriesForTypes in C#.
-const categoriesForTypes: Record<string, ArtistCategories> = {
-	[ArtistType[ArtistType.Animator]]: ArtistCategories.Animator,
-	[ArtistType[ArtistType.Character]]: ArtistCategories.Subject,
-	[ArtistType[ArtistType.Circle]]: ArtistCategories.Circle,
-	[ArtistType[ArtistType.Band]]: ArtistCategories.Band,
-	[ArtistType[ArtistType.Illustrator]]: ArtistCategories.Illustrator,
-	[ArtistType[ArtistType.Label]]: ArtistCategories.Label,
-	[ArtistType[ArtistType.Lyricist]]: ArtistCategories.Other,
-	[ArtistType[ArtistType.OtherGroup]]: ArtistCategories.Circle,
-	[ArtistType[ArtistType.OtherIndividual]]: ArtistCategories.Other,
-	[ArtistType[ArtistType.OtherVocalist]]: ArtistCategories.Vocalist,
-	[ArtistType[ArtistType.OtherVoiceSynthesizer]]: ArtistCategories.Vocalist,
-	[ArtistType[ArtistType.Producer]]: ArtistCategories.Producer,
-	[ArtistType[ArtistType.Unknown]]: ArtistCategories.Other,
-	[ArtistType[ArtistType.Utaite]]: ArtistCategories.Vocalist,
-	[ArtistType[ArtistType.UTAU]]: ArtistCategories.Vocalist,
-	[ArtistType[ArtistType.CeVIO]]: ArtistCategories.Vocalist,
-	[ArtistType[ArtistType.Vocaloid]]: ArtistCategories.Vocalist,
-	[ArtistType[ArtistType.Vocalist]]: ArtistCategories.Vocalist,
-	[ArtistType[ArtistType.SynthesizerV]]: ArtistCategories.Vocalist,
-	[ArtistType[ArtistType.CoverArtist]]: ArtistCategories.Producer,
+const categoriesForTypes: Record<string, ArtistTypeGroup> = {
+	[ArtistType[ArtistType.Animator]]: ArtistTypeGroup.Producer,
+	[ArtistType[ArtistType.Character]]: ArtistTypeGroup.Other,
+	[ArtistType[ArtistType.Circle]]: ArtistTypeGroup.Group,
+	[ArtistType[ArtistType.Band]]: ArtistTypeGroup.Group,
+	[ArtistType[ArtistType.Illustrator]]: ArtistTypeGroup.Producer,
+	[ArtistType[ArtistType.Label]]: ArtistTypeGroup.Group,
+	[ArtistType[ArtistType.Lyricist]]: ArtistTypeGroup.Other,
+	[ArtistType[ArtistType.OtherGroup]]: ArtistTypeGroup.Group,
+	[ArtistType[ArtistType.OtherIndividual]]: ArtistTypeGroup.Other,
+	[ArtistType[ArtistType.OtherVocalist]]: ArtistTypeGroup.Vocalist,
+	[ArtistType[ArtistType.OtherVoiceSynthesizer]]: ArtistTypeGroup.Vocalist,
+	[ArtistType[ArtistType.Producer]]: ArtistTypeGroup.Producer,
+	[ArtistType[ArtistType.Unknown]]: ArtistTypeGroup.Nothing,
+	[ArtistType[ArtistType.Utaite]]: ArtistTypeGroup.Vocalist,
+	[ArtistType[ArtistType.UTAU]]: ArtistTypeGroup.Vocalist,
+	[ArtistType[ArtistType.CeVIO]]: ArtistTypeGroup.Vocalist,
+	[ArtistType[ArtistType.Vocaloid]]: ArtistTypeGroup.Vocalist,
+	[ArtistType[ArtistType.Vocalist]]: ArtistTypeGroup.Vocalist,
+	[ArtistType[ArtistType.SynthesizerV]]: ArtistTypeGroup.Vocalist,
+	[ArtistType[ArtistType.CoverArtist]]: ArtistTypeGroup.Producer,
 };
 
 const artistTypeGroups = _.chain(vdb.values.artistTypes)
 	.orderBy((artistType) =>
-		Object.values(ArtistCategories).indexOf(categoriesForTypes[artistType]),
+		Object.values(ArtistTypeGroup).indexOf(categoriesForTypes[artistType]),
 	)
 	.groupBy((artistType) => categoriesForTypes[artistType])
 	.value();
@@ -47,15 +54,25 @@ const ArtistTypesDropdownKnockout = React.memo(
 
 		return (
 			<select {...props}>
-				{Object.entries(artistTypeGroups).map(([key, value]) => (
-					<optgroup label={key} key={key}>
-						{value.map((artistType) => (
-							<option value={artistType} key={artistType}>
-								{t(`VocaDb.Model.Resources:ArtistTypeNames.${artistType}`)}
-							</option>
-						))}
-					</optgroup>
-				))}
+				{Object.entries(artistTypeGroups).map(([key, value]) =>
+					key === ArtistTypeGroup.Nothing ? (
+						<>
+							{value.map((artistType) => (
+								<option value={artistType} key={artistType}>
+									{t(`VocaDb.Model.Resources:ArtistTypeNames.${artistType}`)}
+								</option>
+							))}
+						</>
+					) : (
+						<optgroup label={key} key={key}>
+							{value.map((artistType) => (
+								<option value={artistType} key={artistType}>
+									{t(`VocaDb.Model.Resources:ArtistTypeNames.${artistType}`)}
+								</option>
+							))}
+						</optgroup>
+					),
+				)}
 			</select>
 		);
 	},
