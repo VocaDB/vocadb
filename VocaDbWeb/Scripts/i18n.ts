174c04bccb73cfbd76110c64d6bfecf30f3fef5c
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2022-02-02 19:18:10 +1000

    Enable browser caching with local storage

--- VocaDbWeb/Scripts/i18n.ts
+++ VocaDbWeb/Scripts/i18n.ts
@@ -1,22 +1,31 @@
 import i18n from 'i18next';
-import Backend from 'i18next-http-backend';
+import ChainedBackend from 'i18next-chained-backend';
+import HttpBackend from 'i18next-http-backend';
+import LocalStorageBackend from 'i18next-localstorage-backend';
 import moment from 'moment';
 import { initReactI18next } from 'react-i18next';
 
 i18n
-	.use(Backend)
+	.use(ChainedBackend)
 	.use(initReactI18next)
 	.init({
 		lng: vdb.values.uiCulture,
 		load: 'currentOnly',
 		fallbackLng: 'en',
-
 		interpolation: {
 			escapeValue: false,
 		},
-
 		react: {
 			useSuspense: false,
+		},
+		backend: {
+			backends: [LocalStorageBackend, HttpBackend],
+			backendOptions: [
+				{
+					expirationTime: 7 * 24 * 60 * 60 * 1000, // 7 days
+				},
+				{},
+			],
 		},
 	});
 
