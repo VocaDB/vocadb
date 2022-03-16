70181bac01bf02f8991536294c30acbd3a951a77
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2022-03-16 23:53:25 +1000

    Create AppRoutes

--- VocaDbWeb/Scripts/App.tsx
+++ VocaDbWeb/Scripts/App.tsx
@@ -4,50 +4,9 @@ import Header from '@Components/Shared/Partials/Header';
 import LeftMenu from '@Components/Shared/Partials/LeftMenu';
 import React from 'react';
 import { Toaster } from 'react-hot-toast';
-import { Route, Routes } from 'react-router-dom';
 
+import AppRoutes from './AppRoutes';
 import ScrollToTop from './ScrollToTop';
-
-const ActivityEntryRoutes = React.lazy(
-	() => import('@Components/ActivityEntry/ActivityEntryRoutes'),
-);
-const AdminRoutes = React.lazy(() => import('@Components/Admin/AdminRoutes'));
-const AlbumRoutes = React.lazy(() => import('@Components/Album/AlbumRoutes'));
-const ArtistRoutes = React.lazy(
-	() => import('@Components/Artist/ArtistRoutes'),
-);
-const DiscussionRoutes = React.lazy(
-	() => import('@Components/Discussion/DiscussionRoutes'),
-);
-const EventRoutes = React.lazy(() => import('@Components/Event/EventRoutes'));
-const HelpRoutes = React.lazy(() => import('@Components/Help/HelpRoutes'));
-const HomeRoutes = React.lazy(() => import('@Components/Home/HomeRoutes'));
-const SearchRoutes = React.lazy(
-	() => import('@Components/Search/SearchRoutes'),
-);
-const SongListRoutes = React.lazy(
-	() => import('@Components/SongList/SongListRoutes'),
-);
-const SongRoutes = React.lazy(() => import('@Components/Song/SongRoutes'));
-const StatsRoutes = React.lazy(() => import('@Components/Stats/StatsRoutes'));
-const TagRoutes = React.lazy(() => import('@Components/Tag/TagRoutes'));
-const UserRoutes = React.lazy(() => import('@Components/User/UserRoutes'));
-const VenueRoutes = React.lazy(() => import('@Components/Venue/VenueRoutes'));
-
-const AlbumDetails = React.lazy(() => import('@Components/Album/AlbumDetails'));
-const ArtistDetails = React.lazy(
-	() => import('@Components/Artist/ArtistDetails'),
-);
-const EventDetails = React.lazy(() => import('@Components/Event/EventDetails'));
-const EventSeriesDetails = React.lazy(
-	() => import('@Components/Event/EventSeriesDetails'),
-);
-const SongDetails = React.lazy(() => import('@Components/Song/SongDetails'));
-const SongListDetails = React.lazy(
-	() => import('@Components/SongList/SongListDetails'),
-);
-const TagDetails = React.lazy(() => import('@Components/Tag/TagDetails'));
-const UserDetails = React.lazy(() => import('@Components/User/UserDetails'));
 
 const App = (): React.ReactElement => {
 	return (
@@ -62,36 +21,7 @@ const App = (): React.ReactElement => {
 
 					<div className="span10 rightFrame well">
 						<React.Suspense fallback={null /* TODO */}>
-							<Routes>
-								<Route
-									path="ActivityEntry/*"
-									element={<ActivityEntryRoutes />}
-								/>
-								<Route path="Admin/*" element={<AdminRoutes />} />
-								<Route path="Album/*" element={<AlbumRoutes />} />
-								<Route path="Artist/*" element={<ArtistRoutes />} />
-								<Route path="discussion/*" element={<DiscussionRoutes />} />
-								<Route path="Event/*" element={<EventRoutes />} />
-								<Route path="Help/*" element={<HelpRoutes />} />
-								<Route path="Search/*" element={<SearchRoutes />} />
-								<Route path="SongList/*" element={<SongListRoutes />} />
-								<Route path="Song/*" element={<SongRoutes />} />
-								<Route path="Stats/*" element={<StatsRoutes />} />
-								<Route path="Tag/*" element={<TagRoutes />} />
-								<Route path="User/*" element={<UserRoutes />} />
-								<Route path="Venue/*" element={<VenueRoutes />} />
-
-								<Route path="Al/:id/*" element={<AlbumDetails />} />
-								<Route path="Ar/:id/*" element={<ArtistDetails />} />
-								<Route path="E/:id/*" element={<EventDetails />} />
-								<Route path="Es/:id/*" element={<EventSeriesDetails />} />
-								<Route path="L/:id/*" element={<SongListDetails />} />
-								<Route path="S/:id/*" element={<SongDetails />} />
-								<Route path="T/:id/*" element={<TagDetails />} />
-								<Route path="Profile/:name/*" element={<UserDetails />} />
-
-								<Route path="*" element={<HomeRoutes />} />
-							</Routes>
+							<AppRoutes />
 						</React.Suspense>
 					</div>
 				</div>
