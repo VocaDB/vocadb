e2c0753d28520f0e7f666423a1df0828201a4d08
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2022-04-02 20:39:23 +1000

    Re-implement EmbedNiconico

--- VocaDbWeb/Scripts/Components/VdbPlayer/EmbedNiconico.tsx
+++ VocaDbWeb/Scripts/Components/VdbPlayer/EmbedNiconico.tsx
@@ -1,10 +1,9 @@
 import PVContract from '@DataContracts/PVs/PVContract';
-import $ from 'jquery';
+import qs from 'qs';
 import React from 'react';
 
 import IPVPlayer, { IPVPlayerOptions } from './IPVPlayer';
 import VdbPlayerConsole from './VdbPlayerConsole';
-import { getScript } from './getScript';
 
 declare namespace nico {
 	export interface NicoPlayer {
@@ -99,158 +98,44 @@ declare global {
 class PVPlayerNiconico implements IPVPlayer {
 	private static origin = 'https://embed.nicovideo.jp';
 
-	private player?: nico.NicoPlayer;
-
 	public constructor(
-		private readonly playerElementRef: React.MutableRefObject<HTMLDivElement>,
+		private readonly playerElementRef: React.MutableRefObject<HTMLIFrameElement>,
 		private readonly options: IPVPlayerOptions,
 	) {
 		VdbPlayerConsole.debug('PVPlayerNiconico.ctor');
 	}
 
-	private static scriptLoaded = false;
-	private static playerFactory?: nico.NicoPlayerFactory;
+	public load = async (pv: PVContract): Promise<void> => {
+		return new Promise((resolve, reject /* TODO: Reject. */) => {
+			VdbPlayerConsole.debug(
+				'PVPlayerNiconico.load',
+				JSON.parse(JSON.stringify(pv)),
+			);
 
-	private loadScript = (): Promise<void> => {
-		return new Promise(async (resolve, reject) => {
-			if (PVPlayerNiconico.scriptLoaded) {
-				VdbPlayerConsole.debug('Niconico script is already loaded');
+			const player = this.playerElementRef.current;
 
-				resolve();
-				return;
-			}
+			// Wait for iframe to load.
+			player.onload = (): void => {
+				player.onload = null;
 
-			window.onNicoPlayerFactoryReady = (factory): void => {
-				PVPlayerNiconico.playerFactory = factory;
-
-				VdbPlayerConsole.debug('Niconico player factory ready', factory);
-
-				window.addEventListener('message', (e: nico.PlayerEvent) => {
-					if (e.origin !== PVPlayerNiconico.origin) return;
-
-					switch (e.data.eventName) {
-						case 'playerStatusChange':
-							VdbPlayerConsole.debug(
-								`Niconico player status changed: ${e.data.data.playerStatus}`,
-							);
-
-							switch (e.data.data.playerStatus) {
-								case nico.PlayerStatus.Play:
-									this.options.onPlay?.();
-									break;
-
-								case nico.PlayerStatus.Pause:
-									this.options.onPause?.();
-									break;
-
-								case nico.PlayerStatus.End:
-									this.options.onEnded?.();
-									break;
-							}
-							break;
-
-						case 'statusChange':
-							VdbPlayerConsole.debug(
-								`Niconico status changed: ${e.data.data.playerStatus}`,
-							);
-							break;
-
-						case 'playerMetadataChange':
-							break;
-
-						case 'loadComplete':
-							VdbPlayerConsole.debug('Niconico load completed');
-
-							// TODO: Implement.
-							break;
-
-						case 'error':
-							// TODO: Implement.
-
-							this.options.onError?.(e.data);
-							break;
-
-						case 'player-error:video:play':
-						case 'player-error:video:seek':
-							this.options.onError?.(e.data);
-							break;
-
-						default:
-							VdbPlayerConsole.warn(
-								'Niconico message',
-								e.data.eventName,
-								e.data.data,
-							);
-							break;
-					}
-				});
+				VdbPlayerConsole.debug('Niconico iframe loaded');
 
 				resolve();
 			};
 
-			try {
-				VdbPlayerConsole.debug('Loading Niconico script...');
-
-				await getScript('https://static.vocadb.net/script/nico/api.js');
-
-				PVPlayerNiconico.scriptLoaded = true;
-
-				VdbPlayerConsole.debug('Niconico script loaded');
-			} catch {
-				VdbPlayerConsole.error('Failed to load Niconico script');
-
-				reject();
-			}
+			player.src = `https://embed.nicovideo.jp/watch/${pv.pvId}?${qs.stringify({
+				jsapi: 1,
+				playerId: 1,
+			})}`;
 		});
-	};
-
-	public load = async (pv: PVContract): Promise<void> => {
-		VdbPlayerConsole.debug(
-			'PVPlayerNiconico.load',
-			JSON.parse(JSON.stringify(pv)),
-		);
-
-		await this.loadScript();
-
-		$(this.playerElementRef.current).empty();
-
-		VdbPlayerConsole.assert(
-			!!PVPlayerNiconico.playerFactory,
-			'Niconico playerFactory is not defined',
-		);
-		if (!PVPlayerNiconico.playerFactory) return;
-
-		this.player = await PVPlayerNiconico.playerFactory.create(
-			this.playerElementRef.current,
-			pv.pvId,
-		);
-
-		VdbPlayerConsole.assert(
-			!!this.player.iframeElement,
-			'player.iframeElement is not defined',
-		);
-
-		this.player.iframeElement.width = '100%';
-		this.player.iframeElement.height = '100%';
-
-		// As of Chrome 66, videos must be muted in order to play automatically.
-		// See also https://github.com/cookpete/react-player/blob/137f1fc3569fc56e14640df7c5c9af1ceba7f992/README.md#autoplay and https://kiite.jp/faq#browser-setting.
-		this.mute();
-	};
-
-	private assertPlayerAttached = (): void => {
-		VdbPlayerConsole.assert(!!this.player, 'Niconico player is not attached');
 	};
 
 	// Code from: https://blog.hayu.io/web/create/nicovideo-embed-player-api/.
 	private postMessage = (message: any): void => {
-		this.assertPlayerAttached();
-		if (!this.player) return;
-
-		this.player.iframeElement.contentWindow?.postMessage(
+		this.playerElementRef.current.contentWindow?.postMessage(
 			{
 				...message,
-				playerId: this.player.playerId,
+				playerId: '1' /* Needs to be a string, not a number. */,
 				sourceConnectorType: 1,
 			},
 			PVPlayerNiconico.origin,
@@ -260,26 +145,17 @@ class PVPlayerNiconico implements IPVPlayer {
 	public play = (): void => {
 		VdbPlayerConsole.debug('PVPlayerNiconico.play');
 
-		this.assertPlayerAttached();
-		if (!this.player) return;
-
-		this.player.play();
+		this.postMessage({ eventName: 'play' });
 	};
 
 	public pause = (): void => {
 		VdbPlayerConsole.debug('PVPlayerNiconico.pause');
 
-		this.assertPlayerAttached();
-		if (!this.player) return;
-
-		this.player.pause();
+		this.postMessage({ eventName: 'pause' });
 	};
 
 	public seekTo = (seconds: number): void => {
 		VdbPlayerConsole.debug('PVPlayerNiconico.seekTo');
-
-		this.assertPlayerAttached();
-		if (!this.player) return;
 
 		this.postMessage({ eventName: 'seek', data: { time: seconds * 1000 } });
 	};
@@ -311,14 +187,29 @@ const EmbedNiconico = React.memo(
 	({ playerRef, ...options }: EmbedNiconicoProps): React.ReactElement => {
 		VdbPlayerConsole.debug('EmbedNiconico');
 
-		const playerElementRef = React.useRef<HTMLDivElement>(undefined!);
+		const playerElementRef = React.useRef<HTMLIFrameElement>(undefined!);
 
 		React.useEffect(() => {
 			playerRef.current = new PVPlayerNiconico(playerElementRef, options);
 		}, [playerRef, options]);
 
 		return (
-			<div ref={playerElementRef} style={{ width: '100%', height: '100%' }} />
+			<div style={{ width: '100%', height: '100%' }}>
+				{/* eslint-disable-next-line jsx-a11y/iframe-has-title */}
+				<iframe
+					ref={playerElementRef}
+					width="100%"
+					height="100%"
+					allowFullScreen
+					style={{ border: 'none' }}
+					// The player has to have the allow="autoplay" attribute.
+					// Otherwise it throws a NotAllowedError: "play() failed because the user didn't interact with the document first".
+					// See also: https://github.com/vimeo/player.js/issues/389.
+					// NOTE: An iframe element created by `PVPlayerNiconico.playerFactory.create` doesn't have the allow="autoplay" attribute,
+					// which causes the above issue when trying to autoplay a video.
+					allow="autoplay; fullscreen"
+				/>
+			</div>
 		);
 	},
 );
