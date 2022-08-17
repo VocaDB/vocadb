177bb2959d008b7c9b44480894dd2355659af2e5
Ekaterina Biryukova <kateabr@yandex.ru>
2022-08-17 20:59:55 +0300

    changed the namespace and added a test

---
+++ Tests/Domain/ReleaseEvents/ReleaseEventTypesTests.cs
@@ -0,0 +1,21 @@
+using VocaDb.Model.Domain.ReleaseEvents;
+
+namespace VocaDb.Tests.Domain.ReleaseEvents;
+
+[TestClass]
+public class ArtistEventTypesTests
+{
+	[DataRow(0, EventCategory.Unspecified)]
+	[DataRow(1, EventCategory.AlbumRelease)]
+	[DataRow(2, EventCategory.Anniversary)]
+	[DataRow(4, EventCategory.Club)]
+	[DataRow(8, EventCategory.Concert)]
+	[DataRow(16, EventCategory.Contest)]
+	[DataRow(32, EventCategory.Convention)]
+	[DataRow(64, EventCategory.Festival)]
+	[TestMethod]
+	public void Value(int expected, EventCategory actual)
+	{
+		((int) actual).Should().Be(expected);
+	}
+}
--- VocaDbModel/Domain/ReleaseEvents/EventCategory.cs
+++ VocaDbModel/Domain/ReleaseEvents/EventCategory.cs
@@ -1,32 +1,34 @@
-#nullable disable
+namespace VocaDb.Model.Domain.ReleaseEvents;
 
-namespace VocaDb.Model.Domain.ReleaseEvents
+public enum EventCategory
 {
-	public enum EventCategory
-	{
-		Unspecified = 0,
-		/// <summary>
-		/// Comiket, VoMas
-		/// </summary>
-		AlbumRelease = 1 << 0,
-		Anniversary = 1 << 1,
-		/// <summary>
-		/// Vocaloid Freak
-		/// </summary>
-		Club = 1 << 2,
-		/// <summary>
-		/// Magical Mirai
-		/// </summary>
-		Concert = 1 << 3,
-		Contest = 1 << 4,
-		/// <summary>
-		/// Nico Chokaigi
-		/// </summary>
-		Convention = 1 << 5,
-		Other = 1 << 6,
-		/// <summary>
-		/// J-POP Cover Festival
-		/// </summary>
-		Festival = 1 << 7,
-	}
+	Unspecified = 0,
+
+	/// <summary>
+	/// Comiket, VoMas
+	/// </summary>
+	AlbumRelease = 1 << 0,
+	Anniversary = 1 << 1,
+
+	/// <summary>
+	/// Vocaloid Freak
+	/// </summary>
+	Club = 1 << 2,
+
+	/// <summary>
+	/// Magical Mirai
+	/// </summary>
+	Concert = 1 << 3,
+	Contest = 1 << 4,
+
+	/// <summary>
+	/// Nico Chokaigi
+	/// </summary>
+	Convention = 1 << 5,
+	Other = 1 << 6,
+
+	/// <summary>
+	/// J-POP Cover Festival
+	/// </summary>
+	Festival = 1 << 7,
 }
