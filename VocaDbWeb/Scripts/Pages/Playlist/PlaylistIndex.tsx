4e2a2300208483de0164112a836ac4121a77abc1
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2022-08-21 12:23:32 +1000

    Play queue

--- VocaDbWeb/Scripts/Pages/Playlist/PlaylistIndex.tsx
+++ VocaDbWeb/Scripts/Pages/Playlist/PlaylistIndex.tsx
@@ -1,16 +1,40 @@
 import { Layout } from '@/Components/Shared/Layout';
+import { useVdbPlayer } from '@/Components/VdbPlayer/VdbPlayerContext';
 import { useVocaDbTitle } from '@/Components/useVocaDbTitle';
+import { EntryUrlMapper } from '@/Shared/EntryUrlMapper';
+import { observer } from 'mobx-react-lite';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
 
-const PlaylistIndex = (): React.ReactElement => {
-	const { t, ready } = useTranslation(['ViewRes.Search']);
+const PlaylistIndex = observer(
+	(): React.ReactElement => {
+		const { t, ready } = useTranslation(['ViewRes.Search']);
 
-	const title = t('ViewRes.Search:Index.Playlist');
+		const title = t('ViewRes.Search:Index.Playlist');
 
-	useVocaDbTitle(title, ready);
+		useVocaDbTitle(title, ready);
 
-	return <Layout title={title}></Layout>;
-};
+		const vdbPlayer = useVdbPlayer();
+
+		return (
+			<Layout title={title}>
+				<table>
+					<tbody>
+						{vdbPlayer.playQueue.items.map((item) => (
+							<tr key={item.id}>
+								<td>
+									<Link to={EntryUrlMapper.details_entry(item.entry)}>
+										{item.entry.name}
+									</Link>
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</Layout>
+		);
+	},
+);
 
 export default PlaylistIndex;
