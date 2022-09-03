4515d3bbe5925cfaf1ccff4bea68b5744caab1d9
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2022-09-03 10:29:58 +1000

    Add buttons to ThumbItem

--- VocaDbWeb/Scripts/Components/Shared/Partials/Shared/ThumbItem.tsx
+++ VocaDbWeb/Scripts/Components/Shared/Partials/Shared/ThumbItem.tsx
@@ -1,5 +1,6 @@
 import { BsPrefixRefForwardingComponent } from '@/Bootstrap/helpers';
 import { EntryToolTip } from '@/Components/KnockoutExtensions/EntryToolTip';
+import { EmbedPVPreviewButtons } from '@/Components/Shared/Partials/PV/EmbedPVPreview';
 import { EntryRefContract } from '@/DataContracts/EntryRefContract';
 import React from 'react';
 
@@ -22,35 +23,49 @@ export const ThumbItem: BsPrefixRefForwardingComponent/* TODO */ <
 	tooltip,
 	...props
 }: ThumbItemProps): React.ReactElement => {
+	const handlePlay = React.useCallback(() => {}, []);
+
+	const handlePlayNext = React.useCallback(() => {}, []);
+
+	const handleAddToPlayQueue = React.useCallback(() => {}, []);
+
 	return (
-		<div>
-			<Component {...props}>
-				<div className="pictureFrame">
-					{entry ? (
-						tooltip ? (
-							<EntryToolTip
-								as="img"
-								src={thumbUrl}
-								alt="Preview" /* TODO: localize */
-								className="coverPic"
-								value={entry}
-							/>
+		<div css={{ marginRight: 9 }}>
+			<div css={{ position: 'relative' }}>
+				<Component {...props}>
+					<div className="pictureFrame">
+						{entry ? (
+							tooltip ? (
+								<EntryToolTip
+									as="img"
+									src={thumbUrl}
+									alt="Preview" /* TODO: localize */
+									className="coverPic"
+									value={entry}
+								/>
+							) : (
+								<img
+									src={thumbUrl}
+									alt="Preview" /* TODO: localize */
+									className="coverPic"
+								/>
+							)
 						) : (
 							<img
 								src={thumbUrl}
 								alt="Preview" /* TODO: localize */
 								className="coverPic"
 							/>
-						)
-					) : (
-						<img
-							src={thumbUrl}
-							alt="Preview" /* TODO: localize */
-							className="coverPic"
-						/>
-					)}
-				</div>
-			</Component>
+						)}
+					</div>
+				</Component>
+
+				<EmbedPVPreviewButtons
+					onPlay={handlePlay}
+					onPlayNext={handlePlayNext}
+					onAddToPlayQueue={handleAddToPlayQueue}
+				/>
+			</div>
 			{caption && <span>{caption}</span>}
 		</div>
 	);
