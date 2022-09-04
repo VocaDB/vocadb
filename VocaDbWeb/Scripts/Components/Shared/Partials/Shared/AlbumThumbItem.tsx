390112c24381692faa410b1bed4c0074f6978803
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2022-09-04 16:34:11 +1000

    Replace onMouseEnter and onMouseLeave with onPointerEnter and onPointerLeave, respectively

--- VocaDbWeb/Scripts/Components/Shared/Partials/Shared/AlbumThumbItem.tsx
+++ VocaDbWeb/Scripts/Components/Shared/Partials/Shared/AlbumThumbItem.tsx
@@ -54,8 +54,8 @@ export const AlbumThumbItem = React.memo(
 				caption={album.name}
 				entry={{ entryType: EntryType[EntryType.Album], id: album.id }}
 				tooltip={tooltip}
-				onMouseEnter={(): void => setHover(true)}
-				onMouseLeave={(): void => setHover(false)}
+				onPointerEnter={(): void => setHover(true)}
+				onPointerLeave={(): void => setHover(false)}
 			>
 				{(hover || isOpen) && (
 					<EmbedPVPreviewButtons
