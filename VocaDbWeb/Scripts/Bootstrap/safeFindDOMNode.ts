38c600a607ebdf8b5ed291186044440b90da1472
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2022-09-15 19:15:47 +1000

    Create OverlayTrigger

---
+++ VocaDbWeb/Scripts/Bootstrap/safeFindDOMNode.ts
@@ -0,0 +1,11 @@
+// Code from: https://github.com/react-bootstrap/react-bootstrap/blob/33f037ba1e9870463f1bd33a4fe66b8e2a7586f6/src/safeFindDOMNode.ts.
+import ReactDOM from 'react-dom';
+
+export default function safeFindDOMNode(
+	componentOrElement: React.ComponentClass | Element | null | undefined,
+): Element | Text | null {
+	if (componentOrElement && 'setState' in componentOrElement) {
+		return ReactDOM.findDOMNode(componentOrElement);
+	}
+	return (componentOrElement ?? null) as Element | Text | null;
+}
