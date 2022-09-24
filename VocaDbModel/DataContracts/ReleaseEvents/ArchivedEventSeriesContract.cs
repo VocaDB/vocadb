8b28996fe016ae3e2c5bc358d51cf98ffb86ac9a
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2022-09-24 14:11:01 +1000

    Use file scoped namespaces

--- VocaDbModel/DataContracts/ReleaseEvents/ArchivedEventSeriesContract.cs
+++ VocaDbModel/DataContracts/ReleaseEvents/ArchivedEventSeriesContract.cs
@@ -2,84 +2,83 @@ using System.Runtime.Serialization;
 using VocaDb.Model.Domain.ReleaseEvents;
 using VocaDb.Model.Utils;
 
-namespace VocaDb.Model.DataContracts.ReleaseEvents
+namespace VocaDb.Model.DataContracts.ReleaseEvents;
+
+[DataContract(Namespace = Schemas.VocaDb)]
+public class ArchivedEventSeriesContract
 {
-	[DataContract(Namespace = Schemas.VocaDb)]
-	public class ArchivedEventSeriesContract
+	private static void DoIfExists(
+		ArchivedReleaseEventSeriesVersion version,
+		ReleaseEventSeriesEditableFields field,
+		XmlCache<ArchivedEventSeriesContract> xmlCache,
+		Action<ArchivedEventSeriesContract> func
+	)
 	{
-		private static void DoIfExists(
-			ArchivedReleaseEventSeriesVersion version,
-			ReleaseEventSeriesEditableFields field,
-			XmlCache<ArchivedEventSeriesContract> xmlCache,
-			Action<ArchivedEventSeriesContract> func
-		)
+		var versionWithField = version.GetLatestVersionWithField(field);
+
+		if (versionWithField?.Data != null)
 		{
-			var versionWithField = version.GetLatestVersionWithField(field);
-
-			if (versionWithField?.Data != null)
-			{
-				var data = xmlCache.Deserialize(versionWithField.Version, versionWithField.Data);
-				func(data);
-			}
+			var data = xmlCache.Deserialize(versionWithField.Version, versionWithField.Data);
+			func(data);
 		}
+	}
 
-		public static ArchivedEventSeriesContract GetAllProperties(ArchivedReleaseEventSeriesVersion version)
-		{
-			var data = new ArchivedEventSeriesContract();
-			var xmlCache = new XmlCache<ArchivedEventSeriesContract>();
-			var thisVersion = version.Data != null ? xmlCache.Deserialize(version.Version, version.Data) : new ArchivedEventSeriesContract();
+	public static ArchivedEventSeriesContract GetAllProperties(ArchivedReleaseEventSeriesVersion version)
+	{
+		var data = new ArchivedEventSeriesContract();
+		var xmlCache = new XmlCache<ArchivedEventSeriesContract>();
+		var thisVersion = version.Data != null ? xmlCache.Deserialize(version.Version, version.Data) : new ArchivedEventSeriesContract();
 
-			data.Category = thisVersion.Category;
-			data.Description = thisVersion.Description;
-			data.Id = thisVersion.Id;
-			data.MainPictureMime = thisVersion.MainPictureMime;
-			data.TranslatedName = thisVersion.TranslatedName;
+		data.Category = thisVersion.Category;
+		data.Description = thisVersion.Description;
+		data.Id = thisVersion.Id;
+		data.MainPictureMime = thisVersion.MainPictureMime;
+		data.TranslatedName = thisVersion.TranslatedName;
 
-			DoIfExists(version, ReleaseEventSeriesEditableFields.Names, xmlCache, v => data.Names = v.Names);
-			DoIfExists(version, ReleaseEventSeriesEditableFields.WebLinks, xmlCache, v => data.WebLinks = v.WebLinks);
+		DoIfExists(version, ReleaseEventSeriesEditableFields.Names, xmlCache, v => data.Names = v.Names);
+		DoIfExists(version, ReleaseEventSeriesEditableFields.WebLinks, xmlCache, v => data.WebLinks = v.WebLinks);
 
-			return data;
-		}
+		return data;
+	}
 
 #nullable disable
-		public ArchivedEventSeriesContract() { }
+	public ArchivedEventSeriesContract() { }
 #nullable enable
 
-		public ArchivedEventSeriesContract(ReleaseEventSeries series, ReleaseEventSeriesDiff diff)
-		{
-			ParamIs.NotNull(() => series);
+	public ArchivedEventSeriesContract(ReleaseEventSeries series, ReleaseEventSeriesDiff diff)
+	{
+		ParamIs.NotNull(() => series);
 
-			Category = series.Category;
-			Description = series.Description;
-			Id = series.Id;
-			MainPictureMime = series.PictureMime;
-			Names = diff.IncludeNames ? series.Names.Names.Select(n => new LocalizedStringContract(n)).ToArray() : null;
-			TranslatedName = new ArchivedTranslatedStringContract(series.TranslatedName);
-			WebLinks = diff.IncludeWebLinks ? series.WebLinks.Select(l => new ArchivedWebLinkContract(l)).ToArray() : null;
-		}
-
-		[DataMember]
-		public string[]? Aliases { get; init; }
-
-		[DataMember]
-		public EventCategory Category { get; set; }
-
-		[DataMember]
-		public string Description { get; set; }
-
-		[DataMember]
-		public int Id { get; set; }
-
-		[DataMember]
-		public string MainPictureMime { get; set; }
-
-		[DataMember]
-		public LocalizedStringContract[]? Names { get; set; }
-
-		[DataMember]
-		public ArchivedTranslatedStringContract TranslatedName { get; set; }
-
-		[DataMember]
-		public ArchivedWebLinkContract[]? WebLinks { get; set; }
+		Category = series.Category;
+		Description = series.Description;
+		Id = series.Id;
+		MainPictureMime = series.PictureMime;
+		Names = diff.IncludeNames ? series.Names.Names.Select(n => new LocalizedStringContract(n)).ToArray() : null;
+		TranslatedName = new ArchivedTranslatedStringContract(series.TranslatedName);
+		WebLinks = diff.IncludeWebLinks ? series.WebLinks.Select(l => new ArchivedWebLinkContract(l)).ToArray() : null;
 	}
+
+	[DataMember]
+	public string[]? Aliases { get; init; }
+
+	[DataMember]
+	public EventCategory Category { get; set; }
+
+	[DataMember]
+	public string Description { get; set; }
+
+	[DataMember]
+	public int Id { get; set; }
+
+	[DataMember]
+	public string MainPictureMime { get; set; }
+
+	[DataMember]
+	public LocalizedStringContract[]? Names { get; set; }
+
+	[DataMember]
+	public ArchivedTranslatedStringContract TranslatedName { get; set; }
+
+	[DataMember]
+	public ArchivedWebLinkContract[]? WebLinks { get; set; }
 }
--- VocaDbModel/DataContracts/Tags/ArchivedTagContract.cs
+++ VocaDbModel/DataContracts/Tags/ArchivedTagContract.cs
@@ -2,111 +2,110 @@ using System.Runtime.Serialization;
 using VocaDb.Model.Domain.Tags;
 using VocaDb.Model.Utils;
 
-namespace VocaDb.Model.DataContracts.Tags
+namespace VocaDb.Model.DataContracts.Tags;
+
+[DataContract(Namespace = Schemas.VocaDb)]
+public class ArchivedTagContract
 {
-	[DataContract(Namespace = Schemas.VocaDb)]
-	public class ArchivedTagContract
+	private static void DoIfExists(
+		ArchivedTagVersion version,
+		TagEditableFields field,
+		XmlCache<ArchivedTagContract> xmlCache,
+		Action<ArchivedTagContract> func
+	)
 	{
-		private static void DoIfExists(
-			ArchivedTagVersion version,
-			TagEditableFields field,
-			XmlCache<ArchivedTagContract> xmlCache,
-			Action<ArchivedTagContract> func
-		)
+		var versionWithField = version.GetLatestVersionWithField(field);
+
+		if (versionWithField?.Data != null)
 		{
-			var versionWithField = version.GetLatestVersionWithField(field);
-
-			if (versionWithField?.Data != null)
-			{
-				var data = xmlCache.Deserialize(versionWithField.Version, versionWithField.Data);
-				func(data);
-			}
+			var data = xmlCache.Deserialize(versionWithField.Version, versionWithField.Data);
+			func(data);
 		}
+	}
 
-		public static ArchivedTagContract GetAllProperties(ArchivedTagVersion version)
+	public static ArchivedTagContract GetAllProperties(ArchivedTagVersion version)
+	{
+		var data = new ArchivedTagContract();
+		var xmlCache = new XmlCache<ArchivedTagContract>();
+		var thisVersion = version.Data != null ? xmlCache.Deserialize(version.Version, version.Data) : new ArchivedTagContract();
+
+		data.CategoryName = thisVersion.CategoryName;
+		data.HideFromSuggestions = thisVersion.HideFromSuggestions;
+		data.Id = thisVersion.Id;
+		data.Targets = thisVersion.Targets;
+		data.TranslatedName = thisVersion.TranslatedName;
+
+		DoIfExists(version, TagEditableFields.Description, xmlCache, v =>
 		{
-			var data = new ArchivedTagContract();
-			var xmlCache = new XmlCache<ArchivedTagContract>();
-			var thisVersion = version.Data != null ? xmlCache.Deserialize(version.Version, version.Data) : new ArchivedTagContract();
+			data.Description = v.Description;
+			data.DescriptionEng = v.DescriptionEng;
+		});
+		DoIfExists(version, TagEditableFields.Names, xmlCache, v => data.Names = v.Names);
+		DoIfExists(version, TagEditableFields.Parent, xmlCache, v => data.Parent = v.Parent);
+		DoIfExists(version, TagEditableFields.RelatedTags, xmlCache, v => data.RelatedTags = v.RelatedTags);
+		DoIfExists(version, TagEditableFields.WebLinks, xmlCache, v => data.WebLinks = v.WebLinks);
 
-			data.CategoryName = thisVersion.CategoryName;
-			data.HideFromSuggestions = thisVersion.HideFromSuggestions;
-			data.Id = thisVersion.Id;
-			data.Targets = thisVersion.Targets;
-			data.TranslatedName = thisVersion.TranslatedName;
-
-			DoIfExists(version, TagEditableFields.Description, xmlCache, v =>
-			{
-				data.Description = v.Description;
-				data.DescriptionEng = v.DescriptionEng;
-			});
-			DoIfExists(version, TagEditableFields.Names, xmlCache, v => data.Names = v.Names);
-			DoIfExists(version, TagEditableFields.Parent, xmlCache, v => data.Parent = v.Parent);
-			DoIfExists(version, TagEditableFields.RelatedTags, xmlCache, v => data.RelatedTags = v.RelatedTags);
-			DoIfExists(version, TagEditableFields.WebLinks, xmlCache, v => data.WebLinks = v.WebLinks);
-
-			return data;
-		}
+		return data;
+	}
 
 #nullable disable
-		public ArchivedTagContract()
-		{
-			Targets = TagTargetTypes.All;
-		}
+	public ArchivedTagContract()
+	{
+		Targets = TagTargetTypes.All;
+	}
 #nullable enable
 
-		public ArchivedTagContract(Tag tag, TagDiff diff) : this()
-		{
-			ParamIs.NotNull(() => tag);
+	public ArchivedTagContract(Tag tag, TagDiff diff) : this()
+	{
+		ParamIs.NotNull(() => tag);
 
-			CategoryName = tag.CategoryName;
-			Description = diff.IncludeDescription ? tag.Description.Original : null;
-			DescriptionEng = diff.IncludeDescription ? tag.Description.English : null;
-			HideFromSuggestions = tag.HideFromSuggestions;
-			Id = tag.Id;
-			Names = diff.IncludeNames ? tag.Names.Names.Select(n => new LocalizedStringContract(n)).ToArray() : null;
-			Parent = ObjectRefContract.Create(tag.Parent);
-			RelatedTags = diff.IncludeRelatedTags ? tag.RelatedTags.Select(t => new ObjectRefContract(t.LinkedTag)).ToArray() : null;
-			Targets = tag.Targets;
-			ThumbMime = tag.Thumb?.Mime;
-			TranslatedName = new ArchivedTranslatedStringContract(tag.TranslatedName);
-			WebLinks = diff.IncludeWebLinks ? tag.WebLinks.Links.Select(l => new ArchivedWebLinkContract(l)).ToArray() : null;
-		}
-
-		[DataMember]
-		public string CategoryName { get; set; }
-
-		[DataMember]
-		public string? Description { get; set; }
-
-		[DataMember]
-		public string? DescriptionEng { get; set; }
-
-		[DataMember]
-		public bool HideFromSuggestions { get; set; }
-
-		[DataMember]
-		public int Id { get; set; }
-
-		[DataMember]
-		public LocalizedStringContract[]? Names { get; set; }
-
-		[DataMember]
-		public ObjectRefContract? Parent { get; set; }
-
-		[DataMember]
-		public ObjectRefContract[]? RelatedTags { get; set; }
-
-		[DataMember]
-		public TagTargetTypes Targets { get; set; }
-
-		[DataMember]
-		public string? ThumbMime { get; init; }
-
-		[DataMember]
-		public ArchivedTranslatedStringContract TranslatedName { get; set; }
-
-		[DataMember]
-		public ArchivedWebLinkContract[]? WebLinks { get; set; }
+		CategoryName = tag.CategoryName;
+		Description = diff.IncludeDescription ? tag.Description.Original : null;
+		DescriptionEng = diff.IncludeDescription ? tag.Description.English : null;
+		HideFromSuggestions = tag.HideFromSuggestions;
+		Id = tag.Id;
+		Names = diff.IncludeNames ? tag.Names.Names.Select(n => new LocalizedStringContract(n)).ToArray() : null;
+		Parent = ObjectRefContract.Create(tag.Parent);
+		RelatedTags = diff.IncludeRelatedTags ? tag.RelatedTags.Select(t => new ObjectRefContract(t.LinkedTag)).ToArray() : null;
+		Targets = tag.Targets;
+		ThumbMime = tag.Thumb?.Mime;
+		TranslatedName = new ArchivedTranslatedStringContract(tag.TranslatedName);
+		WebLinks = diff.IncludeWebLinks ? tag.WebLinks.Links.Select(l => new ArchivedWebLinkContract(l)).ToArray() : null;
 	}
+
+	[DataMember]
+	public string CategoryName { get; set; }
+
+	[DataMember]
+	public string? Description { get; set; }
+
+	[DataMember]
+	public string? DescriptionEng { get; set; }
+
+	[DataMember]
+	public bool HideFromSuggestions { get; set; }
+
+	[DataMember]
+	public int Id { get; set; }
+
+	[DataMember]
+	public LocalizedStringContract[]? Names { get; set; }
+
+	[DataMember]
+	public ObjectRefContract? Parent { get; set; }
+
+	[DataMember]
+	public ObjectRefContract[]? RelatedTags { get; set; }
+
+	[DataMember]
+	public TagTargetTypes Targets { get; set; }
+
+	[DataMember]
+	public string? ThumbMime { get; init; }
+
+	[DataMember]
+	public ArchivedTranslatedStringContract TranslatedName { get; set; }
+
+	[DataMember]
+	public ArchivedWebLinkContract[]? WebLinks { get; set; }
 }
