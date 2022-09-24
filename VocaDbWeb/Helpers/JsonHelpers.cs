ec4c8b016766c6a782a816ff6b21588bf8a17da4
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2022-09-24 15:40:32 +1000

    Use file scoped namespaces

--- VocaDbWeb/Helpers/JsonHelpers.cs
+++ VocaDbWeb/Helpers/JsonHelpers.cs
@@ -4,26 +4,28 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Serialization;
 
-namespace VocaDb.Web.Helpers
+namespace VocaDb.Web.Helpers;
+
+public static class JsonHelpers
 {
-	public static class JsonHelpers
+	public static string Serialize(object? value, bool lowerCase = true, bool dateTimeConverter = false)
 	{
-		public static string Serialize(object? value, bool lowerCase = true, bool dateTimeConverter = false)
-		{
-			var settings = new JsonSerializerSettings();
+		var settings = new JsonSerializerSettings();
 
-			if (lowerCase)
-				settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+		if (lowerCase)
+			settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
 
-			if (dateTimeConverter)
-				settings.Converters = new[] { new JavaScriptDateTimeConverter() };
+		if (dateTimeConverter)
+			settings.Converters = new[] { new JavaScriptDateTimeConverter() };
 
-			return JsonConvert.SerializeObject(value, Formatting.None, settings);
-		}
+		return JsonConvert.SerializeObject(value, Formatting.None, settings);
+	}
 
-		/// <summary>
-		/// <seealso href="https://github.com/VocaDB/vocadb/pull/736"/>
-		/// </summary>
-		public static IHtmlContent ToJS(object? value, bool lowerCase = true, bool dateTimeConverter = false) => new HtmlString($@"JSON.parse(""{HttpUtility.JavaScriptStringEncode(Serialize(value, lowerCase, dateTimeConverter))}"")");
+	/// <summary>
+	/// <seealso href="https://github.com/VocaDB/vocadb/pull/736"/>
+	/// </summary>
+	public static IHtmlContent ToJS(object? value, bool lowerCase = true, bool dateTimeConverter = false)
+	{
+		return new HtmlString($@"JSON.parse(""{HttpUtility.JavaScriptStringEncode(Serialize(value, lowerCase, dateTimeConverter))}"")");
 	}
 }
