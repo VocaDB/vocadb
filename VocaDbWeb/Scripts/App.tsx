64777733a3b25f6028a968b0f95b02a06b312150
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2022-10-30 02:21:07 +0000

    Create AppContainer

--- VocaDbWeb/Scripts/App.tsx
+++ VocaDbWeb/Scripts/App.tsx
@@ -8,34 +8,34 @@ import { ScrollToTop } from '@vocadb/route-sphere';
 import React from 'react';
 import { Toaster } from 'react-hot-toast';
 
+const AppContainer = (): React.ReactElement => {
+	return (
+		<Container
+			fluid
+			css={{ flex: '1 1 100%', paddingBottom: miniPlayerHeight }}
+		>
+			<div className="row-fluid">
+				<div className="span12 rightFrame well">
+					<React.Suspense fallback={null /* TODO */}>
+						<AppRoutes />
+					</React.Suspense>
+				</div>
+				<AboutDisclaimer />
+			</div>
+		</Container>
+	);
+};
+
 const App = (): React.ReactElement => {
 	return (
 		<>
 			<ScrollToTop />
-
 			<Header />
-
 			<div css={{ display: 'flex' }}>
 				<LeftMenu />
-
-				<Container
-					fluid
-					css={{ flex: '1 1 100%', paddingBottom: miniPlayerHeight }}
-				>
-					<div className="row-fluid">
-						<div className="span12 rightFrame well">
-							<React.Suspense fallback={null /* TODO */}>
-								<AppRoutes />
-							</React.Suspense>
-						</div>
-
-						<AboutDisclaimer />
-					</div>
-				</Container>
+				<AppContainer />
 			</div>
-
 			<Toaster containerStyle={{ top: '10vh' }} gutter={0} />
-
 			<VdbPlayer />
 		</>
 	);
