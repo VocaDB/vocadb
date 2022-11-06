c4dd4398252737018c7cae07d57c48376473d41d
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2022-11-06 04:28:31 +0000

    Create WebAddress

---
+++ VocaDbModel/Domain/WebAddress.cs
@@ -0,0 +1,91 @@
+using VocaDb.Model.Domain.Users;
+
+namespace VocaDb.Model.Domain;
+
+// https://github.com/ycanardeau/yamatouta/blob/ba67503d3ded79c942c7240394faa1931e36f0e4/backend/src/entities/WebAddressHost.ts
+public class WebAddressHost
+{
+	public virtual int Id { get; set; }
+
+	public virtual DateTimeOffset CreatedAt { get; set; }
+
+	public virtual DateTimeOffset UpdatedAt { get; set; }
+
+	public virtual required string Hostname { get; set; }
+
+	public virtual int ReferenceCount { get; set; }
+
+	public virtual required User Actor { get; set; }
+
+	public WebAddressHost() { }
+
+	public WebAddressHost(string hostname, User actor)
+	{
+		Hostname = hostname;
+		Actor = actor;
+	}
+
+	public void IncrementReferenceCount()
+	{
+		ReferenceCount++;
+	}
+
+	public void DecrementReferenceCount()
+	{
+		ReferenceCount--;
+	}
+}
+
+// https://github.com/ycanardeau/yamatouta/blob/ba67503d3ded79c942c7240394faa1931e36f0e4/backend/src/entities/WebAddress.ts
+public class WebAddress
+{
+	public virtual int Id { get; set; }
+
+	public virtual DateTimeOffset CreatedAt { get; set; }
+
+	public virtual DateTimeOffset UpdatedAt { get; set; }
+
+	public virtual required string Url { get; set; }
+
+	public virtual required string Scheme { get; set; }
+
+	public virtual required WebAddressHost Host { get; set; }
+
+	public virtual required int Port { get; set; }
+
+	public virtual required string Path { get; set; }
+
+	public virtual required string Query { get; set; }
+
+	public virtual required string Fragment { get; set; }
+
+	public virtual int ReferenceCount { get; set; }
+
+	public virtual required User Actor { get; set; }
+
+	public WebAddress() { }
+
+	public WebAddress(Uri uri, WebAddressHost host, User actor)
+	{
+		Url = uri.ToString();
+		Scheme = uri.Scheme;
+		Host = host;
+		Port = uri.Port;
+		Path = uri.LocalPath;
+		Query = uri.Query;
+		Fragment = uri.Fragment;
+		Actor = actor;
+	}
+
+	public void IncrementReferenceCount()
+	{
+		Host.IncrementReferenceCount();
+		ReferenceCount++;
+	}
+
+	public void DecrementReferenceCount()
+	{
+		Host.DecrementReferenceCount();
+		ReferenceCount--;
+	}
+}
---
+++ VocaDbModel/Mapping/WebAddressMap.cs
@@ -0,0 +1,42 @@
+using FluentNHibernate.Mapping;
+using VocaDb.Model.Domain;
+
+namespace VocaDb.Model.Mapping;
+
+public class WebAddressHostMap : ClassMap<WebAddressHost>
+{
+	public WebAddressHostMap()
+	{
+		Cache.ReadWrite();
+		Id(m => m.Id);
+
+		Map(m => m.CreatedAt).Not.Nullable();
+		Map(m => m.UpdatedAt).Not.Nullable();
+		Map(m => m.Hostname).Not.Nullable();
+		Map(m => m.ReferenceCount).Not.Nullable();
+
+		References(m => m.Actor).Not.Nullable();
+	}
+}
+
+public class WebAddressMap : ClassMap<WebAddress>
+{
+	public WebAddressMap()
+	{
+		Cache.ReadWrite();
+		Id(m => m.Id);
+
+		Map(m => m.CreatedAt).Not.Nullable();
+		Map(m => m.UpdatedAt).Not.Nullable();
+		Map(m => m.Url).Not.Nullable();
+		Map(m => m.Scheme).Not.Nullable();
+		Map(m => m.Port).Not.Nullable();
+		Map(m => m.Path).Not.Nullable();
+		Map(m => m.Query).Not.Nullable();
+		Map(m => m.Fragment).Not.Nullable();
+		Map(m => m.ReferenceCount).Not.Nullable();
+
+		References(m => m.Host).Not.Nullable();
+		References(m => m.Actor).Not.Nullable();
+	}
+}
