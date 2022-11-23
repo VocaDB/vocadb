0e7b10c78b82d55f397fe066d7fde24e3e99f20b
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2022-11-23 08:21:13 +0000

    Create GlobalResources

--- VocaDbWeb/Scripts/typings/global.d.ts
+++ VocaDbWeb/Scripts/typings/global.d.ts
@@ -1,25 +1,9 @@
+import { GlobalResources } from '@/Shared/GlobalResources';
 import { GlobalValues } from '@/Shared/GlobalValues';
 
 declare global {
 	const vdb: {
-		resources: {
-			albumEdit: any;
-			entryEdit: any;
-			shared: any;
-			song:
-				| any /* TODO: Remove. */
-				| { newSongInfo?: string; rankingsTitle?: string };
-			album: {
-				addedToCollection?: string;
-				newAlbumArtistDesc?: string;
-				newAlbumInfo?: string;
-			};
-			albumDetails: { download: string };
-			artist: { authoredBy?: string; newArtistExternalLink?: string };
-			layout: { paypalDonateTitle?: string };
-			home: { welcome?: string; welcomeSubtitle?: string };
-			user: { requestVerificationInfo?: string };
-		};
+		resources: GlobalResources;
 		values: GlobalValues;
 	};
 }
