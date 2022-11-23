a75cd111bd061cef857dac207a868d6b5c6f4cd6
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2022-11-23 08:39:34 +0000

    Update App.tsx and index.tsx

--- VocaDbWeb/Scripts/App.tsx
+++ VocaDbWeb/Scripts/App.tsx
@@ -1,12 +1,18 @@
 import AppRoutes from '@/AppRoutes';
+import '@/ArrayExtensions';
 import Container from '@/Bootstrap/Container';
 import { AboutDisclaimer } from '@/Components/Shared/Partials/AboutDisclaimer';
 import { Header } from '@/Components/Shared/Partials/Header';
 import { LeftMenu } from '@/Components/Shared/Partials/LeftMenu';
 import { miniPlayerHeight, VdbPlayer } from '@/Components/VdbPlayer/VdbPlayer';
+import { VdbPlayerProvider } from '@/Components/VdbPlayer/VdbPlayerContext';
+import { MutedUsersProvider } from '@/MutedUsersContext';
+import '@/i18n';
+import { NostalgicDivaProvider } from '@vocadb/nostalgic-diva';
 import { ScrollToTop } from '@vocadb/route-sphere';
 import React from 'react';
 import { Toaster } from 'react-hot-toast';
+import { BrowserRouter } from 'react-router-dom';
 
 const AppContainer = (): React.ReactElement => {
 	return (
@@ -33,16 +39,22 @@ const AppContainer = (): React.ReactElement => {
 
 const App = (): React.ReactElement => {
 	return (
-		<>
-			<ScrollToTop />
-			<Header />
-			<div css={{ display: 'flex' }}>
-				<LeftMenu />
-				<AppContainer />
-			</div>
-			<Toaster containerStyle={{ top: '10vh' }} gutter={0} />
-			<VdbPlayer />
-		</>
+		<BrowserRouter>
+			<NostalgicDivaProvider>
+				<VdbPlayerProvider>
+					<MutedUsersProvider>
+						<ScrollToTop />
+						<Header />
+						<div css={{ display: 'flex' }}>
+							<LeftMenu />
+							<AppContainer />
+						</div>
+						<Toaster containerStyle={{ top: '10vh' }} gutter={0} />
+						<VdbPlayer />
+					</MutedUsersProvider>
+				</VdbPlayerProvider>
+			</NostalgicDivaProvider>
+		</BrowserRouter>
 	);
 };
 
