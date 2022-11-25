6d20311991c0c6c2adff595fab72df1a9da3b2de
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2022-11-25 13:36:24 +0000

    Refactor GlobalResources and GlobalValues

--- VocaDbWeb/Scripts/typings/global.d.ts
+++ VocaDbWeb/Scripts/typings/global.d.ts
@@ -1,6 +1,7 @@
 import { GlobalResources } from '@/Shared/GlobalResources';
 import { GlobalValues } from '@/Shared/GlobalValues';
 
+// TODO: Remove.
 declare global {
 	const vdb: {
 		resources: GlobalResources;
