e7c9d1da8ab4c043e975433a2ac8e4f8d46eb4e7
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2022-11-26 01:19:26 +0000

    Update VdbProvider

--- VocaDbWeb/Scripts/VdbContext.tsx
+++ VocaDbWeb/Scripts/VdbContext.tsx
@@ -1,5 +1,7 @@
 import { GlobalResources } from '@/Shared/GlobalResources';
 import { GlobalValues } from '@/Shared/GlobalValues';
+import { httpClient } from '@/Shared/HttpClient';
+import { urlMapper } from '@/Shared/UrlMapper';
 import React from 'react';
 
 interface VdbContextProps {
@@ -16,10 +18,25 @@ interface VdbProviderProps {
 export const VdbProvider = ({
 	children,
 }: VdbProviderProps): React.ReactElement => {
-	return (
-		<VdbContext.Provider value={(window as any).vdb /* TODO */}>
-			{children}
-		</VdbContext.Provider>
+	const [vdb, setVdb] = React.useState<VdbContextProps>();
+
+	React.useEffect(() => {
+		Promise.all([
+			httpClient.get<GlobalResources>(
+				urlMapper.mapRelative('/api/globals/resources'),
+			),
+			httpClient.get<GlobalValues>(
+				urlMapper.mapRelative('/api/globals/values'),
+			),
+		]).then(([resources, values]) => {
+			setVdb({ resources, values });
+		});
+	}, []);
+
+	return vdb ? (
+		<VdbContext.Provider value={vdb}>{children}</VdbContext.Provider>
+	) : (
+		<></>
 	);
 };
 
