67b1a17445a2efa1986ea8c3425dbcd37057e156
szc126 <29127915+szc126@users.noreply.github.com>
2023-03-14 18:44:44 -0700

    Replace the piapro embed player: change from swf to HTML5 iframe

--- VocaDbWeb/Scripts/Components/Shared/Partials/PV/EmbedPiapro.tsx
+++ VocaDbWeb/Scripts/Components/Shared/Partials/PV/EmbedPiapro.tsx
@@ -10,20 +10,13 @@ interface EmbedPiaproProps {
 export const EmbedPiapro = React.memo(
 	({ pv, width, height }: EmbedPiaproProps): React.ReactElement => {
 		return (
-			// <object> embed instead of iframe because iframe doesn't work with flash disabled
-			<object
-				type="application/x-shockwave-flash"
-				width={width ?? 340}
-				height={height ?? 80}
+			// eslint-disable-next-line jsx-a11y/iframe-has-title
+			<iframe
+				width={width}
+				height={height}
 				style={{ border: 0 }}
-				data={`//piapro.jp/modpub/swf/player03_h.swf?030503&id=${pv.pvId}&c=1`}
-			>
-				<param
-					name="movie"
-					value={`//piapro.jp/modpub/swf/player03_h.swf?030503&id=${pv.pvId}&c=1`}
-				/>
-				<param name="quality" value="high" />
-			</object>
+				src={`//piapro.jp/html5_player_popup/?id=${pv.pvId}`}
+			/>
 		);
 	},
 );
