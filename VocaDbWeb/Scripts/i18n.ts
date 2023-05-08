a83502167375f1a5f3864181f175736a5370f735
FinnRG <finngaertner@protonmail.com>
2023-05-08 19:21:24 +0200

    feat: Add i18n localstorage caching

--- VocaDbWeb/Scripts/i18n.ts
+++ VocaDbWeb/Scripts/i18n.ts
@@ -1,9 +1,11 @@
 import i18n from 'i18next';
-import Backend from 'i18next-http-backend';
+import I18NextChainedBackend from 'i18next-chained-backend';
+import I18NextHttpBackend from 'i18next-http-backend';
+import I18NextLocalStorageBackend from 'i18next-localstorage-backend';
 import { initReactI18next } from 'react-i18next';
 
 i18n
-	.use(Backend)
+	.use(I18NextChainedBackend)
 	.use(initReactI18next)
 	.init({
 		load: 'all',
@@ -13,5 +15,14 @@ i18n
 		},
 		react: {
 			useSuspense: false,
+		},
+		backend: {
+			backends: [I18NextLocalStorageBackend, I18NextHttpBackend],
+			backendOptions: [
+				{
+					expirationTime: 7 * 24 * 60 * 60 * 1000, // 7 days
+					versions: {},
+				},
+			],
 		},
 	});
