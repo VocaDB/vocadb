6f45f403ee0b64873c264d2b5fba4fe3a0d2dc77
FinnRG <finngaertner@protonmail.com>
2023-05-31 00:39:31 +0200

    feat: Implement lyric language auto detection

--- VocaDbWeb/Scripts/CultureCodesContext.tsx
+++ VocaDbWeb/Scripts/CultureCodesContext.tsx
@@ -3,17 +3,25 @@ import React, { useEffect, useState } from 'react';
 import { userLanguageCultures } from './Components/userLanguageCultures';
 
 type CodeDescription = { englishName: string; nativeName: string };
-type Codes = { [k: string]: CodeDescription };
+type Codes = Record<string, CodeDescription>;
 
-const CultureCodesContext = React.createContext<Codes | undefined>(undefined);
+const CultureCodesContext = React.createContext<CultureCodeData | undefined>(
+	undefined,
+);
 
 interface CultureCodesProviderProps {
 	children?: React.ReactNode;
 }
 
+interface CultureCodeData {
+	codes?: Codes;
+	iso639to1?: Record<string, string>;
+}
+
 interface CultureCodeTools {
 	codes?: Codes;
 	getCodeDescription: (code: string) => CodeDescription | undefined;
+	iso639to1?: Record<string, string>;
 }
 
 // Lazily loads extendedUserLanguageCultures
@@ -23,15 +31,17 @@ export const CultureCodesProvider = ({
 	const [cultureCodes, setCultureCodes] = useState<Codes | undefined>(
 		undefined,
 	);
+	const [iso639to1, setIso639to1] = useState<Record<string, string>>({});
 
 	useEffect(() => {
-		import('./Components/extendedUserLanguageCultures').then((resp) =>
-			setCultureCodes(resp.extendedUserLanguageCultures),
-		);
+		import('./Components/extendedUserLanguageCultures').then((resp) => {
+			setCultureCodes(resp.extendedUserLanguageCultures);
+			setIso639to1(resp.iso6393To1);
+		});
 	}, []);
 
 	return (
-		<CultureCodesContext.Provider value={cultureCodes}>
+		<CultureCodesContext.Provider value={{ codes: cultureCodes, iso639to1 }}>
 			{children}
 		</CultureCodesContext.Provider>
 	);
@@ -39,14 +49,12 @@ export const CultureCodesProvider = ({
 
 export const useCultureCodes = (): CultureCodeTools => {
 	const codes = React.useContext(CultureCodesContext);
-
 	const getCodeDescription = (code: string): CodeDescription | undefined => {
 		if (!(code in userLanguageCultures)) {
-			return codes === undefined ? undefined : codes[code];
+			return codes?.codes === undefined ? undefined : codes.codes[code];
 		}
 
 		return userLanguageCultures[code];
 	};
-
-	return { codes, getCodeDescription };
+	return { ...codes, getCodeDescription };
 };
