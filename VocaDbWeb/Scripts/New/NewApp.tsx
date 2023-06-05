867137df63877f4bd0235ca24f185278f6e98256
FinnRG <finngaertner@protonmail.com>
2023-06-05 22:33:59 +0200

    chores: Move NewApp into separate folder

--- VocaDbWeb/Scripts/New/NewApp.tsx
+++ VocaDbWeb/Scripts/New/NewApp.tsx
@@ -7,11 +7,12 @@ import {
 	ThemeIcon,
 	Text,
 	MantineColor,
+	MantineProvider,
 } from '@mantine/core';
 import { IconMusic } from '@tabler/icons-react';
 
 const linkData = [
-	{ icon: <IconMusic size="1rem" />, color: 'blue', label: 'Songs' },
+	{ icon: <IconMusic size="1rem" />, color: 'teal', label: 'Songs' },
 ];
 
 interface MainLinkProps {
@@ -35,7 +36,6 @@ const MainLink = ({
 				borderRadius: theme.radius.sm,
 				color:
 					theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black,
-
 				'&:hover': {
 					backgroundColor:
 						theme.colorScheme === 'dark'
@@ -48,7 +48,6 @@ const MainLink = ({
 				<ThemeIcon color={color} variant="light">
 					{icon}
 				</ThemeIcon>
-
 				<Text size="sm">{label}</Text>
 			</Group>
 		</UnstyledButton>
@@ -57,32 +56,52 @@ const MainLink = ({
 
 const NewApp = (): React.ReactElement => {
 	return (
-		<AppShell
-			navbarOffsetBreakpoint="sm"
-			navbar={
-				<Navbar p="md" hiddenBreakpoint="sm" width={{ sm: 200, lg: 300 }}>
-					{linkData.map((link) => (
-						<MainLink {...link} key={link.label} />
-					))}
-				</Navbar>
-			}
-			header={
-				<Header height={{ base: 50, md: 70 }} p="md">
-					{/* Header content */}
-				</Header>
-			}
-			// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-			styles={(theme) => ({
-				main: {
-					backgroundColor:
-						theme.colorScheme === 'dark'
-							? theme.colors.dark[8]
-							: theme.colors.gray[0],
+		<MantineProvider
+			theme={{
+				colors: {
+					teal: [
+						'#EDF8F7',
+						'#CCEAE9',
+						'#ACDDDB',
+						'#8BD0CD',
+						'#6AC3BF',
+						'#4AB6B1',
+						'#3B918E',
+						'#2C6D6A',
+						'#1D4947',
+						'#0F2423',
+					],
 				},
-			})}
+				primaryColor: 'teal',
+			}}
 		>
-			{/* Your application here */}
-		</AppShell>
+			<AppShell
+				navbarOffsetBreakpoint="sm"
+				navbar={
+					<Navbar p="md" hiddenBreakpoint="sm" width={{ sm: 200, lg: 300 }}>
+						{linkData.map((link) => (
+							<MainLink {...link} key={link.label} />
+						))}
+					</Navbar>
+				}
+				header={
+					<Header height={{ base: 50, md: 70 }} p="md">
+						{/* Header content */}
+					</Header>
+				}
+				// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+				styles={(theme) => ({
+					main: {
+						backgroundColor:
+							theme.colorScheme === 'dark'
+								? theme.colors.dark[8]
+								: theme.colors.gray[0],
+					},
+				})}
+			>
+				{/* Your application here */}
+			</AppShell>
+		</MantineProvider>
 	);
 };
 
