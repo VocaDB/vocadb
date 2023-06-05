2af79dd8a8ad11e46225529b55d1797661e2c0fa
FinnRG <finngaertner@protonmail.com>
2023-06-05 02:21:35 +0200

    chores: Add native language names

--- VocaDbWeb/Scripts/Components/extendedUserLanguageCultures.ts
+++ VocaDbWeb/Scripts/Components/extendedUserLanguageCultures.ts
@@ -3,18 +3,21 @@ const rawLangs: RawLangs = [
 		'ain',
 		{
 			languages: ['Ainu', 'Hokkaido Ainu'],
+			nativeName: 'アイヌ・イタㇰ',
 		},
 	],
 	[
 		'arq',
 		{
 			languages: ['Algerian Arabic', 'Dziria', 'Darja', 'Derja'],
+			nativeName: 'الدارجة الجزائرية',
 		},
 	],
 	[
 		'am',
 		{
 			languages: ['Amharic'],
+			nativeName: 'አማርኛ',
 		},
 	],
 	[
@@ -30,60 +33,70 @@ const rawLangs: RawLangs = [
 				'Tsanglo',
 				'Tenyidie',
 			],
+			nativeName: 'Tenyidie',
 		},
 	],
 	[
 		'qan',
 		{
 			languages: ['Animalese'],
+			nativeName: 'Animalese',
 		},
 	],
 	[
 		'ar',
 		{
 			languages: ['Arabic'],
+			nativeName: 'اَلْعَرَبِيَّةُ',
 		},
 	],
 	[
 		'awa',
 		{
 			languages: ['Awadhi', 'Audhi'],
+			nativeName: 'अवधी',
 		},
 	],
 	[
 		'aze',
 		{
 			languages: ['Azerbaijani', 'Azeri', 'Azeri Turkic', 'Azeri Turkish'],
+			nativeName: 'Azərbaycan dili',
 		},
 	],
 	[
 		'eu',
 		{
 			languages: ['Basque'],
+			nativeName: 'euskara',
 		},
 	],
 	[
 		'be',
 		{
 			languages: ['Belarusian'],
+			nativeName: 'Беларуская мова',
 		},
 	],
 	[
 		'bn',
 		{
 			languages: ['Bengali', 'Bangla'],
+			nativeName: 'বাংলা',
 		},
 	],
 	[
 		'bho',
 		{
 			languages: ['Bhojpuri'],
+			nativeName: 'भोजपुरी',
 		},
 	],
 	[
 		'bs',
 		{
 			languages: ['Bosnian'],
+			nativeName: 'Bosanski',
 		},
 	],
 	[
@@ -94,120 +107,140 @@ const rawLangs: RawLangs = [
 				'Portuguese of Brazil',
 				'South American Portuguese',
 			],
+			nativeName: 'Português Brasileiro',
 		},
 	],
 	[
 		'bg',
 		{
 			languages: ['Bulgarian'],
+			nativeName: 'български',
 		},
 	],
 	[
 		'my',
 		{
 			languages: ['Burmese', 'Myanmar language'],
+			nativeName: 'မြန်မာစာ',
 		},
 	],
 	[
 		'yue',
 		{
 			languages: ['Cantonese', 'Yue Chinese'],
+			nativeName: '廣東話',
 		},
 	],
 	[
 		'ca',
 		{
 			languages: ['Catalan', 'Valencian'],
+			nativeName: 'català',
 		},
 	],
 	[
 		'chr',
 		{
 			languages: ['Cherokee', 'Tsalagi'],
+			nativeName: 'ᏣᎳᎩ ᎦᏬᏂᎯᏍᏗ',
 		},
 	],
 	[
 		'zh',
 		{
 			languages: ['Chinese'],
+			nativeName: '汉语',
 		},
 	],
 	[
 		'xct',
 		{
-			languages: ['Classical Tibetan'],
+			languages: ['Central Tibetan'],
+			nativeName: 'དབུས',
 		},
 	],
 	[
 		'dtp',
 		{
 			languages: ['Coastal Kadazan'],
+			nativeName: 'Kadazan Tangaa',
 		},
 	],
 	[
 		'hrv',
 		{
 			languages: ['Croatian'],
+			nativeName: 'hrvatski',
 		},
 	],
 	[
 		'cs',
 		{
 			languages: ['Czech', 'Bohemian'],
+			nativeName: 'čeština',
 		},
 	],
 	[
 		'da',
 		{
 			languages: ['Danish'],
+			nativeName: 'dansk',
 		},
 	],
 	[
 		'prs',
 		{
 			languages: ['Dari', 'Dari Persian'],
+			nativeName: 'دری',
 		},
 	],
 	[
 		'qdo',
 		{
 			languages: ['Dovahzul'],
+			nativeName: 'Dovahzul',
 		},
 	],
 	[
 		'dtp',
 		{
 			languages: ['Dusun', 'Central Dusun', 'Bunduliwan'],
+			nativeName: 'Bunduliwan',
 		},
 	],
 	[
 		'nld',
 		{
 			languages: ['Dutch'],
+			nativeName: 'Nederlands',
 		},
 	],
 	[
 		'arz',
 		{
 			languages: ['Egyptian Arabic', 'Colloquial Egyptian', 'Masri', 'Masry'],
+			nativeName: 'مصرى',
 		},
 	],
 	[
 		'en',
 		{
 			languages: ['English'],
+			nativeName: 'English',
 		},
 	],
 	[
 		'eo',
 		{
 			languages: ['Esperanto'],
+			nativeName: 'Esperanto',
 		},
 	],
 	[
 		'et',
 		{
 			languages: ['Estonian'],
+			nativeName: 'eesti keel',
 		},
 	],
 	[
@@ -219,12 +252,14 @@ const rawLangs: RawLangs = [
 				'Constructed language',
 				'Artificial language',
 			],
+			nativeName: 'fictional',
 		},
 	],
 	[
 		'fi',
 		{
 			languages: ['Finnish', 'Suomi'],
+			nativeName: 'suomi',
 		},
 	],
 	[
@@ -236,108 +271,126 @@ const rawLangs: RawLangs = [
 				'Belgian Dutch',
 				'Southern Dutch',
 			],
+			nativeName: 'Vlaams',
 		},
 	],
 	[
 		'fr',
 		{
 			languages: ['French'],
+			nativeName: 'français',
 		},
 	],
 	[
 		'gan',
 		{
 			languages: ['Gan Chinese', 'Gann', 'Kan'],
+			nativeName: '贛語',
 		},
 	],
 	[
 		'kat',
 		{
 			languages: ['Georgian'],
+			nativeName: 'ქართული ენა',
 		},
 	],
 	[
 		'de',
 		{
 			languages: ['German', 'High German'],
+			nativeName: 'Deutsch',
 		},
 	],
 	[
 		'zxx',
 		{
 			languages: ['Gibberish', 'Nonsensical', 'Vocables'],
+			nativeName: 'Gibberish',
 		},
 	],
 	[
 		'el',
 		{
 			languages: ['Greek'],
+			nativeName: 'ελληνικά',
 		},
 	],
 	[
 		'gu',
 		{
 			languages: ['Gujarati'],
+			nativeName: 'ગુજરાત',
 		},
 	],
 	[
 		'hak',
 		{
 			languages: ['Hakka Chinese', 'Keyu', 'Kejiahua', 'Hak-k\u00e2-fa'],
+			nativeName: '客家話',
 		},
 	],
 	[
 		'ha',
 		{
 			languages: ['Hausa'],
+			nativeName: 'هَرْشَن هَوْسَ',
 		},
 	],
 	[
 		'haw',
 		{
 			languages: ['Hawaiian'],
+			nativeName: 'ʻŌlelo Hawaiʻi',
 		},
 	],
 	[
 		'he',
 		{
 			languages: ['Hebrew'],
+			nativeName: 'עִבְרִית',
 		},
 	],
 	[
 		'hi',
 		{
 			languages: ['Hindi'],
+			nativeName: 'हिंदी',
 		},
 	],
 	[
 		'nan',
 		{
 			languages: ['Hokkien'],
+			nativeName: '閩南話',
 		},
 	],
 	[
 		'hu',
 		{
 			languages: ['Hungarian'],
+			nativeName: 'magyar nyelv',
 		},
 	],
 	[
 		'qhy',
 		{
 			languages: ['Hymmnos', 'Hymnos'],
+			nativeName: 'Hymmnos',
 		},
 	],
 	[
 		'is',
 		{
 			languages: ['Icelandic'],
+			nativeName: 'íslenska',
 		},
 	],
 	[
 		'id',
 		{
 			languages: ['Indonesian'],
+			nativeName: 'Bahasa Indonesia',
 		},
 	],
 	[
@@ -349,221 +402,245 @@ const rawLangs: RawLangs = [
 				'Western Farsi',
 				'Persian',
 			],
+			nativeName: 'فارسی',
 		},
 	],
 	[
 		'it',
 		{
 			languages: ['Italian'],
+			nativeName: 'italiano',
 		},
 	],
 	[
 		'ja',
 		{
 			languages: ['Japanese'],
+			nativeName: '日本語',
 		},
 	],
 	[
 		'jv',
 		{
 			languages: ['Javanese'],
+			nativeName: 'ꦧꦱꦗꦮ',
 		},
 	],
 	[
 		'cjy',
 		{
 			languages: ['Jin Chinese'],
+			nativeName: '晋语',
 		},
 	],
 	[
 		'kn',
 		{
 			languages: ['Kannada', 'Canarese'],
+			nativeName: 'ಕನ್ನಡ',
 		},
 	],
 	[
 		'ko',
 		{
 			languages: ['Korean'],
+			nativeName: '한국어',
 		},
 	],
 	[
 		'la',
 		{
 			languages: ['Latin'],
+			nativeName: 'lingua Latīna',
 		},
 	],
 	[
 		'bo',
 		{
 			languages: ['Lhasa Tibetan', 'Standard Tibetan'],
+			nativeName: 'བོད་སྐད་',
 		},
 	],
 	[
 		'ln',
 		{
 			languages: ['Lingala', 'Ngala'],
+			nativeName: 'Lingála',
 		},
 	],
 	[
 		'lt',
 		{
 			languages: ['Lithuanian'],
+			nativeName: 'lietuvių kalba',
 		},
 	],
 	[
 		'mai',
 		{
 			languages: ['Maithili'],
+			nativeName: 'मैथिली',
 		},
 	],
 	[
 		'ms',
 		{
 			languages: ['Malay'],
+			nativeName: 'Bahasa Melayu',
 		},
 	],
 	[
 		'mal',
 		{
 			languages: ['Malayalam'],
-		},
-	],
-	[
-		'zsm',
-		{
-			languages: [
-				'Malaysian Malay',
-				'Standard Malay',
-				'Bahasa Malaysia',
-				'Malay',
-			],
+			nativeName: 'മലയാളം',
 		},
 	],
 	[
 		'cmn',
 		{
 			languages: ['Mandarin Chinese', 'Northern Chinese'],
+			nativeName: '官話',
 		},
 	],
 	[
 		'mri',
 		{
 			languages: ['Maori', 'M\u0101ori', 'te reo M\u0101ori', 'te reo'],
+			nativeName: 'Māori',
 		},
 	],
 	[
 		'mr',
 		{
 			languages: ['Marathi'],
+			nativeName: 'Marāṭhī',
 		},
 	],
 	[
 		'ltc',
 		{
 			languages: ['Middle Chinese', 'Ancient Chinese', 'Qieyun system'],
+			nativeName: '中古漢語',
 		},
 	],
 	[
 		'arb',
 		{
 			languages: ['Modern Standard Arabic', 'Modern Written Arabic'],
+			nativeName: 'العربية الفصحى',
 		},
 	],
 	[
 		'mn',
 		{
 			languages: ['Mongolian'],
+			nativeName: 'монгол хэл',
 		},
 	],
 	[
 		'nap',
 		{
 			languages: ['Neapolitan'],
+			nativeName: 'napulitano',
 		},
 	],
 	[
 		'pcm',
 		{
 			languages: ['Nigerian Pidgin', 'Naij\u00e1', 'Naija'],
+			nativeName: 'Naijá',
 		},
 	],
 	[
 		'no',
 		{
 			languages: ['Norwegian'],
+			nativeName: 'norsk',
 		},
 	],
 	[
 		'ori',
 		{
 			languages: ['Odia', 'Oriya'],
+			nativeName: 'ଓଡ଼ିଆ',
 		},
 	],
 	[
 		'och',
 		{
 			languages: ['Old Chinese', 'Archaic Chinese'],
+			nativeName: '上古漢語',
 		},
 	],
 	[
 		'ojp',
 		{
 			languages: ['Old Japanese'],
+			nativeName: '上代日本語',
 		},
 	],
 	[
 		'fas',
 		{
 			languages: ['Persian', 'Farsi'],
+			nativeName: 'فارسی',
 		},
 	],
 	[
 		'qpl',
 		{
 			languages: ['Pig Latin'],
+			nativeName: 'Pig Latin',
 		},
 	],
 	[
 		'pl',
 		{
 			languages: ['Polish'],
+			nativeName: 'polski',
 		},
 	],
 	[
 		'pt',
 		{
 			languages: ['Portuguese'],
+			nativeName: 'português',
 		},
 	],
 	[
 		'pan',
 		{
 			languages: ['Punjabi', 'Panjabi', 'Western Panjabi', 'Eastern Panjabi'],
+			nativeName: 'ਪੰਜਾਬੀ',
 		},
 	],
 	[
 		'que',
 		{
 			languages: ['Quechua', 'Runasimi', 'Kichwa'],
+			nativeName: 'Runasimi',
 		},
 	],
 	[
 		'ro',
 		{
 			languages: ['Romanian', 'Rumanian', 'Roumanian'],
+			nativeName: 'limba română',
 		},
 	],
 	[
 		'ru',
 		{
 			languages: ['Russian'],
+			nativeName: 'Русский',
 		},
 	],
 	[
 		'qry',
 		{
 			languages: ['Ryukyuan', 'Lewchewan', 'Luchuan', 'Japonic', 'Japanic'],
+			nativeName: '琉球語派',
 		},
 	],
 	[
@@ -574,54 +651,63 @@ const rawLangs: RawLangs = [
 				'\u1e62a\u02bd\u012bdi Arabic',
 				'Upper Egyptian Arabic',
 			],
+			nativeName: 'صعيدى',
 		},
 	],
 	[
 		'sa',
 		{
 			languages: ['Sanskrit'],
+			nativeName: 'संस्कृतम्',
 		},
 	],
 	[
 		'gla',
 		{
 			languages: ['Scottish Gaelic', 'Scots Gaelic', 'Gaelic'],
+			nativeName: 'Gàidhlig',
 		},
 	],
 	[
 		'snd',
 		{
 			languages: ['Sindhi'],
+			nativeName: 'سنڌي',
 		},
 	],
 	[
 		'sk',
 		{
 			languages: ['Slovak'],
+			nativeName: 'slovenčina',
 		},
 	],
 	[
 		'slv',
 		{
 			languages: ['Slovene', 'Slovenian'],
+			nativeName: 'slovenski jezik',
 		},
 	],
 	[
 		'so',
 		{
 			languages: ['Somali'],
+			nativeName: '𐒖𐒍 𐒈𐒝𐒑𐒛𐒐𐒘',
 		},
 	],
 	[
 		'nan',
 		{
 			languages: ['Southern Min', 'Min Nan', 'Minnan', 'Banlam'],
+			nativeName: '閩南語',
 		},
 	],
 	[
 		'es',
 		{
 			languages: ['Spanish', 'Castilian'],
+			nativeName: 'español',
 		},
 	],
 	[
@@ -633,144 +719,166 @@ const rawLangs: RawLangs = [
 				'Colloquial Sudanese',
 				'Common Sudanese',
 			],
+			nativeName: 'لهجة سودانية',
 		},
 	],
 	[
 		'sun',
 		{
 			languages: ['Sundanese'],
+			nativeName: 'basa Sunda',
 		},
 	],
 	[
 		'sw',
 		{
 			languages: ['Swahili', 'Kiswahili'],
+			nativeName: 'Kiswahili',
 		},
 	],
 	[
 		'sv',
 		{
 			languages: ['Swedish'],
+			nativeName: 'Svenska',
 		},
 	],
 	[
-		'fil',
+		'tl',
 		{
 			languages: ['Filipino', 'Tagalog'],
+			nativeName: 'Wikang Tagalog',
 		},
 	],
 	[
 		'tg',
 		{
-			languages: ['Tajik', 'Tajiki Persian', 'Tajiki', 'Filipino'],
+			languages: ['Tajik', 'Tajiki Persian', 'Tajiki'],
+			nativeName: 'Тоҷикӣ',
 		},
 	],
 	[
 		'ta',
 		{
 			languages: ['Tamil'],
+			nativeName: 'தமிழ்',
 		},
 	],
 	[
 		'te',
 		{
 			languages: ['Telugu'],
+			nativeName: 'తెలుగు',
 		},
 	],
 	[
 		'th',
 		{
 			languages: ['Thai', 'Central Thai', 'Siamese'],
+			nativeName: 'ภาษาไทย',
 		},
 	],
 	[
 		'tig',
 		{
 			languages: ['Tigre', 'Tigrayit'],
+			nativeName: 'ትግረ',
 		},
 	],
 	[
 		'tok',
 		{
 			languages: ['Toki Pona', 'Good Language'],
+			nativeName: 'toki pona',
 		},
 	],
 	[
 		'tr',
 		{
 			languages: ['Turkish'],
+			nativeName: 'Türkçe',
 		},
 	],
 	[
 		'uk',
 		{
 			languages: ['Ukrainian'],
+			nativeName: 'українська мова',
 		},
 	],
 	[
 		'ur',
 		{
 			languages: ['Urdu'],
+			nativeName: 'اردو',
 		},
 	],
 	[
 		'vi',
 		{
 			languages: ['Vietnamese'],
+			nativeName: 'tiếng Việt',
 		},
 	],
 	[
 		'cy',
 		{
 			languages: ['Welsh'],
+			nativeName: 'Cymraeg',
 		},
 	],
 	[
 		'wuu',
 		{
 			languages: ['Wu Chinese', 'Shanghainese'],
+			nativeName: '吴语',
 		},
 	],
 	[
 		'hsn',
 		{
 			languages: ['Xiang Chinese', 'Hsiang', 'Hunanese'],
+			nativeName: '湘语',
 		},
 	],
 	[
 		'yi',
 		{
 			languages: ['Yiddish'],
+			nativeName: 'ייִדיש',
 		},
 	],
 	[
 		'yo',
 		{
 			languages: ['Yoruba'],
+			nativeName: 'Èdè Yorùbá',
 		},
 	],
 	[
 		'yue',
 		{
 			languages: ['Yue Chinese', 'Cantonese'],
+			nativeName: '粤语',
 		},
 	],
 	[
 		'zu',
 		{
 			languages: ['Zulu', 'isiZulu'],
+			nativeName: 'isiZulu',
 		},
 	],
 ];
 
-type RawLangs = [string, { languages: string[] }][];
+type RawLangs = [string, { languages: string[]; nativeName: string }][];
 
 const extendedUserLanguageCultures = Object.fromEntries(
 	rawLangs.map((lang) => [
 		lang[0],
 		{
 			englishName: lang[1].languages[0],
-			nativeName: lang[1].languages[0],
+			nativeName: lang[1].nativeName,
 		},
 	]),
 );
