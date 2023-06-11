749349c7b56c74c3458090e531f4512d5ff14853
FinnRG <finngaertner@protonmail.com>
2023-06-11 14:43:20 +0200

    feat: Improved welcome message

--- VocaDbWeb/New/components/Welcome/Welcome.tsx
+++ VocaDbWeb/New/components/Welcome/Welcome.tsx
@@ -1,14 +1,24 @@
-import { Title, Text, Anchor } from '@mantine/core';
+import { Title, Text, Anchor, useMantineTheme } from '@mantine/core';
 import useStyles from './Welcome.styles';
 
 export function Welcome() {
 	const { classes } = useStyles();
+	const theme = useMantineTheme();
 
 	return (
 		<>
 			<Title className={classes.title} align="center" mt={100}>
 				Welcome to{' '}
-				<Text inherit variant="gradient" component="span">
+				<Text
+					c={theme.primaryColor}
+					variant="gradient"
+					gradient={{
+						from: theme.colors[theme.primaryColor][9],
+						to: theme.colors[theme.primaryColor][2],
+					}}
+					inherit
+					component="span"
+				>
 					VocaDB
 				</Text>
 			</Title>
