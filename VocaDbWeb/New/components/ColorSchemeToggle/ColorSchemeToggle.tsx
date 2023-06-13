1de7ff785e7c1f34e0d02a171c9667b620900218
FinnRG <finngaertner@protonmail.com>
2023-06-14 01:43:06 +0200

    fix: Decrease color scheme toggle size

--- VocaDbWeb/New/components/ColorSchemeToggle/ColorSchemeToggle.tsx
+++ VocaDbWeb/New/components/ColorSchemeToggle/ColorSchemeToggle.tsx
@@ -6,11 +6,11 @@ export function ColorSchemeToggle() {
 	const { colorScheme, toggleColorScheme } = useMantineColorScheme();
 
 	return (
-		<Group position="center" my={30}>
+		<Group position="center" mr={10}>
 			<Switch
 				checked={colorScheme === 'dark'}
 				onChange={() => toggleColorScheme()}
-				size="lg"
+				size="md"
 				onLabel={<IconSun color={theme.white} size="1.25rem" stroke={1.5} />}
 				offLabel={
 					<IconMoonStars color={theme.colors.gray[6]} size="1.25rem" stroke={1.5} />
