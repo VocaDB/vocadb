275a2dbc8b9f5a3e57352da510047bdf01625c00
FinnRG <finngaertner@protonmail.com>
2023-06-19 21:58:50 +0200

    chores: Create placeholder user menu

--- VocaDbWeb/New/components/AppShell/UserButton.tsx
+++ VocaDbWeb/New/components/AppShell/UserButton.tsx
@@ -6,8 +6,17 @@ import {
 	Avatar,
 	Text,
 	createStyles,
+	Menu,
 } from '@mantine/core';
-import { IconChevronRight } from '@tabler/icons-react';
+import {
+	IconAdjustments,
+	IconChevronRight,
+	IconDisc,
+	IconLogout,
+	IconMusic,
+	IconUser,
+} from '@tabler/icons-react';
+import Image from 'next/image';
 
 const useStyles = createStyles((theme) => ({
 	user: {
@@ -27,30 +36,53 @@ interface UserButtonProps extends UnstyledButtonProps {
 	image: string;
 	name: string;
 	email: string;
-	icon?: React.ReactNode;
 }
 
-export function UserButton({ image, name, email, icon, ...others }: UserButtonProps) {
+export function UserButton({ image, name, email, ...others }: UserButtonProps) {
 	const { classes } = useStyles();
 
 	return (
-		<UnstyledButton className={classes.user} {...others}>
-			<Group>
-				<Avatar alt={name} src={image} radius="xl" />
+		<Menu position="right" shadow="md" offset={25} width={200}>
+			<Menu.Target>
+				<UnstyledButton className={classes.user} {...others}>
+					<Group>
+						{/* TODO: Find out why this looks bad * }
+						{/* <Image
+							alt={name}
+							src={image}
+							style={{ borderRadius: '50%', objectFit: 'cover' }}
+							width={38}
+							height={38}
+						/> */}
+						<Avatar alt={name} src={image} radius="xl" />
 
-				<div style={{ flex: 1 }}>
-					<Text size="sm" weight={500}>
-						{name}
-					</Text>
+						<div style={{ flex: 1 }}>
+							<Text size="sm" weight={500}>
+								{name}
+							</Text>
 
-					<Text color="dimmed" size="xs">
-						{email}
-					</Text>
-				</div>
+							<Text color="dimmed" size="xs">
+								{email}
+							</Text>
+						</div>
 
-				{icon || <IconChevronRight size="0.9rem" stroke={1.5} />}
-			</Group>
-		</UnstyledButton>
+						<IconChevronRight size="0.9rem" stroke={1.5} />
+					</Group>
+				</UnstyledButton>
+			</Menu.Target>
+			<Menu.Dropdown>
+				<Menu.Label>My Items</Menu.Label>
+				<Menu.Item icon={<IconUser size={14} />}>Profile</Menu.Item>
+				<Menu.Item icon={<IconDisc size={14} />}>My albums</Menu.Item>
+				<Menu.Item icon={<IconMusic size={14} />}>My songs</Menu.Item>
+
+				<Menu.Divider />
+
+				<Menu.Label>My Settings</Menu.Label>
+				<Menu.Item icon={<IconAdjustments size={14} />}>Settings</Menu.Item>
+				<Menu.Item icon={<IconLogout size={14} />}>Log out</Menu.Item>
+			</Menu.Dropdown>
+		</Menu>
 	);
 }
 
