6788e6e2a3e04896a2625e3859b58e91256d77b3
Takahashi Akari <akaritakahashioss@gmail.com>
2023-06-26 22:51:52 +0900

    Add a new discussion page (#1531)
    * Add: Discussion Page (index page only)
    
    * Fix review comments in PR #1531
    
    - Use 'Paper' component
    - Use mantine theme colors
    - Fix typos
    - Replace spaces with tabs
    - Remove 'inline-flex'
    - Refactor code
    
    * Fix various issues based on review comments PR #1531
    - Fix layout issues on smartphones
    - Remove unnecessary background color
    - Remove unnecessary keys
    - Merge duplicate classes
    - Reduce latency by using Promise.all

---
+++ VocaDbWeb/New/components/RecentTopic/RecentTopic.tsx
@@ -0,0 +1,41 @@
+import { DiscussionTopicContract } from '@/types/DataContracts/Discussion/DiscussionTopicContract';
+import { Text, Paper, Grid } from '@mantine/core';
+import useStyles from './RecentTopic.styles';
+
+interface RecentTopicProps {
+	topic: DiscussionTopicContract;
+}
+
+export function RecentTopic({ topic }: RecentTopicProps) {
+	const styles = useStyles();
+	return (
+		<>
+			<Text mt="md" mb="xs" className={styles.classes.topicName}>
+				{topic.name}
+			</Text>
+			<Text mt="md" mb="xs" className={styles.classes.topicDate}>
+				{new Date(topic.created).toLocaleString()} by {topic.author?.name}
+			</Text>
+		</>
+	);
+}
+
+interface RecentTopicsProps {
+	topics: DiscussionTopicContract[];
+}
+
+export function RecentTopics({ topics }: RecentTopicsProps) {
+	const styles = useStyles();
+	return (
+		<Grid className={styles.classes.folderGrid} grow gutter="xl">
+			{topics.map((topic) => (
+				<Grid.Col span={3} key={topic.id} style={{ maxWidth: "100%" }}>
+					<Paper shadow="sm" radius="md" p="md" withBorder className={styles.classes.folderGridList}>
+						<RecentTopic topic={topic} />
+					</Paper>
+				</Grid.Col>
+			))}
+		</Grid>
+	);
+}
+
