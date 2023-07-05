aceb340facd20b6b4e0fc9ed59c3219b90d906e0
FinnRG <finngaertner@protonmail.com>
2023-07-05 17:00:04 +0200

    chores: Formatting and types

--- VocaDbWeb/Scripts/Bootstrap/AbstractNav.tsx
+++ VocaDbWeb/Scripts/Bootstrap/AbstractNav.tsx
@@ -21,55 +21,53 @@ interface AbstractNavProps
 	parentOnSelect?: SelectCallback;
 }
 
-const AbstractNav: BsPrefixRefForwardingComponent<
-	'ul',
-	AbstractNavProps
-> = React.forwardRef<HTMLElement, AbstractNavProps>(
-	(
-		{
-			// Need to define the default "as" during prop destructuring to be compatible with styled-components github.com/react-bootstrap/react-bootstrap/issues/3595
-			as: Component = 'ul',
-			onSelect,
-			activeKey,
-			role,
-			onKeyDown,
-			...props
+const AbstractNav: BsPrefixRefForwardingComponent<'ul', AbstractNavProps> =
+	React.forwardRef<HTMLElement, AbstractNavProps>(
+		(
+			{
+				// Need to define the default "as" during prop destructuring to be compatible with styled-components github.com/react-bootstrap/react-bootstrap/issues/3595
+				as: Component = 'ul',
+				onSelect,
+				activeKey,
+				role,
+				onKeyDown,
+				...props
+			},
+			ref,
+		) => {
+			const parentOnSelect = useContext(SelectableContext);
+			const tabContext = useContext(TabContext);
+
+			let getControlledId, getControllerId;
+
+			if (tabContext) {
+				role = role || 'tablist';
+				activeKey = tabContext.activeKey;
+				getControlledId = tabContext.getControlledId;
+				getControllerId = tabContext.getControllerId;
+			}
+
+			const handleSelect = (key: any, event: any): void => {
+				//if (key == null) return;
+				onSelect?.(key, event);
+				parentOnSelect?.(key, event);
+			};
+
+			return (
+				<SelectableContext.Provider value={handleSelect}>
+					<NavContext.Provider
+						value={{
+							role, // used by NavLink to determine it's role
+							activeKey: makeEventKey(activeKey),
+							getControlledId: getControlledId || noop,
+							getControllerId: getControllerId || noop,
+						}}
+					>
+						<Component {...props} ref={ref} role={role} />
+					</NavContext.Provider>
+				</SelectableContext.Provider>
+			);
 		},
-		ref,
-	) => {
-		const parentOnSelect = useContext(SelectableContext);
-		const tabContext = useContext(TabContext);
-
-		let getControlledId, getControllerId;
-
-		if (tabContext) {
-			role = role || 'tablist';
-			activeKey = tabContext.activeKey;
-			getControlledId = tabContext.getControlledId;
-			getControllerId = tabContext.getControllerId;
-		}
-
-		const handleSelect = (key: any, event: any): void => {
-			//if (key == null) return;
-			onSelect?.(key, event);
-			parentOnSelect?.(key, event);
-		};
-
-		return (
-			<SelectableContext.Provider value={handleSelect}>
-				<NavContext.Provider
-					value={{
-						role, // used by NavLink to determine it's role
-						activeKey: makeEventKey(activeKey),
-						getControlledId: getControlledId || noop,
-						getControllerId: getControllerId || noop,
-					}}
-				>
-					<Component {...props} ref={ref} role={role} />
-				</NavContext.Provider>
-			</SelectableContext.Provider>
-		);
-	},
-);
+	);
 
 export default AbstractNav;
