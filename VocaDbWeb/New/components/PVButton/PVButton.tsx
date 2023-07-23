ac3a43c6d0cde2b4911bfa2c8be5f202f59155fd
FinnRG <finngaertner@protonmail.com>
2023-07-23 03:58:51 +0200

    fix: JSX stroke-linejoin -> strokeLinejoin

--- VocaDbWeb/New/components/PVButton/PVButton.tsx
+++ VocaDbWeb/New/components/PVButton/PVButton.tsx
@@ -20,9 +20,9 @@ const NicoNicoIcon = () => {
 		<svg
 			fill="none"
 			stroke="currentColor"
-			stroke-width="2"
-			stroke-linecap="round"
-			stroke-linejoin="round"
+			strokeWidth="2"
+			strokeLinecap="round"
+			strokeLinejoin="round"
 			role="img"
 			viewBox="0 0 24 24"
 			width="20"
