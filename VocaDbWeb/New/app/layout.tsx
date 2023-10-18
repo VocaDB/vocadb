d8fbcaf45dcfeba024a155a4b68da8f7b475809e
FinnRG <finngaertner@protonmail.com>
2023-10-18 16:13:15 +0200

    feat: Sidebar

--- VocaDbWeb/New/app/layout.tsx
+++ VocaDbWeb/New/app/layout.tsx
@@ -4,7 +4,7 @@ import { Metadata } from "next"
 import { siteConfig } from "@/config/site"
 import { fontSans } from "@/lib/fonts"
 import { cn } from "@/lib/utils"
-import { SidebarNav } from "@/components/sidebar-nav"
+import { Sidebar } from "@/components/sidebar-nav"
 import { SiteHeader } from "@/components/site-header"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
@@ -46,105 +46,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
               <SiteHeader />
               <div className="container flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10">
                 <aside className="fixed top-12 z-30 -ml-2 hidden h-[calc(100vh-3.5rem)] w-full shrink-0 md:sticky md:block">
-                  <SidebarNav
-                    items={[
-                      {
-                        title: "Getting Started",
-                        items: [
-                          {
-                            title: "Introduction",
-                            href: "/docs",
-                            items: [],
-                          },
-                          {
-                            title: "Installation",
-                            href: "/docs/installation",
-                            items: [],
-                          },
-                          {
-                            title: "components.json",
-                            href: "/docs/components-json",
-                            items: [],
-                          },
-                          {
-                            title: "Theming",
-                            href: "/docs/theming",
-                            items: [],
-                          },
-                          {
-                            title: "Dark mode",
-                            href: "/docs/dark-mode",
-                            items: [],
-                          },
-                          {
-                            title: "CLI",
-                            href: "/docs/cli",
-                            items: [],
-                          },
-                          {
-                            title: "Typography",
-                            href: "/docs/components/typography",
-                            items: [],
-                          },
-                          {
-                            title: "Figma",
-                            href: "/docs/figma",
-                            items: [],
-                          },
-                          {
-                            title: "Changelog",
-                            href: "/docs/changelog",
-                            items: [],
-                          },
-                          {
-                            title: "About",
-                            href: "/docs/about",
-                            items: [],
-                          },
-                        ],
-                      },
-                      {
-                        title: "Installation",
-                        items: [
-                          {
-                            title: "Next.js",
-                            href: "/docs/installation/next",
-                            items: [],
-                          },
-                          {
-                            title: "Vite",
-                            href: "/docs/installation/vite",
-                            items: [],
-                          },
-                          {
-                            title: "Remix",
-                            href: "/docs/installation/remix",
-                            items: [],
-                          },
-                          {
-                            title: "Gatsby",
-                            href: "/docs/installation/gatsby",
-                            items: [],
-                          },
-                          {
-                            title: "Astro",
-                            href: "/docs/installation/astro",
-                            items: [],
-                          },
-                          {
-                            title: "Laravel",
-                            href: "/docs/installation/laravel",
-                            items: [],
-                          },
-                          {
-                            title: "Manual",
-                            href: "/docs/installation/manual",
-                            items: [],
-                          },
-                        ],
-                      },
-                    ]}
-                  />
+                  <Sidebar playlists={[]} />
                 </aside>
                 <div>{children}</div>
               </div>
--- VocaDbWeb/New/components/sidebar-nav.tsx
+++ VocaDbWeb/New/components/sidebar-nav.tsx
@@ -1,51 +1,222 @@
 "use client"
 
-import { usePathname } from "next/navigation"
-
 import { cn } from "@/lib/utils"
 
-import { Icons } from "./icons"
+import { Button } from "./ui/button"
+import { ScrollArea } from "./ui/scroll-area"
 
-export interface SidebarNavItem {
-  title: string
-  href?: string
-  disabled?: boolean
-  external?: boolean
-  icon?: keyof typeof Icons
-  label?: string
-  items?: SidebarNavItem[]
+interface SidebarProps extends React.HTMLAttributes<HTMLDivElement> {
+  playlists: Playlist[]
 }
 
-export interface SidebarNavProps {
-  items: SidebarNavItem[]
-}
+export type Playlist = (typeof playlists)[number]
 
-export function SidebarNav({ items }: SidebarNavProps) {
-  const pathname = usePathname()
+export const playlists = [
+  "Recently Added",
+  "Recently Played",
+  "Top Songs",
+  "Top Albums",
+  "Top Artists",
+  "Logic Discography",
+  "Bedtime Beats",
+  "Feeling Happy",
+  "I miss Y2K Pop",
+  "Runtober",
+  "Mellow Days",
+  "Eminem Essentials",
+]
 
-  return items.length ? (
-    <div className="w-full">
-      {items.map((item, index) => (
-        <div key={index} className={cn("pb-4")}>
-          <h4 className="mb-1 px-2 py-1 text-sm font-semibold ">
-            {item.title}
-          </h4>
-          {item.items?.length && (
-            <SidebarNavItems items={item.items} pathname={pathname} />
-          )}
+export function Sidebar({ className, playlists }: SidebarProps) {
+  return (
+    <div className={cn("pb-12", className)}>
+      <div className="space-y-4 py-4">
+        <div className="px-3 py-2">
+          <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight">
+            Discover
+          </h2>
+          <div className="space-y-1">
+            <Button variant="secondary" className="w-full justify-start">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="mr-2 h-4 w-4"
+              >
+                <circle cx="12" cy="12" r="10" />
+                <polygon points="10 8 16 12 10 16 10 8" />
+              </svg>
+              Listen Now
+            </Button>
+            <Button variant="ghost" className="w-full justify-start">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="mr-2 h-4 w-4"
+              >
+                <rect width="7" height="7" x="3" y="3" rx="1" />
+                <rect width="7" height="7" x="14" y="3" rx="1" />
+                <rect width="7" height="7" x="14" y="14" rx="1" />
+                <rect width="7" height="7" x="3" y="14" rx="1" />
+              </svg>
+              Browse
+            </Button>
+            <Button variant="ghost" className="w-full justify-start">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="mr-2 h-4 w-4"
+              >
+                <path d="M4.9 19.1C1 15.2 1 8.8 4.9 4.9" />
+                <path d="M7.8 16.2c-2.3-2.3-2.3-6.1 0-8.5" />
+                <circle cx="12" cy="12" r="2" />
+                <path d="M16.2 7.8c2.3 2.3 2.3 6.1 0 8.5" />
+                <path d="M19.1 4.9C23 8.8 23 15.1 19.1 19" />
+              </svg>
+              Radio
+            </Button>
+          </div>
         </div>
-      ))}
+        <div className="px-3 py-2">
+          <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight">
+            Library
+          </h2>
+          <div className="space-y-1">
+            <Button variant="ghost" className="w-full justify-start">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="mr-2 h-4 w-4"
+              >
+                <path d="M21 15V6" />
+                <path d="M18.5 18a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z" />
+                <path d="M12 12H3" />
+                <path d="M16 6H3" />
+                <path d="M12 18H3" />
+              </svg>
+              Playlists
+            </Button>
+            <Button variant="ghost" className="w-full justify-start">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="mr-2 h-4 w-4"
+              >
+                <circle cx="8" cy="18" r="4" />
+                <path d="M12 18V2l7 4" />
+              </svg>
+              Songs
+            </Button>
+            <Button variant="ghost" className="w-full justify-start">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="mr-2 h-4 w-4"
+              >
+                <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
+                <circle cx="12" cy="7" r="4" />
+              </svg>
+              Made for You
+            </Button>
+            <Button variant="ghost" className="w-full justify-start">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="mr-2 h-4 w-4"
+              >
+                <path d="m12 8-9.04 9.06a2.82 2.82 0 1 0 3.98 3.98L16 12" />
+                <circle cx="17" cy="7" r="5" />
+              </svg>
+              Artists
+            </Button>
+            <Button variant="ghost" className="w-full justify-start">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="mr-2 h-4 w-4"
+              >
+                <path d="m16 6 4 14" />
+                <path d="M12 6v14" />
+                <path d="M8 8v12" />
+                <path d="M4 4v16" />
+              </svg>
+              Albums
+            </Button>
+          </div>
+        </div>
+        <div className="py-2">
+          <h2 className="relative px-7 text-lg font-semibold tracking-tight">
+            Playlists
+          </h2>
+          <ScrollArea className="h-[300px] px-1">
+            <div className="space-y-1 p-2">
+              {playlists?.map((playlist, i) => (
+                <Button
+                  key={`${playlist}-${i}`}
+                  variant="ghost"
+                  className="w-full justify-start font-normal"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="mr-2 h-4 w-4"
+                  >
+                    <path d="M21 15V6" />
+                    <path d="M18.5 18a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z" />
+                    <path d="M12 12H3" />
+                    <path d="M16 6H3" />
+                    <path d="M12 18H3" />
+                  </svg>
+                  {playlist}
+                </Button>
+              ))}
+            </div>
+          </ScrollArea>
+        </div>
+      </div>
     </div>
-  ) : null
-}
-
-interface SidebarNavItemsProps {
-  items?: SidebarNavItem[]
-  pathname: string | null
-}
-
-export function SidebarNavItems({ items, pathname }: SidebarNavItemsProps) {
-  return items?.length ? (
-    <div className="grid grid-flow-row auto-rows-max text-sm">{}</div>
-  ) : null
+  )
 }
