369d95e4157f0552fd6aa06ee28aabb84b0b5a91
Finn Gärtner <finngaertner@protonmail.com>
2023-12-21 17:59:23 +0100

    fix: Remove old playlists[] code

--- VocaDbWeb/New/app/layout.tsx
+++ VocaDbWeb/New/app/layout.tsx
@@ -54,7 +54,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
               <SiteHeader />
               <div className="container flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10">
                 <aside className="fixed top-12 z-30 -ml-2 hidden h-[calc(100vh-3.5rem)] w-full shrink-0 md:sticky md:block">
-                  <Sidebar playlists={[]} />
+                  <Sidebar />
                 </aside>
                 <div>{children}</div>
               </div>
