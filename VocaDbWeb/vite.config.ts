140b48830366595e0fcb9493e6b28411053c7320
Finn Gärtner <finngaertner@protonmail.com>
2024-11-12 00:23:39 +0100

    feat: Faro performance monitoring

--- VocaDbWeb/vite.config.ts
+++ VocaDbWeb/vite.config.ts
@@ -1,44 +1,54 @@
+import faroUploader from '@grafana/faro-rollup-plugin';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
-import { visualizer } from 'rollup-plugin-visualizer';
-import { PluginOption, defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	build: {
-		outDir: 'wwwroot',
-	},
-	publicDir: 'public',
-	resolve: {
-		alias: {
-			'@': resolve(__dirname, './Scripts'),
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd());
+	const VITE_FARO_API_KEY = `${env.VITE_FARO_API_KEY ?? ''}`;
+
+	return {
+		build: {
+			outDir: 'wwwroot',
 		},
-	},
-	plugins: [
-		react({
-			// https://dev.to/ajitsinghkamal/using-emotionjs-with-vite-2ndj#comment-1nif3
-			jsxImportSource: '@emotion/react',
-		}),
-		visualizer({
-			template: 'treemap',
-			open: true,
-			filename: 'analyse.html',
-		}) as PluginOption,
-	],
-	server: {
-		proxy: {
-			'/api': {
-				target: 'https://localhost:5001',
-				changeOrigin: true,
-				// https://stackoverflow.com/questions/74033733/vite-self-signed-certificate-error-when-calling-local-api/74033815#74033815
-				secure: false,
-			},
-			'^/stats/.*': {
-				target: 'https://localhost:5001',
-				changeOrigin: true,
-				// https://stackoverflow.com/questions/74033733/vite-self-signed-certificate-error-when-calling-local-api/74033815#74033815
-				secure: false,
+		publicDir: 'public',
+		resolve: {
+			alias: {
+				'@': resolve(__dirname, './Scripts'),
 			},
 		},
-	},
+		plugins: [
+			react({
+				// https://dev.to/ajitsinghkamal/using-emotionjs-with-vite-2ndj#comment-1nif3
+				jsxImportSource: '@emotion/react',
+			}),
+			faroUploader({
+				appName: 'vocadb_frontend',
+				endpoint: 'https://faro-api-prod-eu-west-2.grafana.net/faro/api/v1',
+				appId: 'vocadb_frontend',
+				stackId: '1088236',
+				// instructions on how to obtain your API key are in the documentation
+				// https://grafana.com/docs/grafana-cloud/monitor-applications/frontend-observability/sourcemap-upload-plugins/#obtain-an-api-key
+				apiKey: VITE_FARO_API_KEY,
+				gzipContents: true,
+			}),
+		],
+		server: {
+			proxy: {
+				'/api': {
+					target: 'https://localhost:5001',
+					changeOrigin: true,
+					// https://stackoverflow.com/questions/74033733/vite-self-signed-certificate-error-when-calling-local-api/74033815#74033815
+					secure: false,
+				},
+				'^/stats/.*': {
+					target: 'https://localhost:5001',
+					changeOrigin: true,
+					// https://stackoverflow.com/questions/74033733/vite-self-signed-certificate-error-when-calling-local-api/74033815#74033815
+					secure: false,
+				},
+			},
+		},
+	};
 });
