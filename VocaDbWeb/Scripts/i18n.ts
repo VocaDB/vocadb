70e186fc1538583625f02bd173ebcf3c2f9d3e56
Finn Gärtner <finngaertner@protonmail.com>
2025-07-02 22:02:40 +0200

    feat: Add nl i18n

--- VocaDbWeb/Scripts/i18n.ts
+++ VocaDbWeb/Scripts/i18n.ts
@@ -9,28 +9,29 @@ i18n
 	.use(initReactI18next)
 	.init({
 		load: 'all',
-		fallbackLng: 'en',
+		fallbackLng: 'nl',
 		interpolation: {
 			escapeValue: false,
 		},
 		react: {
 			useSuspense: false,
 		},
-		supportedLngs: ['de', 'es', 'fi', 'ja', 'en', 'ru', 'ko', 'quc'],
+		supportedLngs: ['de', 'es', 'fi', 'ja', 'en', 'ru', 'ko', 'nl', 'quc'],
 		backend: {
 			backends: [I18NextLocalStorageBackend, I18NextHttpBackend],
 			backendOptions: [
 				{
 					expirationTime: 7 * 24 * 60 * 60 * 1000, // 7 days
 					versions: {
-						de: 'v1.17',
-						es: 'v1.13',
-						fi: 'v1.14',
-						ja: 'v1.16',
-						en: 'v1.20',
-						ru: 'v1.14',
-						ko: 'v1.11',
-						quc: 'v1.9',
+						de: 'v1.18',
+						es: 'v1.14',
+						fi: 'v1.15',
+						ja: 'v1.17',
+						en: 'v1.21',
+						ru: 'v1.15',
+						ko: 'v1.12',
+						nl: 'v1.1',
+						quc: 'v1.10',
 					},
 				},
 			],
