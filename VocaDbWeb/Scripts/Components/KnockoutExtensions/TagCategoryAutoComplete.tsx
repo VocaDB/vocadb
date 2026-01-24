77d030ccee22a540922bf46bf07f36f15f8025de
Finn R. Gärtner <65015656+FinnRG@users.noreply.github.com>
2026-01-24 21:47:01 +0100

    fix: Properly autocomplete tag categories (#2039)

--- VocaDbWeb/Scripts/Components/KnockoutExtensions/TagCategoryAutoComplete.tsx
+++ VocaDbWeb/Scripts/Components/KnockoutExtensions/TagCategoryAutoComplete.tsx
@@ -2,6 +2,7 @@ import JQueryUIAutocomplete from '@/JQueryUI/JQueryUIAutocomplete';
 import { functions } from '@/Shared/GlobalFunctions';
 import { httpClient } from '@/Shared/HttpClient';
 import useMergedRefs from '@restart/hooks/useMergedRefs';
+import $ from 'jquery';
 import React from 'react';
 
 interface TagCategoryAutoCompleteProps
@@ -22,28 +23,38 @@ export const TagCategoryAutoComplete = React.forwardRef<
 		const inputRef = React.useRef<HTMLInputElement>(undefined!);
 		const mergedRef = useMergedRefs<HTMLInputElement | null>(inputRef, ref);
 
+		const source = React.useCallback(
+			(ui: { term: any }, callback: (result: string[]) => void): void => {
+				httpClient.get<string[]>(url, { query: ui.term }).then(callback);
+			},
+			[url],
+		);
+
+		const select = React.useCallback(
+			(event: Event, ui: JQueryUI.AutocompleteUIParams): boolean => {
+				onAcceptSelection(ui.item.label);
+
+				if (clearValue) {
+					if (inputRef.current) inputRef.current.value = '';
+					return false;
+				} else {
+					return true;
+				}
+			},
+			[],
+		);
+
 		return (
 			<JQueryUIAutocomplete
-				source={(
-					ui: { term: any },
-					callback: (result: string[]) => void,
-				): void => {
-					httpClient
-						.get<string[]>(url, { query: ui.term })
-						.then(callback);
-				}}
-				select={(event: Event, ui): boolean => {
-					onAcceptSelection(ui.item.label);
-
-					if (clearValue) {
-						if (inputRef.current) inputRef.current.value = '';
-						return false;
-					} else {
-						return true;
-					}
-				}}
+				source={source}
+				select={select}
 				{...props}
+				minLength={0}
 				ref={mergedRef}
+				onFocus={(e): void => {
+					$(e.target).autocomplete('search', e.target.value);
+					props.onFocus?.(e);
+				}}
 			/>
 		);
 	},
