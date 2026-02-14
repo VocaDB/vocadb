2ac9df8374e5466f0ce5a6b6d4a4bdad9c8234ba
Finn R. Gärtner <65015656+FinnRG@users.noreply.github.com>
2026-02-14 22:51:35 +0100

    feat: Duplicate account event (#2095)
    * feat: Duplicate account event
    
    * chores: Update tests

--- Tests/Domain/WebhookTests.cs
+++ Tests/Domain/WebhookTests.cs
@@ -8,6 +8,7 @@ public sealed class WebhookEventsTests
 	[DataRow(0, WebhookEvents.Default)]
 	[DataRow(1, WebhookEvents.User)]
 	[DataRow(2, WebhookEvents.EntryReport)]
+	[DataRow(4, WebhookEvents.DuplicateAccount)]
 	[TestMethod]
 	public void Value(int expected, WebhookEvents actual)
 	{
